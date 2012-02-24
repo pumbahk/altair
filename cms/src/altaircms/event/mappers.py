4a92943c1039cc52729f5af6cdab4293091bc6ae
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2012-02-24 15:32:18 +0900

    BaseRESTAPIの共通部分をまとめた

---
+++ cms/src/altaircms/event/mappers.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+from bpmappers import Mapper
+from bpmappers.fields import RawField, DelegateField, ListDelegateField
+
+class EventMapper(Mapper):
+    def _ensure_timeformat(self, value):
+        return value.now().isoformat()
+
+    def after_filter_event_open(self, value):
+        return self._ensure_timeformat(value)
+
+    def after_filter_event_close(self, value):
+        return self._ensure_timeformat(value)
+
+    def after_filter_deal_open(self, value):
+        return self._ensure_timeformat(value)
+
+    def after_filter_deal_close(self, value):
+        return self._ensure_timeformat(value)
+
+    def after_filter_id(self, value):
+        return int(value)
+
+    def after_filter_is_searchable(self, value):
+        return True if value == 'y' else False
+
+    id = RawField()
+    title = RawField()
+    subtitle = RawField()
+    description = RawField()
+    place = RawField()
+    inquiry_for = RawField()
+    event_open = RawField()
+    event_close = RawField()
+    deal_open = RawField()
+    deal_close = RawField()
+    is_searchable = RawField()
+    client_id = RawField()  # delegateしたほうがいいかも
+
+
+class EventsMapper(Mapper):
+    events = ListDelegateField(EventMapper)
