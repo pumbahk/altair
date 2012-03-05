74c1631f21d69dc4f5eedec14b8cf553bc151b61
podhmo <podhmo+altair@beproud.jp>
2012-03-05 13:42:36 +0900

    dialog.makoのfilepathを変更した

--- cms/src/altaircms/helpers/__init__.py
+++ cms/src/altaircms/helpers/__init__.py
@@ -1,8 +1,5 @@
-# from . import base
-# from . import front
-# from . import asset
+from . import base
+from . import front
+from . import asset
 
-import base
-import front
-import asset
 __all__ = ["base", "front", "asset"]
