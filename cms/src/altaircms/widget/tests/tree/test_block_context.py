f32c6419c12c2637c096336d9fb2691c145c240f
podhmo <podhmo+altair@beproud.jp>
2012-03-05 10:16:12 +0900

    BlockContextで利用したい値を定義しておくことができるようにした(e.g. request必須など)
    testも書いた。

--- cms/src/altaircms/widget/tests/tree/test_block_context.py
+++ cms/src/altaircms/widget/tests/tree/test_block_context.py
@@ -5,9 +5,17 @@ class DummyWidget(object):
     def to_html(self):
         return "<p>%s</p>" % self.name
 
-    def merge_settings(self, bname, bsettings):
+    def merge_settings(self, bname, bcontext):
         D = {bname: self.to_html()}
-        bsettings.merge(D)
+        bcontext.merge(D)
+
+class NeedValueWidget(DummyWidget):
+    def merge_settings(self, bname, bcontext):
+        bcontext.need_extra_in_scan("user")
+        def top_html():
+            user = bcontext.extra["user"]
+            return "<p>user: %s</p>" % user
+        bcontext.merge({"top": top_html})
 
 class WithJSWidget(object):
     def __init__(self, name, id):
@@ -23,53 +31,91 @@ class WithJSWidget(object):
 _.each(%s, echo);
 """ % ids
 
-    def _attach_jscode_if_need(self, bsettings):
-        if not bsettings.is_attached(self, "js_prerender"):
+    def _attach_jscode_if_need(self, bcontext):
+        if not bcontext.is_attached(self, "js_prerender"):
             def to_html():
                 code = """function echo(i){console.log(i)} 
                 _.each([%s], echo);
-                """ % ", ".join(bsettings.get_store(self))
+                """ % ", ".join(bcontext.get_store(self))
                 return code
-            bsettings.add("js_prerender", to_html)
-            bsettings.attach_widget(self, "js_prerender")
+            bcontext.add("js_prerender", to_html)
+            bcontext.attach_widget(self, "js_prerender")
         
-    def merge_settings(self, bname, bsettings):
-        self._attach_jscode_if_need(bsettings)
-        bsettings.add(bname, self.to_html())
+    def merge_settings(self, bname, bcontext):
+        self._attach_jscode_if_need(bcontext)
+        bcontext.add(bname, self.to_html())
 
-        if not bsettings.has_store(self):
-            bsettings.create_store(self, [])
-        bsettings.get_store(self).append(str(self.id))
-
-        
+        if not bcontext.has_store(self):
+            bcontext.create_store(self, [])
+        bcontext.get_store(self).append(str(self.id))
 
 import unittest
 from altaircms.widget.tree.block_context import BlockContext
 
 class BlockContextTest(unittest.TestCase):
     def test_simple(self):
+        """動作チェック
+        """
         class WTree(object):
             blocks = {"top": [DummyWidget("foo"), DummyWidget("bar")]}
 
-        bsettings = BlockContext.from_widget_tree(WTree)
-        self.assertEquals("".join(bsettings.blocks["top"]),
+        bcontext = BlockContext.from_widget_tree(WTree)
+        bcontext.scan()
+        self.assertEquals("".join(bcontext.blocks["top"]),
                           '<p>bar</p><p>foo</p>')
 
     def test_with_js_widget(self):
+        """関数を含んだwidgetで動作チェック
+        """
         class WTree(object):
             blocks = {"top": [WithJSWidget("foo", 1), WithJSWidget("bar", 2)]}
-        bsettings = BlockContext.from_widget_tree(WTree)
-        self.assertEquals("".join(bsettings.blocks["top"]),
+        bcontext = BlockContext.from_widget_tree(WTree)
+        bcontext.scan()
+        self.assertEquals("".join(bcontext.blocks["top"]),
                           '<p id="1">foo</p><p id="2">bar</p>')
 
     def test_jscode(self):
+        """ 複数のwidgetの内容をマージして、ひとつの部分(jscode)を生成できているか確認
+        """
         class WTree(object):
             blocks = {"top": [WithJSWidget("foo", 1), WithJSWidget("bar", 2)]}
-        bsettings = BlockContext.from_widget_tree(WTree, scan=True)
-        self.assertEquals(bsettings.blocks["js_prerender"][0], 
+        bcontext = BlockContext.from_widget_tree(WTree)
+        bcontext.scan()
+        self.assertEquals(list(bcontext.blocks["js_prerender"])[0], 
                           """function echo(i){console.log(i)} 
                 _.each([1, 2], echo);
                 """)
+
+    def test_external_context(self):
+        """外部コンテキストの注入のチェック
+        """
+        class WTree(object):
+            blocks = {"top": [DummyWidget("foo")]}
+        bcontext = BlockContext.from_widget_tree(WTree)
+        bcontext.scan(user="user")
+        self.assertEquals(bcontext.extra["user"], "user")
+
+    def test_extra_raise_exception_if_not_found(self):
+        """指定した値がself.extraに存在しなかった場合例外が投げられるはず
+        """
+        class WTree(object):
+            blocks = {"top": [NeedValueWidget("foo")]}
+        bcontext = BlockContext.from_widget_tree(WTree)
+
+        from altaircms.widget.tree.block_context import BlockContextException
+        self.assertRaises(BlockContextException, bcontext.scan)
+        
+    def test_extra_validator_ok(self):
+        """指定した値がself.extraにあった場合はok
+        """
+        class WTree(object):
+            blocks = {"top": [NeedValueWidget("foo")]}
+        bcontext = BlockContext.from_widget_tree(WTree)
+        bcontext.scan(user="user")
+        self.assertEquals(list(bcontext.blocks["top"])[0], 
+                          "<p>user: user</p>")
+        
         
 if __name__ == "__main__":
+
     unittest.main()
