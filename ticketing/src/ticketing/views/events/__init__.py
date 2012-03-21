521e4303024708af2e3790188ba6f88f0d58f562
Misato <misato@takahashi.name>
2012-03-21 20:19:12 +0900

    byebye deform

--- ticketing/src/ticketing/views/events/__init__.py
+++ ticketing/src/ticketing/views/events/__init__.py
@@ -6,3 +6,4 @@ def add_routes(config):
     config.add_route('admin.events.new'     , '/new')
     config.add_route('admin.events.show'    , '/show/{event_id}')
     config.add_route('admin.events.edit'    , '/edit/{event_id}')
+    config.add_route('admin.events.delete'  , '/delete/{event_id}')
--- ticketing/src/ticketing/views/events/forms.py
+++ ticketing/src/ticketing/views/events/forms.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from deform.widget import SelectWidget
-from colander import MappingSchema, SchemaNode, String, Int, DateTime, Bool, Decimal, Float
-from ticketing.models.master import Prefecture
+from wtforms import TextField, PasswordField, TextAreaField, DateField, SelectField, SubmitField, HiddenField, BooleanField, FileField
+from wtforms.validators import Required, Email, Length, NumberRange,EqualTo,optional
+from wtforms import Form
 
-class EventForm(MappingSchema):
-    start_on            = SchemaNode(DateTime()   , title=u'開始日')
-    end_on              = SchemaNode(DateTime()   , title=u'終了日')
-    code                = SchemaNode(String()   , title=u'公演コード')
-    title               = SchemaNode(String()   , title=u'タイトル')
-    abbreviated_title   = SchemaNode(String()   , title=u'タイトル略')
-    margin_ratio        = SchemaNode(Float()    , title=u'マージン')
-    printing_fee        = SchemaNode(Decimal()  , title=u'印刷手数料')
-    registration_fee    = SchemaNode(Decimal()  , title=u'登録手数料')
+class EventForm(Form):
+    start_on            = DateField(u'開始日', validators=[Required()])
+    end_on              = DateField(u'終了日', validators=[Required()])
+    code                = TextField(u'公演コード', validators=[Required()])
+    title               = TextField(u'タイトル', validators=[Required()])
+    abbreviated_title   = TextField(u'タイトル略', validators=[Required()])
+    margin_ratio        = TextField(u'マージン', validators=[])
+    printing_fee        = TextField(u'印刷手数料', validators=[])
+    registration_fee    = TextField(u'登録手数料', validators=[])
+
