6c099b00178a9e2710d76a2cda51d9ecd20949e1
Moriyoshi Koizumi <mozo@mozo.jp>
2012-04-03 10:08:14 +0900

    Fix guide creation function.

--- ticket-designer/src/views/Viewport.hx
+++ ticket-designer/src/views/Viewport.hx
@@ -5,4 +5,5 @@ interface Viewport implements Disposable {
     public var on(default, null):Dynamic;
     public var scrollPosition(get_scrollPosition, set_scrollPosition):Point;
     public var size(get_size, null):Point;
+    public var screenOffset(default, null):Point;
 }
