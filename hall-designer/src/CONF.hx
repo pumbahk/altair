efc28ceb1ccbdd25c4efc8d07aa1dd69a008f500
Moriyoshi Koizumi <mozo@mozo.jp>
2012-04-03 12:00:17 +0900

    Add utility script to run the demo locally.

--- hall-designer/src/CONF.hx
+++ hall-designer/src/CONF.hx
@@ -4,7 +4,7 @@ class CONF {
     public static inline var FASHION_IMPL_PRIORITY = ["SVG", "VML", "Canvas"];
     public static inline var FASHION_TARGET_ID     = "main";
 
-    public static inline var SERVER:String = "http://localhost:81/hall-designer/demo/";
+    public static inline var SERVER:String = "http://localhost:8081/hall-designer/demo/";
     public static inline var SRC_LO:String = "test_L0.json";
     public static inline var SRC_L1:String = "test_L1.json";
     public static inline var MEMORIZE:Bool = true;
