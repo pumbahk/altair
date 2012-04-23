9ecf7160994fb22668b875ff9e2a634be22fedbc
Atsushi Odagiri <aodagx@gmail.com>
2012-04-23 16:36:13 +0900

    garden

--- cms/src/altaircms/front/__init__.py
+++ cms/src/altaircms/front/__init__.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
 def includeme(config):
-    from .resources import  PageRenderingResource
-    config.add_route('front', '/publish/{page_name:.*}', factory=PageRenderingResource) # fix-url after. implemnt preview
-    config.add_route("front_to_preview", "/to/preview/{page_id}", factory=PageRenderingResource)
-    config.add_route('front_preview', '/preview/{page_name:.*}', factory=PageRenderingResource)
+
+    config.add_route('front', '/publish/{page_name:.*}', factory=".resources.PageRenderingResource") # fix-url after. implemnt preview
+    config.add_route("front_to_preview", "/to/preview/{page_id}", factory=".resources.PageRenderingResource")
+    config.add_route('front_preview', '/preview/{page_name:.*}', factory=".resources.PageRenderingResource")
 
     config.scan('.views')
