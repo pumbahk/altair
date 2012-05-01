fb4a05e32cfa39d13706ec15460c586f60a4c49a
podhmo <podhmo+altair@beproud.jp>
2012-05-01 16:25:42 +0900

    added migration code for heading widget

---
+++ cms/alembic/versions/1e3174b92c80_create_heading_widge.py
@@ -0,0 +1,27 @@
+"""create heading widget
+
+Revision ID: 1e3174b92c80
+Revises: d8a2755faa4
+Create Date: 2012-05-01 16:16:28.465367
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1e3174b92c80'
+down_revision = 'd8a2755faa4'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('widget_heading',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('items', sa.Unicode(length=255), nullable=True),
+                    sa.Column("kind", sa.String(length=32), nullable=False), 
+                    sa.ForeignKeyConstraint(['id'], ['widget.id'], ),
+                    sa.PrimaryKeyConstraint('id')
+                    )
+
+def downgrade():
+    op.drop_table("widget_heading")
