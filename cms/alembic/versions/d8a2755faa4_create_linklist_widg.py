9fe95f86860ca62de28bb3c718903b04cb14fe8a
podhmo <podhmo+altair@beproud.jp>
2012-05-01 14:45:28 +0900

    added migration code for linklist widget

---
+++ cms/alembic/versions/d8a2755faa4_create_linklist_widg.py
@@ -0,0 +1,28 @@
+"""create linklist widget
+
+Revision ID: d8a2755faa4
+Revises: 18c8a8c86e2d
+Create Date: 2012-05-01 14:43:49.355596
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd8a2755faa4'
+down_revision = '18c8a8c86e2d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('widget_linklist',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('items', sa.Unicode(length=255), nullable=True),
+                    sa.Column("kind", sa.String(length=32), nullable=False), 
+                    sa.ForeignKeyConstraint(['id'], ['widget.id'], ),
+                    sa.PrimaryKeyConstraint('id')
+                    )
+
+def downgrade():
+    op.drop_table("widget_linklist")
+
