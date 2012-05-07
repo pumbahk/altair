cd130c341b65bf8cb3016950a41f55bfc6c2632a
加藤早織 <ribontolili@gmail.com>
2012-05-07 10:39:29 +0900

    delete files

--- artist-page/mock/artist.php
+++
@@ -1,7 +0,0 @@
-<?php
-$artistname = file_get_contents('./artist.csv');
-$itigyou = explode("\n",$artistname);
-for($i=0;$i<$itigyou;$i++){
-echo $itigyou[i];
-}
-?>
--- artist-page/mock/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-$doc = simplexml_load_file('http://api.rakuten.co.jp/rws/3.0/rest?developerId=12657057e6e263dfe5dd57b5565078da&operation=BooksCDSearch&version=2011-12-01&artistName=AI');
-$doc->registerXPathNamespace('booksCDSearch', 'http://api.rakuten.co.jp/rws/rest/BooksCDSearch/2011-12-01');
-foreach ($doc->xpath('Body/booksCDSearch:BooksCDSearch/Items/Item') as $item) {
-    echo $item->title."\n";
-}
-
-?>
