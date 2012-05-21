8a440fa8cfa82aebd9c663cc946e4cfc0681a0b5
podhmo <podhmo+altair@beproud.jp>
2012-05-21 21:59:07 +0900

    replace tables

---
+++ bin/replace-tables.sh
@@ -0,0 +1,11 @@
+## drop
+echo  "drop database altaircms; create database altaircms default character set utf8; drop database ticketing; create database ticketing default character set utf8;" | mysql -u root -p
+## create
+pushd ../cms
+python setup.py upgrade_db
+pmain -c production.ini -s altaircms.scripts.pmain.insert_demodata
+popd
+
+pushd ../ticketing/src
+python seed_import.py
+popd
