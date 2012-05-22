d6d5c452d2b7a902ef5e617444a7f4e634a3de9d
Atsushi Odagiri <aodagx@gmail.com>
2012-05-22 17:19:46 +0900

    安心決済コンポーネントをticketing内部に移動

---
+++ ticketing/src/ticketing/checkout/interfaces.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+from zope.interface import Interface, Attribute
+
+
+class ISigner(Interface):
+    hash_algorithm = Attribute(u"hash algorithm name, SHA1 or MD5")
+    def __call__(checkout):
+        """ 正規化XMLの署名作成する
+        """
