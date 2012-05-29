10944863b2bc50579ccb000a43025eeecc9ae866
podhmo <podhmo+altair@beproud.jp>
2012-05-29 13:35:49 +0900

    topic modelに説明をたくさん書いた

--- cms/src/altaircms/plugins/widget/topic/forms.py
+++ cms/src/altaircms/plugins/widget/topic/forms.py
@@ -6,6 +6,14 @@ from altaircms.topic.models import Topic
 from altaircms.topic.models import Topcontent
 from altaircms.models import DBSession
 
+"""
+レンダリングされるtopicモデルの種類
+----------------------------------------
+
+noimage(文章のみ)では、topic modelが
+hasimage(画像あり)では、topcontentがレンダリングされる
+"""
+
 class TopicChoiceForm(form.Form):
     def configure_choices_field(self):
         choices = [(q[0], q[0])for q in DBSession.query(Topic.subkind).order_by("subkind").distinct()] ##
@@ -19,8 +27,8 @@ class TopicChoiceForm(form.Form):
     subkind = fields.SelectField(id="subkind", label=u"サブ分類")
     display_count = fields.IntegerField(id="display_count", label=u"表示件数", default=5, validators=[validators.Required()])
     display_global = fields.BooleanField(id="display_global", label=u"グローバルトピックを表示", default=True)
-    display_page = fields.BooleanField(id="display_page", label=u"ページに関連したトピックのみを表示", default=True)
-    display_event = fields.BooleanField(id="display_event", label=u"イベントに関連したトピックのみを表示", default=True)
+    display_page = fields.BooleanField(id="display_page", label=u"ページに関連したトピックを表示", default=True)
+    display_event = fields.BooleanField(id="display_event", label=u"イベントに関連したトピックを表示", default=True)
 
     # def transform(self, topic_type):
     #     if topic_type == "topcontent":
