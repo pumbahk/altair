5808770d443fbafa944dfb1e679991548e42557b
podhmo <podhmo+altair@beproud.jp>
2012-05-30 19:17:18 +0900

    __init__.pyの不要な分岐削除

--- cms/src/altaircms/__init__.py
+++ cms/src/altaircms/__init__.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 
 from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
-from pyramid.settings import asbool
 
 import sqlahelper
 from sqlalchemy import engine_from_config
@@ -24,13 +23,6 @@ try:
     logger.info('Using PyMySQL')
 except:
     pass
-
-
-def main_with_strip_secret(global_config, settings):
-    D = {"altaircms.debug.strip_security": True}
-    settings.update(D)
-    return main(global_config, **settings)
-
 
 def _get_policies(settings):
     from pyramid.authentication import AuthTktAuthenticationPolicy
@@ -52,10 +44,7 @@ def main(global_config, **settings):
     )
 
     ## bind authenticated user to request.user
-    if asbool(settings.get("altaircms.debug.strip_security", 'false')):
-        config.set_request_property("altaircms.auth.helpers.get_debug_user", "user", reify=True)
-    else:
-        config.set_request_property("altaircms.auth.helpers.get_authenticated_user", "user", reify=True)
+    config.set_request_property("altaircms.auth.helpers.get_authenticated_user", "user", reify=True)
 
 
     config.include("altaircms.lib.crud")    
@@ -88,7 +77,6 @@ def main(global_config, **settings):
                           "pyramid.events.BeforeRender")
     config.add_subscriber(".subscribers.add_choices_query_refinement", 
                           ".lib.formevent.AfterFormInitialize")
-    # config.scan('.subscribers')
     
     config.add_static_view('static', 'altaircms:static', cache_max_age=3600)
     config.add_static_view('plugins/static', 'altaircms:plugins/static', cache_max_age=3600)
@@ -98,14 +86,4 @@ def main(global_config, **settings):
     sqlahelper.get_session().remove()
     sqlahelper.add_engine(engine)
 
-    ## 設定ファイルを読み込んで追加でinclude.(debug用)
-    if asbool(settings.get("altaircms.debug.start_when_dropall", "false")):
-        from altaircms.models import initialize_sql
-        warnings.warn("altaircms.debug.start_when_dropall is true. all table are dropped!")
-        initialize_sql(engine, dropall=True)
-
-    if settings.get("altaircms.debug.additional_includes"):
-        for m in re.split("\s+", settings.get("altaircms.debug.additional_includes").lstrip()):
-            warnings.warn("------------additional include " + m)
-            config.include(m); config.scan(m)
     return config.make_wsgi_app()
