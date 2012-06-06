1727192562cb5dab194ce348133e10cb07cf186c
mmatsui <masahiro.matsui@gmail.com>
2012-06-06 13:45:49 +0900

    routingの冗長なパスを修正

--- ticketing/src/ticketing/events/payment_delivery_method_pairs/__init__.py
+++ ticketing/src/ticketing/events/payment_delivery_method_pairs/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def includeme(config):
-    config.add_route('payment_delivery_method_pairs.new', '/{sales_segment_id}/payment_delivery_method_pairs/new')
+    config.add_route('payment_delivery_method_pairs.new', '/{sales_segment_id}/new')
     config.add_route('payment_delivery_method_pairs.edit', '/edit/{payment_delivery_method_pair_id}/')
     config.add_route('payment_delivery_method_pairs.delete', '/delete/{payment_delivery_method_pair_id}/')
--- ticketing/src/ticketing/events/sales_segments/__init__.py
+++ ticketing/src/ticketing/events/sales_segments/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def includeme(config):
-    config.add_route('sales_segments.index', '/{event_id}/sales_segments')
+    config.add_route('sales_segments.index', '/{event_id}')
     config.add_route('sales_segments.show', '/show/{sales_segment_id}')
     config.add_route('sales_segments.new', '/new')
     config.add_route('sales_segments.edit', '/edit/{sales_segment_id}')
