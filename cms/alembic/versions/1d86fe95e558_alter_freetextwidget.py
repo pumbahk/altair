9dd0f42c657c8d918c38c6b89131554e00c4b1b8
podhmo <podhmo+altair@beproud.jp>
2012-06-07 12:40:09 +0900

    freetext widget unicode -> unicode text

---
+++ cms/alembic/versions/1d86fe95e558_alter_freetextwidget.py
@@ -0,0 +1,23 @@
+"""alter freetextwidget text
+
+Revision ID: 1d86fe95e558
+Revises: 126c50f99cd7
+Create Date: 2012-06-07 10:15:37.715396
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1d86fe95e558'
+down_revision = '126c50f99cd7'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column("widget_text", "text", type_=sa.Unicode(length=255), existing_type=sa.UnicodeText)
+
+
+def downgrade():
+    op.alter_column("widget_text", "text", existing_type=sa.Unicode(length=255), type_=sa.UnicodeText)
+
