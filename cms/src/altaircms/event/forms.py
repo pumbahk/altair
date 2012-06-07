89eecd79383ee9db96cdb592b8053ebdfdbae2fd
podhmo <podhmo+altair@beproud.jp>
2012-06-07 16:43:22 +0900

    eentのbackend_idも守るようにした

--- cms/src/altaircms/event/forms.py
+++ cms/src/altaircms/event/forms.py
@@ -2,7 +2,7 @@
 from wtforms import fields, validators
 from wtforms.form import Form
 from altaircms.helpers.formhelpers import required_field, append_errors
-
+from .models import Event
 
 class EventForm(Form):
     title = fields.TextField(label=u'タイトル', validators=[required_field()])
@@ -23,6 +23,15 @@ class EventForm(Form):
             append_errors(self.errors, "event_open", u"イベント終了日よりも後に設定されてます")
         if data["deal_open"] > data["deal_close"]:
             append_errors(self.errors, "deal_open", u"販売終了日よりも後に設定されてます")
+        return not bool(self.errors)
+
+    def object_validate(self, obj=None):
+        data = self.data
+        qs = Event.query.filter(Event.backend_id == data["backend_id"])
+        if obj:
+            qs = qs.filter(Event.backend_id != obj.backend_id)
+        if qs.count() >= 1:
+            append_errors(self.errors, "backend_id", u"バックエンドIDが重複しています。(%s)" % data["backend_id"])
         return not bool(self.errors)
 
     __display_fields__ = [u"title", u"subtitle", 
