588dd009768fcbe0cbc169ab5c1ce60739277c45
Moriyoshi Koizumi <mozo@mozo.jp>
2012-06-13 10:37:11 +0900

    Using bulk insert to speed up x20.

--- ticketing/misc/seed/fixture.py
+++ ticketing/misc/seed/fixture.py
@@ -256,43 +256,98 @@ class DataWalker(object):
                             self(_data)
         return data
 
+class InsertStmtBuilder(object):
+    def __init__(self, builder):
+        self.builder = builder
+        self.prev_table = None
+        self.prev_keys = u''
+        self.nbytes_sent = 0
+
+    def flush(self):
+        if self.prev_table is not None:
+            self.write(";\n");
+        self.prev_table = None
+        self.prev_keys = u''
+        self.nbytes_sent = 0
+
+    def write(self, str):
+        self.builder.out.write(str)
+        self.nbytes_sent += len(str)
+
+    def __call__(self, table, _values):
+        encoding = self.builder.encoding
+        values = []
+        keys = u''
+        value_len = 0
+        for k, v in _values:
+            v_ = self.builder.put_scalar(v)
+            keys += k
+            values.append((k, v_))
+            value_len += len(v_)
+
+        if self.nbytes_sent >= 131072:
+            self.flush() 
+
+        if self.prev_table != table or self.prev_keys != keys:
+            self.flush()
+            self.write("INSERT INTO %s (%s) VALUES\n" % (
+                table.encode(encoding),
+                ', '.join(k.encode(encoding) for k, v in values)))
+        else:
+            self.write(",\n")
+
+        if value_len < 1024:
+            self.write("(" + ", ".join(v for _, v in values) + ")")
+        else:
+            self.write("(")
+            first = True
+            for _, v in values:
+                if not first:
+                    self.write(", ")
+                self.write(v)
+                first = False
+            self.write(")")
+        self.prev_table = table
+        self.prev_keys = keys
+
 class SQLBuilder(object):
     def __init__(self, out, encoding=None):
         self.out = out
         self.encoding = encoding or self.out.encoding
+        self.last_stmt = None
 
     def __del__(self):
         self.flush()
 
     def put_scalar(self, scalar):
-        if isinstance(scalar, basestring):
-            return u"'%s'" % scalar.replace(u"'", u"''")
+        if isinstance(scalar, str):
+            return "'%s'" % scalar.replace("'", "''")
+        elif isinstance(scalar, unicode):
+            return "'%s'" % scalar.replace(u"'", u"''").encode(self.encoding)
         elif isinstance(scalar, datetime.datetime):
-            return u"'%s'" % scalar.strftime("%Y-%m-%d %H:%M:%S")
+            return "'%s'" % scalar.strftime("%Y-%m-%d %H:%M:%S")
         elif isinstance(scalar, datetime.date):
-            return u"'%s'" % scalar.strftime("%Y-%m-%d")
+            return "'%s'" % scalar.strftime("%Y-%m-%d")
         elif isinstance(scalar, datetime.time):
-            return u"'%s'" % scalar.strftime("%H:%M:%S")
+            return "'%s'" % scalar.strftime("%H:%M:%S")
         elif isinstance(scalar, (float, int, long)):
-            return u'%d' % scalar
+            return '%d' % scalar
         elif scalar is None:
-            return u'NULL'
+            return 'NULL'
         else:
             raise Exception("Unsupported type: " + type(scalar).__name__)
 
     def insert(self, table, values):
-        values = list(values)
-        self.out.write("INSERT INTO %s (%s) VALUES (" % (table.encode(self.encoding), ', '.join(k.encode(self.encoding) for k, v in values)))
-        first = True
-        for k, v in values:
-            if not first:
-                self.out.write(", ")
-            self.out.write(self.put_scalar(v).encode(self.encoding))
-            first = False
-        self.out.write(");\n");
+        if not isinstance(self.last_stmt, InsertStmtBuilder):
+            if self.last_stmt is not None:
+                self.last_stmt.flush()
+            self.last_stmt = InsertStmtBuilder(self)
+        self.last_stmt(table, values)
 
     def flush(self):
-        pass
+        if self.last_stmt is not None:
+            self.last_stmt.flush()
+        self.last_stmt = None
          
 class SQLSerializer(object):
     logger = logging.getLogger('fixture.SQLSerializer')
