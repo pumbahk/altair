66f938efd4084b123f54eac06d72e4e970e1e9fe
podhmo <podhmo+altair@beproud.jp>
2012-06-20 15:17:23 +0900

    fix branching migration

--- cms/alembic/versions/3375593899c0_add_event_notice_fie.py
+++ cms/alembic/versions/3375593899c0_add_event_notice_fie.py
@@ -1,14 +1,14 @@
 """add event.notice field
 
 Revision ID: 3375593899c0
-Revises: 3dc2ec8e7c1d
+Revises: 560ea70c16f0
 Create Date: 2012-06-18 09:34:30.624969
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '3375593899c0'
-down_revision = '3dc2ec8e7c1d'
+down_revision = '560ea70c16f0'
 
 from alembic import op
 import sqlalchemy as sa
