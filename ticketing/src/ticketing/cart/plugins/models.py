25431e1e3947da71c2ece9106bb5f8e3bce95eef
Atsushi Odagiri <aodagx@gmail.com>
2012-06-21 22:04:54 -0700

    窓口支払いプラグイン

--- ticketing/src/ticketing/cart/plugins/models.py
+++ ticketing/src/ticketing/cart/plugins/models.py
@@ -11,3 +11,11 @@ class ReservedNumber(Base):
     id = sa.Column(sa.Integer, primary_key=True)
     order_no = sa.Column(sa.Unicode(255), unique=True)
     number = sa.Column(sa.Unicode(32))
+
+
+class PaymentReservedNumber(Base):
+    __tablename__ = "payment_reserved_number"
+    query = DBSession.query_property()
+    id = sa.Column(sa.Integer, primary_key=True)
+    order_no = sa.Column(sa.Unicode(255), unique=True)
+    number = sa.Column(sa.Unicode(32))
