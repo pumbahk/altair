2bacdc8fa4cffbb98cdbbc66f5d87df469cfb091
Atsushi Odagiri <aodagx@gmail.com>
2012-06-27 02:47:08 -0700

    89er フォーム入力からカート生成

--- ticketing/src/ticketing/bj89er/resources.py
+++ ticketing/src/ticketing/bj89er/resources.py
@@ -4,3 +4,4 @@ class Bj89erCartResource(TicketingCartResrouce):
     def __init__(self, request):
         super(Bj89erCartResource, self).__init__(request)
         self.event_id = request.registry.settings['89er.event_id']
+        self.performance_id = request.registry.settings['89er.performance_id']
