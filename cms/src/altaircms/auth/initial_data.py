4b0bd0117b7b3a425d789f396eece569b55eb146
podhmo <podhmo+altair@beproud.jp>
2012-07-03 18:18:52 +0900

    test_auth.pyはtest_views.pyで行っているテストとかぶっているので削除

--- cms/src/altaircms/auth/initial_data.py
+++ cms/src/altaircms/auth/initial_data.py
@@ -13,20 +13,20 @@ from altaircms.auth.models import Role, DEFAULT_ROLE, PERMISSIONS
 #         PERMISSIONS.append('%s_%s' % (t, act))
 # 
 
-def insert_initial_authdata():
-    """
-    ロールとパーミッションを登録する
-    """
+# def insert_initial_authdata():
+#     """
+#     ロールとパーミッションを登録する
+#     """
 
-    DBSession = sqlahelper.get_session()
+#     DBSession = sqlahelper.get_session()
 
-    # administrator
-    role = Role(name=DEFAULT_ROLE, permissions=PERMISSIONS)
-    DBSession.add(role)
+#     # administrator
+#     role = Role(name=DEFAULT_ROLE, permissions=PERMISSIONS)
+#     DBSession.add(role)
 
-    perms = ["_create", "_read", "_delete", "_update"]
-    for target in ['event', 'topic', 'ticket', 'magazine', 'asset', 'page', 'tag', 'layout', 'operator']:
-        # viewer
-        role = Role(name=target + "_viewer", permissions=[target + "_read"])
-        # editor
-        role = Role(name=target + "_editor", permissions=[(target + perm) for perm in perms])
+#     perms = ["_create", "_read", "_delete", "_update"]
+#     for target in ['event', 'topic', 'ticket', 'magazine', 'asset', 'page', 'tag', 'layout', 'operator']:
+#         # viewer
+#         role = Role(name=target + "_viewer", permissions=[target + "_read"])
+#         # editor
+#         role = Role(name=target + "_editor", permissions=[(target + perm) for perm in perms])
