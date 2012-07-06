b7571d636037ab94669b049a68ae47f77be3c633
Misato <misato@takahashi.name>
2012-07-06 16:18:35 +0900

    rename 89er -> 89ers

--- ticketing/src/ticketing/bj89ers/resources.py
+++ ticketing/src/ticketing/bj89ers/resources.py
@@ -9,8 +9,8 @@ MEMBERSHIP_NAME = '89er'
 class Bj89erCartResource(TicketingCartResource):
     def __init__(self, request):
         super(Bj89erCartResource, self).__init__(request)
-        self.event_id = request.registry.settings['89er.event_id']
-        self.performance_id = request.registry.settings['89er.performance_id']
+        self.event_id = request.registry.settings['89ers.event_id']
+        self.performance_id = request.registry.settings['89ers.performance_id']
 
     def get_or_create_user(self):
         from ticketing.cart import api
