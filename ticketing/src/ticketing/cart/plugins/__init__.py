044db9dcf5ddab55ca1dbad011ba5487c895aae4
Moriyoshi Koizumi <mozo@mozo.jp>
2012-07-10 22:59:45 +0900

    カートプラグイン用 static view

--- ticketing/src/ticketing/cart/plugins/__init__.py
+++ ticketing/src/ticketing/cart/plugins/__init__.py
@@ -11,6 +11,7 @@ def includeme(config):
     config.add_directive("add_payment_plugin", ".directives.add_payment_plugin")
     config.add_directive("add_delivery_plugin", ".directives.add_delivery_plugin")
     config.add_directive("add_payment_delivery_plugin", ".directives.add_payment_delivery_plugin")
+    config.add_static_view('static', 'ticketing.cart.plugins:static', cache_max_age=3600)
 
     config.include(".multicheckout")
     config.include(".reservednumber")
