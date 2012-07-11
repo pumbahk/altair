af852f0ccf78494e2b84e19c84da285be4ce6828
Moriyoshi Koizumi <mozo@mozo.jp>
2012-07-12 08:14:57 +0900

    素の Exception は exception view で拾うことを想定していない模様

--- ticketing/src/ticketing/bj89ers/__init__.py
+++ ticketing/src/ticketing/bj89ers/__init__.py
@@ -69,8 +69,8 @@ def main(global_conf, **settings):
     config.add_view('.views.contact_view', route_name="contact", renderer="static_mobile/contact.html", request_type='ticketing.cart.interfaces.IMobileRequest')
     config.add_view('.views.notfound_view', context=HTTPNotFound, renderer="errors/not_fount.html", )
     config.add_view('.views.notfound_view', context=HTTPNotFound,  renderer="errors_mobile/not_fount.html", request_type='ticketing.cart.interfaces.IMobileRequest')
-    config.add_view('.views.exception_view',  context=Exception, renderer="errors/error.html")
-    config.add_view('.views.exception_view', context=Exception,  renderer="errors_mobile/error.html", request_type='ticketing.cart.interfaces.IMobileRequest')
+    config.add_view('.views.exception_view',  context=StandardError, renderer="errors/error.html")
+    config.add_view('.views.exception_view', context=StandardError,  renderer="errors_mobile/error.html", request_type='ticketing.cart.interfaces.IMobileRequest')
 
     # @view_config()
 
