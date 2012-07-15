a3df8ed7517aed46971b7fc6a5c48139df27da0d
podhmo <podhmo+altair@beproud.jp>
2012-07-15 18:09:00 +0900

    usersiteのerror page, forbidden pageをiniで設定できるように変更

---
+++ cms/src/altaircms/templates/front/errors/89ers/forbidden.py
@@ -0,0 +1,46 @@
+## kadomaru
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="jp">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="/static/89ers/css/style.css" media="all" />
+	<title>89ers</title>
+</head>
+<body>
+	<!-- wrapper -->	
+	<div class="wrapper">
+		<!-- メインカラム -->
+		<div class="maincol">
+			<header>
+				<div class="gnavi">
+					<ul>
+						<li><a href="#">チケット購入・引取方法</a></li>
+						<li><a href="#">入場方法</a></li>
+						<li><a href="#">ファンクラブ申し込みフォーム</a></li>
+						<li><a href="#">よくある質問</a></li>
+					</ul>　　
+				</div>
+			</header>
+
+			<div class="kadomaru">
+ページの閲覧は禁止されています。
+			</div>
+			<!-- kadomaru終わり -->
+
+
+
+		</div>
+		<!-- メインカラムおわり -->
+		<!-- サイドバー -->
+		<div class="sidebar">
+			
+			
+			
+		</div>	
+		<!-- サイドバーおわり -->
+		
+	</div>
+	<!-- wrapperおわり -->
+</body>
+</html>
