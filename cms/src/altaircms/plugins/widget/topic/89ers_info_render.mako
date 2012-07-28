58d9fa0821da9bce2a52856e63eb6c5a235a5004
podhmo <podhmo+altair@beproud.jp>
2012-07-28 16:24:16 +0900

    購入方法用のデザイン追加

---
+++ cms/src/altaircms/plugins/widget/topic/89ers_info_render.mako
@@ -0,0 +1,11 @@
+<% import itertools %>
+<table class="info">
+	<tr>
+		<td>
+		  %for i, topic in itertools.izip(itertools.count(1),topics):
+		    <h4><span class="order">${i}.</span>${topic.title|n}</h4>
+			${topic.text|n}
+		  %endfor
+		</td>
+	</tr>
+</table>
--- cms/src/altaircms/plugins/widget/topic/models.py
+++ cms/src/altaircms/plugins/widget/topic/models.py
@@ -101,6 +101,10 @@ MERGE_SETTINGS_DISPATH = {
         topics_merge_settings, 
         "altaircms.plugins.widget:topic/89ers_faq_render.mako"
         ), 
+    ("noimage", u"89ers取引方法詳細"): functools.partial(
+        topics_merge_settings, 
+        "altaircms.plugins.widget:topic/89ers_info_render.mako"
+        ), 
     ("noimage", u"トピックス"): functools.partial(
         topics_merge_settings, 
         "altaircms.plugins.widget:topic/topic_render.mako"), 
