3b3b524f6d8e1bf3ee7e13159c2e3f8da8538009
podhmo <podhmo+altair@beproud.jp>
2012-07-30 13:24:56 +0900

    とても途中

--- cms/src/altaircms/scripts/misc/89ers.fixtures.py
+++ cms/src/altaircms/scripts/misc/89ers.fixtures.py
@@ -53,27 +53,43 @@ class Bj89ersFixtureBuilder(FixtureBuilder):
     def __init__(self, Datum):
         """
         layout_triples: (title, template_filename, blocks)
-        page_triples: (name, url, layout)
+        page_triples: (name, url, layout, structure)
         category_items: (orderno, name, label, hierarchy, page_name)
         """
         super(Bj89ersFixtureBuilder, self).__init__(Datum)
         layout_triples = [
-            (u'89ersシンプル', '89ers.base.mako', '[["header"], ["kadomaru"]]'), 
-            (u'89ersチケットトップ', '89ers.complex.mako', '[["above_kadomaru"],["kadomaru"],["below_kadomaru"]]'), 
-            (u'89ers.introduction','89ers.introduction.mako', '[["header"], ["kadomaru"], ["card_and_QR"],["card_and_seven"],["card_and_home"],["card_and_onsite"]]'),
+            (u'89ersシンプル', '89ers.base.mako', '[["header"], ["kadomaru"]]'),
+            (u'89ersチケットトップ', '89ers.complex.mako', '[["above_kadomaru"],["kadomaru"],["below_kadomaru"]]'),
+            (u'89ers.introduction', '89ers.introduction.mako', '[["above_kadomaru"],["card_and_QR"],["card_and_seven"],["card_and_home"],["anshin_and_QR"],["anshin_and_seven"],["anshin_and_home"],["seven_and_seven"]]'),
+            (u'89ers:kadomaru2', '89ers.kadomaru2.mako', '[["above_kadomaru"],["kadomaru"],["kadomaru2"],["below_kadomaru"]]'),
+            (u'89ers:kadomaru3', '89ers.kadomaru3.mako', '[["above_kadomaru"],["kadomaru"],["kadomaru2"],["kadomaru3"],["below_kadomaru"]]'),
+            (u'89ers:kadomaru4', '89ers.kadomaru4.mako', '[["above_kadomaru"],["kadomaru"],["kadomaru2"],["kadomaru3"],["kadomaru4"],["below_kadomaru"]]'),
+            (u'89ers:kadomaru5', '89ers.kadomaru5.mako', '[["above_kadomaru"],["kadomaru"],["kadomaru2"],["kadomaru3"],["kadomaru4"],["kadomaru5"],["below_kadomaru"]]')
             ]
         self.layout_triples = layout_triples
 
+        ## structureひどい.
         page_triples = [
-            (u"89ers:チケットトップ", "tickets/top", u"89ersチケットトップ"),
-            (u"89ers:チケット購入・引き取り方法", "purcharsed/credit", u"89ers.introduction"),
-            (u"89ers:ブースタークラブ申込", "purcharsed/seven", u"89ersシンプル"),
-            (u"89ers:よくある質問", "faq", u"89ersシンプル"),
+            (u'89ers:チケットトップ', 'top', u'89ersチケットトップ','{"above_kadomaru": [], "kadomaru": [{"pk": null, "name": "image"}], "below_kadomaru": [{"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}]}'),
+            (u'89ers:チケット購入・引き取り方法', "howto", u"89ers.introduction", '{"seven_and_seven": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "anshin_and_seven": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "above_kadomaru": [{"pk": null, "name": "heading"}], "card_and_seven": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "card_and_home": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "anshin_and_QR": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "anshin_and_home": [{"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "card_and_QR": [{"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}]}'),
+            (u'89ers:ブースタークラブ申込', 'purcharsed/seven', u'89ersシンプル', '{}'),
+            (u'89ers:よくある質問', 'faq', u'89ersシンプル', '{"header": [], "kadomaru": [{"pk": null, "name": "heading"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "topic"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "topic"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "topic"}]}'),
+            (u'89ers:価格座席図', "price", u"89ers:kadomaru2", '{"above_kadomaru": [{"pk": null, "name": "heading"}], "kadomaru": [{"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}], "below_kadomaru": [], "kadomaru2": [{"pk": null, "name": "heading"}, {"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}]}'), 
+            (u'89ers:残席情報', "unocurppied", u"89ersチケットトップ", '{"above_kadomaru": [{"pk": null, "name": "heading"}], "kadomaru": [{"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "below_kadomaru": []}'), 
+            (u'89ers:チケット取り扱い数量', "playguide", u"89ersシンプル", '{}'), 
+            (u'89ers:チケット購入', "purchase", u"89ers:kadomaru4", '{"above_kadomaru": [{"pk": null, "name": "heading"}], "kadomaru4": [{"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}], "kadomaru3": [{"pk": null, "name": "heading"}, {"pk": null, "name": "image"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "freetext"}], "kadomaru2": [{"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}], "kadomaru": [{"pk": null, "name": "freetext"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}, {"pk": null, "name": "heading"}, {"pk": null, "name": "freetext"}], "below_kadomaru": []}')]
             ]
         self.page_triples = page_triples
 
         category_items = [
             (1, "top", u"チケットTOP", "header_menu", u"89ers:チケットトップ", ), 
+            (2, "howto", u"購入・引き取り方法", "header_menu", u"", ), 
+            (3, "purchase", u"チケット購入", "header_menu", u"", ), 
+            (3, "faq", u"FAQ", "header_menu", u"", ), 
+            (4, "price", u"価格・座席図", "header_menu", u"", ), 
+            (5, "unoccupied", u"残席情報", "header_menu", u"", ), 
+            (6, "playguide", u"チケット取扱数量", "header_menu", u"", ), 
+
             (2, "order_way",  u"チケット購入・引き取り方法", "header_menu", u"89ers:チケット購入・引き取り方法"), 
             (3, "register_club",  u"ブースタークラブ申込", "header_menu", u"89ers:ブースタークラブ申込"), 
             (4, "faq",  u"よくある質問", "header_menu", u"89ers:よくある質問"), 
