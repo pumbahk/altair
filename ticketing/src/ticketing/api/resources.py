cba1d9903edf813ed9165543b79a49aee2d09c3e
podhmo <podhmo+altair@beproud.jp>
2012-08-02 18:05:18 +0900

    API認証のcontextがおかしかったので修正

--- ticketing/src/ticketing/api/resources.py
+++ ticketing/src/ticketing/api/resources.py
@@ -1,17 +1,24 @@
 from zope.interface import implementer
 from ..interfaces import IAPIContext
 from ..authentication.apikey.api import get_apikeyentry
+from pyramid.interfaces import IRootFactory
 
 @implementer(IAPIContext)
 class APIContext(object):
     __name__ = 'api'
 
-    @property
-    def __parent__(self):
-        return self.request.config.root_factory(self.request)
+    # @property
+    # def __parent__(self):
+    #     reg = self.request.registry
+    #     return reg.queryUtility(IRootFactory)(self.request)
 
     def __init__(self, request):
         self.request = request
         # assign the APIKeyEntry object if the API request is being made
         self.apikeyentry = get_apikeyentry(request)
 
+
+        reg = self.request.registry
+        parent = reg.queryUtility(IRootFactory)(self.request)
+        if parent and hasattr(parent, "__acl__"):
+            self.__acl__ = parent.__acl__
