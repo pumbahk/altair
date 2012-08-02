ab7f8f13ae6b75e0aad0a43d5ff39c38856ea8be
Atsushi Odagiri <aodagx@gmail.com>
2012-08-02 04:31:13 -0700

    サブクエリの論理削除条件に対応

--- ticketing/src/ticketing/logicaldeleting.py
+++ ticketing/src/ticketing/logicaldeleting.py
@@ -15,7 +15,8 @@ def install():
 class LogicalDeletableSession(orm.Session):
     def query(self, *args, **kwargs):
         q = super(LogicalDeletableSession, self).query(*args, **kwargs)
-        return q.options(LogicalDeletingOption("deleted_at"))
+        option = LogicalDeletingOption("deleted_at")
+        return option._process(q)
 
 class LogicalDeletingOption(MapperOption):
     def __init__(self, attr_name):
@@ -31,9 +32,15 @@ class LogicalDeletingOption(MapperOption):
     def _process(self, query):
         
         _query = query
+        assert query._entities
         for e in  query._entities:
             if hasattr(e.type, self.attr_name):
-                query = query.filter(getattr(e.type, "deleted_at")==None)
+                query = query.filter(getattr(e.type, self.attr_name)==None)
+            elif hasattr(e, 'actual_froms'):
+                for t in e.actual_froms:
+                    if hasattr(t.c, self.attr_name):
+                        query = query.filter(getattr(t.c, self.attr_name)==None)
         _query._criterion = query._criterion
         _query._enable_assertions = False
+        return _query
 
