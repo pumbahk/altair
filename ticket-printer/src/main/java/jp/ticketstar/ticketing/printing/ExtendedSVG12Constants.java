a73d8d28fe586b220e24e33f05b87ac7a9ef6ffd
Moriyoshi Koizumi <mozo@mozo.jp>
2012-08-05 15:03:33 +0900

    ファイルの削除忘れ

--- ticket-printer/src/main/java/jp/ticketstar/ticketing/printing/ExtendedSVG12Constants.java
+++
@@ -1,6 +0,0 @@
-package jp.ticketstar.ticketing.printing;
-
-public interface ExtendedSVG12Constants {
-	public static final String SVG_PAGESET_TAG = "pageSet";
-	public static final String SVG_PAGE_TAG = "page";
-}
