d09468bc292903f555c5d522680327d97fc8f317
Shinya Okano <shinya.okano@beproud.jp>
2012-08-07 10:59:22 +0900

    テストコード追加

--- ticketing/src/ticketing/events/reports/sheet_tests.py
+++ ticketing/src/ticketing/events/reports/sheet_tests.py
@@ -83,3 +83,62 @@ class SeatRecordsFromSeatSources(TestCase):
         self.assertEqual(result[2].start, "A1")
         self.assertEqual(result[2].end, "A1")
         self.assertEqual(result[2].quantity, 1)
+
+
+class SeatRecordsFromSeatSourcesUnsold(TestCase):
+    def setUp(self):
+        self.seat_sources = []
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A1",
+            status=SeatStatusEnum.Vacant.v,
+        ))
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A2",
+            status=SeatStatusEnum.Vacant.v,
+        ))
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A3",
+            status=SeatStatusEnum.Vacant.v,
+        ))
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A4",
+            status=SeatStatusEnum.NotOnSale.v,
+        ))
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A5",
+            status=SeatStatusEnum.Vacant.v,
+        ))
+        self.seat_sources.append(sheet.SeatSource(
+            block=u"テストブロック",
+            floor=u"1階",
+            line=u"あ",
+            seat=u"A6",
+            status=SeatStatusEnum.Vacant.v,
+        ))
+
+    def test_ok(self):
+        result = sheet.seat_records_from_seat_sources_unsold(self.seat_sources)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].line, u"あ")
+        self.assertEqual(result[0].start, "A1")
+        self.assertEqual(result[0].end, "A3")
+        self.assertEqual(result[0].quantity, 3)
+        self.assertEqual(result[1].line, u"あ")
+        self.assertEqual(result[1].start, "A5")
+        self.assertEqual(result[1].end, "A6")
+        self.assertEqual(result[1].quantity, 2)
