a7390adaec717f8e56909f314804010c4a24329f
mmatsui <masahiro.matsui@gmail.com>
2012-08-16 00:58:01 +0900

    mypageのimportパス修正、配券先のページング件数修正

--- ticketing/src/ticketing/events/stock_holders/views.py
+++ ticketing/src/ticketing/events/stock_holders/views.py
@@ -35,7 +35,7 @@ class StockHolders(BaseView):
         stock_holders = paginate.Page(
             query,
             page=int(self.request.params.get('page', 0)),
-            items_per_page=2,
+            items_per_page=20,
             url=paginate.PageURL_WebOb(self.request)
         )
 
