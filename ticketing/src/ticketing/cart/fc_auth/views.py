fd1aa35b8900a76baf8069a0c0e33f1b556ccbe7
Atsushi Odagiri <aodagx@gmail.com>
2012-08-16 18:05:41 -0700

    garden

--- ticketing/src/ticketing/cart/fc_auth/views.py
+++ ticketing/src/ticketing/cart/fc_auth/views.py
@@ -2,13 +2,20 @@ from repoze.who.api import get_api as get_who_api
 from pyramid.view import view_config
 
 class LoginView(object):
+    renderer_tmpl = "ticketing.cart:templates/fclogin/{membership}/login.html"
+
     def __init__(self, request):
         self.request = request
 
 
+    def select_renderer(self, membership):
+        self.request.override_renderer = self.renderer_tmpl.format(membership=membership)
+
     @view_config(request_method="GET")
     def login_form(self):
-        pass
+        membership = self.context.membership
+        self.select_renderer(membership)
+        return dict()
 
 
     @view_config(request_method="POST")
@@ -28,6 +35,8 @@ class LoginView(object):
         return_to_url = self.return_to_url
         res = HTTPFound(location=return_to_url)
         res.header_list.extend(headers)
+
+        self.select_renderer(membership)
 
         return res
 
