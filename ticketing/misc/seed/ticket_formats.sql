c1878b377648cb31dd9719dd587ce422edea4021
Moriyoshi Koizumi <mozo@mozo.jp>
2012-08-22 10:10:20 +0900

    seed データ修正

--- ticketing/misc/seed/ticket_formats.sql
+++ ticketing/misc/seed/ticket_formats.sql
@@ -15,16 +15,30 @@ INSERT INTO TicketFormat(id, organization_id, name, data) VALUES (1, 1,
 }
 ');
 INSERT INTO TicketFormat(id, organization_id, name, data) VALUES (2, 1,
-'セブン-イレブン新式',
+'セブン-イレブン旧式',
 '{
   "size": { "width": "177.8mm", "height": "58mm" },
   "perforations": {
-    "vertical": [ "29.8mm", "140.8mm" ],
+    "vertical": [ "28.2mm", "139.3mm" ],
     "horizontal": []
   },
   "printable_areas": [
-    { "x": "32.8mm", "y": "3mm", "width": "104mm", "height": "52mm" },
+    { "x": "43.8mm", "y": "3mm", "width": "104mm", "height": "52mm" },
     { "x": "144.8mm", "y": "3mm", "width": "30mm", "height": "52mm" }
+  ]
+}
+');
+INSERT INTO TicketFormat(id, organization_id, name, data) VALUES (3, 1,
+'セブン-イレブン新式',
+'{
+  "size": { "width": "165.1mm", "height": "76mm" },
+  "perforations": {
+    "vertical": [ "128.1mm" ],
+    "horizontal": []
+  },
+  "printable_areas": [
+    { "x": "31.1mm", "y": "6.5mm", "width": "93mm", "height": "63mm" },
+    { "x": "132.1mm", "y": "6.5mm", "width": "30mm", "height": "63mm" }
   ]
 }
 ');
