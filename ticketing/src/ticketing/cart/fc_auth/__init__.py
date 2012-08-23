44abecf34251e19ada35b37ed4809cb755aa6fe0
Atsushi Odagiri <aodagx@gmail.com>
2012-08-23 00:13:30 -0700

    非会員ログインURL

--- ticketing/src/ticketing/cart/fc_auth/__init__.py
+++ ticketing/src/ticketing/cart/fc_auth/__init__.py
@@ -3,6 +3,8 @@
 def includeme(config):
     config.add_route('fc_auth.login', '/fc/{membership}/login',
         factory='.resources.FCAuthResource')
+    config.add_route('fc_auth.guest', '/fc/{membership}/guest',
+        factory='.resources.FCAuthResource')
 
     config.add_tween('.tweens.FCAuthTween')
     config.add_static_view('fc_static', 'ticketing.cart.fc_auth:static')
