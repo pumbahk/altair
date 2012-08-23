5de2a46fae09f6b45d2780b2ffa07d3a568f4320
podhmo <podhmo+altair@beproud.jp>
2012-08-23 10:06:01 +0900

    someone forget it

--- ticketing/src/ticketing/cart/errors/views.py
+++ ticketing/src/ticketing/cart/errors/views.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from pyramid.view import view_config
-from pyramid.exceptions import NotFound, Forbidden
+from pyramid.exceptions import NotFound, Forbidden, HTTPFound
 from ..exceptions import *
 import logging
 
