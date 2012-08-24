caaa4dcb5506338ec9c4b81d2cd2663cd2328048
Shinya Okano <shinya.okano@beproud.jp>
2012-08-24 16:15:25 +0900

    モバイルのカートの購入枚数制限

--- ticketing/src/ticketing/cart/errors/views.py
+++ ticketing/src/ticketing/cart/errors/views.py
@@ -29,3 +29,19 @@ def handle_noeventerror(request):
     request.response.status = 404
     return {}
 
+@view_config(context=InvalidCSRFTokenException, renderer='ticketing.cart:templates/errors/forbidden.html')
+def csrf(request):
+    logger.error(request.context, exc_info=request.exc_info)
+    request.response.status = 403
+    return {}
+
+class OverQuantityLimitErrorView(object):
+    def __init__(self, context, request):
+        self.request = request
+        self.context = context
+
+    @view_config(context=OverQuantityLimitError, renderer='ticketing.cart:templates/carts_mobile/error.html', request_type="..interfaces.IMobileRequest")
+    def __call__(self):
+        logger.error(self.context, exc_info=self.request.exc_info)
+        upper_limit = self.context.upper_limit
+        return dict(message=u"枚数は合計%d枚以内で選択してください" % upper_limit)
