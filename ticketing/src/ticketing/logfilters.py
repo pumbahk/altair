b3a08f5ce4a69d20603aebe5dfdda330e81ba4c7
Atsushi Odagiri <aodag@aodag-Aspire-S3-391.(none)>
2012-08-28 13:04:57 +0900

    added browserid field to logrecord.

---
+++ ticketing/src/ticketing/logfilters.py
@@ -0,0 +1,17 @@
+import logging
+from pyramid.threadlocal import get_current_request
+
+class BrowserIdHandler(logging.StreamHandler):
+    def __init__(self, *args, **kwargs):
+        super(BrowserIdHandler, self).__init__(*args, **kwargs)
+        self.addFilter(BrowserIdFilter())
+
+class BrowserIdFilter(logging.Filter):
+    def filter(self, record):
+        record.browserid = None
+        request = get_current_request()
+        if request is None:
+            return True
+
+        record.browserid = request.environ.get('repoze.browserid')
+        return True
