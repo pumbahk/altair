06a182f6b0f437f18a48c5e81c7612e83647959a
podhmo <podhmo+altair@beproud.jp>
2012-08-30 19:16:18 +0900

    alembic

--- ticketing/alembic/versions/362dc10402f5_create_extra_mailinf.py
+++ ticketing/alembic/versions/362dc10402f5_create_extra_mailinf.py
@@ -1,14 +1,14 @@
 """create extra mailinfo
 
 Revision ID: 362dc10402f5
-Revises: 406252f94050
+Revises: 408a36838f2e
 Create Date: 2012-08-22 19:48:02.905148
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '362dc10402f5'
-down_revision = '406252f94050'
+down_revision = '408a36838f2e'
 
 from alembic import op
 import sqlalchemy as sa
@@ -21,11 +21,13 @@ def upgrade():
     op.create_table('ExtraMailInfo',
                     sa.Column('id', Identifier, nullable=False),
                     sa.Column('organization_id', Identifier, nullable=True),
+                    sa.Column('event_id', Identifier, nullable=True),
                     sa.Column('data', sa.Unicode(65536), nullable=True),
                     sa.Column('created_at', sa.TIMESTAMP(), server_default=sqlf.current_timestamp(), nullable=False),
                     sa.Column('updated_at', sa.TIMESTAMP(), server_default=text('0'), nullable=False),
                     sa.Column('deleted_at', sa.TIMESTAMP(), nullable=True),
                     sa.ForeignKeyConstraint(['organization_id'], ['Organization.id'], name="extramailinfo_ibfk_1"),
+                    sa.ForeignKeyConstraint(['event_id'], ['Event.id'], name="extramailinfo_ibfk_2"),
                     sa.PrimaryKeyConstraint('id')
                     )
 
