dd5aae2e06962bec0032e403221bb77dd4458b66
podhmo <podhmo+altair@beproud.jp>
2012-08-30 10:10:32 +0900

    注文キャンセルメールの生成を移動

--- ticketing/src/ticketing/cart/sendmail.py
+++ ticketing/src/ticketing/cart/sendmail.py
@@ -3,21 +3,19 @@ from pyramid_mailer import get_mailer
 from . import logger
 from ticketing.mails.api import get_complete_mail
 from ..models import DBSession
-## dummy mailer
 
 def on_order_completed(order_completed):
     try:
         order = order_completed.order
         request = order_completed.request
-        message = create_message(request, order)
+
+        complete_mail = get_complete_mail(request)
+        message = complete_mail.build_message(order)
+
         mailer = get_mailer(request) ## todo.component化
         mailer.send(message)
-        logger.info("send mail to %s" % message.recipients)
-    except Exception, e:
+        logger.info("send complete mail to %s" % message.recipients)
+    except Exception:
         ## id見れないと困る
         DBSession.flush(order)
         logger.error("*complete mail* send mail is failed. order id: %d" % order.id,  exc_info=1)
-
-def create_message(request, order):
-    complete_mail = get_complete_mail(request)
-    return complete_mail.build_message(order)
--- ticketing/src/ticketing/mails/api.py
+++ ticketing/src/ticketing/mails/api.py
@@ -1,6 +1,10 @@
 from .interfaces import ICompleteMail
 from pyramid.interfaces import IRequest
+import logging
+
+logger = logging.getLogger(__name__)
 
 def get_complete_mail(request):
     cls = request.registry.adapters.lookup([IRequest], ICompleteMail, "")
     return cls(request)
+
