13f3838f970bf403c0602e3aa3f12d5d302340ff
podhmo <podhmo+altair@beproud.jp>
2012-09-07 10:43:01 +0900

    rename

--- ticketing/src/ticketing/orderreview/__init__.py
+++ ticketing/src/ticketing/orderreview/__init__.py
@@ -18,6 +18,7 @@ def main(global_conf, **settings):
     config.add_renderer('.html' , 'pyramid.mako_templating.renderer_factory')
     config.add_renderer('.txt' , 'pyramid.mako_templating.renderer_factory')
     config.add_static_view('static', 'ticketing.bj89ers:static', cache_max_age=3600)
+    config.add_static_view('static_', 'ticketing.cart:static', cache_max_age=3600)
     config.add_static_view('img', 'ticketing.cart:static', cache_max_age=3600)
     config.include('ticketing.checkout')
     config.include('ticketing.multicheckout')
