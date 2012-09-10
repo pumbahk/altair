6174c7f0f50af8d70cbc25ac4a23e93da3be8a4d
podhmo <podhmo+altair@beproud.jp>
2012-09-10 15:43:02 +0900

    preview表示画面でpayment,delivery methodの変更をしながら結果を見れるようにした

--- ticketing/src/ticketing/mails/views.py
+++ ticketing/src/ticketing/mails/views.py
@@ -2,10 +2,11 @@ from pyramid.view import view_config
 from ticketing.fanstatic import with_bootstrap
 from ticketing.mails.api import get_mail_utility
 from ..core.models import Organization, Event, Performance
+from . import forms
 
 @view_config(route_name="mails.preview.organization", 
              decorator=with_bootstrap, permission="authenticated", 
-             renderer="string")
+             renderer="ticketing:templates/events/mailinfo/preview.html")
 def mail_preview_preorder_with_organization(context, request):
     mutil = get_mail_utility(request, request.matchdict["mailtype"])
     payment_id = request.params["payment_methods"]
@@ -14,11 +15,16 @@ def mail_preview_preorder_with_organization(context, request):
     organization_id = int(request.matchdict.get("organization_id", 0))
     organization = Organization.get(organization_id)
     fake_order = mutil.create_fake_order(request, organization, payment_id, delivery_id)
-    return mutil.preview_text(request, fake_order)
+    form = forms.MailInfoTemplate(request, organization).as_choice_formclass()(
+        payment_methods=payment_id, 
+        delivery_methods=delivery_id, 
+        )
+    return {"preview_text": mutil.preview_text(request, fake_order), 
+            "form": form}
 
 @view_config(route_name="mails.preview.event", 
              decorator=with_bootstrap, permission="authenticated", 
-             renderer="string")
+             renderer="ticketing:templates/events/mailinfo/preview.html")
 def mail_preview_preorder_with_event(context, request):
     mutil = get_mail_utility(request, request.matchdict["mailtype"])
     payment_id = request.params["payment_methods"]
@@ -28,11 +34,17 @@ def mail_preview_preorder_with_event(context, request):
     event = Event.get(event_id)
     fake_order = mutil.create_fake_order(request, event.organization, 
                                          payment_id, delivery_id, event=event)
-    return mutil.preview_text(request, fake_order)
+
+    form = forms.MailInfoTemplate(request, event.organization).as_choice_formclass()(
+        payment_methods=payment_id, 
+        delivery_methods=delivery_id, 
+        )
+    return {"preview_text": mutil.preview_text(request, fake_order), 
+            "form": form}
 
 @view_config(route_name="mails.preview.performance", 
              decorator=with_bootstrap, permission="authenticated", 
-             renderer="string")
+             renderer="ticketing:templates/events/mailinfo/preview.html")
 def mail_preview_preorder_with_performance(context, request):
     mutil = get_mail_utility(request, request.matchdict["mailtype"])
     payment_id = request.params["payment_methods"]
@@ -42,4 +54,9 @@ def mail_preview_preorder_with_performance(context, request):
     performance = Performance.get(performance_id)
     fake_order = mutil.create_fake_order(request, performance.event.organization,
                                          payment_id, delivery_id, performance=performance)
-    return mutil.preview_text(request, fake_order)
+    form = forms.MailInfoTemplate(request, performance.event.organization).as_choice_formclass()(
+        payment_methods=payment_id, 
+        delivery_methods=delivery_id, 
+        )
+    return {"preview_text": mutil.preview_text(request, fake_order), 
+            "form": form}
