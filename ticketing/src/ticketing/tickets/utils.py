58c56cb7526c4bc274a2fd088d02012d7babba60
Moriyoshi Koizumi <mozo@mozo.jp>
2012-09-11 19:59:41 +0900

    shipping_address が空のときに発券できない問題を修正

--- ticketing/src/ticketing/tickets/utils.py
+++ ticketing/src/ticketing/tickets/utils.py
@@ -244,7 +244,7 @@ class DictBuilder(object):
                 u"tel_1": shipping_address.tel_1,
                 u"tel_2": shipping_address.tel_2,
                 u"fax": shipping_address.fax
-                },
+                } if shipping_address else {},
             u'aux': ticket_bundle.attributes if ticket_bundle else {},
             u'券種名': product_item.name or product.name,
             u'商品名': product_item.name or product.name,
