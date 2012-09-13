98d6b8a4ffdd71405b92a2785108e917d4dda97a
Moriyoshi Koizumi <mozo@mozo.jp>
2012-09-13 18:49:03 +0900

    s3 の設定が無くても起動できるように修正

--- ticketing/src/ticketing/pyramid_boto/__init__.py
+++ ticketing/src/ticketing/pyramid_boto/__init__.py
@@ -3,9 +3,7 @@ from .s3 import IS3ConnectionFactory, newDefaultS3ConnectionFactory
 
 def register_default_implementations(config):
     factory = newDefaultS3ConnectionFactory(config)
-    if factory is None:
-        raise ConfigurationError
-    config.registry.registerUtility(
-        factory,
-        IS3ConnectionFactory)
-
+    if factory is not None:
+        config.registry.registerUtility(
+            factory,
+            IS3ConnectionFactory)
