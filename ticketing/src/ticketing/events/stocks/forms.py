5d3701904d03214b95bb859cb1739a1fee13542a
mmatsui <masahiro.matsui@gmail.com>
2012-09-19 18:18:51 +0900

    既に販売済みの席がある場合に数受けの配席を更新すると在庫数がずれるバグを修正

--- ticketing/src/ticketing/events/stocks/forms.py
+++ ticketing/src/ticketing/events/stocks/forms.py
@@ -35,13 +35,20 @@ class AllocateStockForm(Form):
     )
 
     def validate_quantity(form, field):
-        # Seatの割当数と一致すること
         stock = Stock.get(form.id.data)
         if stock and stock.stock_type:
+            # Seatの割当数と一致すること
             if stock.stock_type.is_seat and not stock.stock_type.quantity_only:
                 allocated_seats = Seat.filter_by(stock_id=form.id.data).count()
                 if allocated_seats != int(form.quantity.data):
                     raise ValidationError(u'席種に割り当てられている在庫数合計が一致しません')
+
+            # 数受けの場合は販売済み座席数以上であること
+            if stock.stock_type.is_seat and stock.stock_type.quantity_only:
+                vacant_quantity = stock.count_vacant_quantity()
+                reserved_quantity = stock.quantity - vacant_quantity
+                if reserved_quantity > int(form.quantity.data):
+                    raise ValidationError(u'既に販売済みまたは販売中の席数(%d席)よりも少ない在庫数は入力できません' % reserved_quantity)
 
 class AllocateStockTypeForm(Form):
 
