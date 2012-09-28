c6f9f5626b4ea9e9340eab27b1465d164b8dfe8b
podhmo <podhmo+altair@beproud.jp>
2012-09-28 14:08:00 +0900

    Column 'updated_at' in order clause is ambiguous..

--- cms/src/altaircms/event/views.py
+++ cms/src/altaircms/event/views.py
@@ -82,7 +82,7 @@ def event_list(request):
         search_form = forms.EventSearchForm()
 
     return dict(
-        events=events.order_by(sa.desc("updated_at", "id")), 
+        events=events.order_by(sa.desc("updated_at"), sa.asc("id")), 
         search_form=search_form, 
     )
 
