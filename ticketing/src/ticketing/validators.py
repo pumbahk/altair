bb947eee6e08224971b477ab093afcb048648f23
dev@ticketstar.jp <dev@ticketstar.jp>
2012-10-05 18:03:40 +0900

    メールアドレスの正規表現を修正

--- ticketing/src/ticketing/validators.py
+++ ticketing/src/ticketing/validators.py
@@ -3,7 +3,7 @@ from wtforms import validators as _v
 
 class Email(_v.Regexp):
     def __init__(self, message=None):
-        super(Email, self).__init__(r'^[a-zA-Z_+\-*/=.]+@[^.][a-zA-Z_\-.]*\.[a-z]{2,10}$', re.IGNORECASE, message)
+        super(Email, self).__init__(r'^[a-zA-Z0-9_+\-*/=.]+@[^.][a-zA-Z0-9_\-.]*\.[a-z]{2,10}$', re.IGNORECASE, message)
 
     def __call__(self, form, field):
         if self.message is None:
