8aad05a00e2345120f4b1aaa7a2496cbab947bef
Moriyoshi Koizumi <mozo@mozo.jp>
2012-10-16 10:08:08 +0900

    js.underscore は本家がアップデートされたので、自前のでなくてよくなった。

--- ticketing/setup.py
+++ ticketing/setup.py
@@ -93,7 +93,6 @@ setup(name='ticketing',
       install_requires=requires,
       dependency_links = [
         'file:../commons#egg=altair-commons-0.0',
-        'file:../bundle/js.underscore#egg=js.underscore-1.3.3',
         'https://github.com/moriyoshi/tableau/tarball/master#egg=tableau-0.0.4pre',
         'https://bitbucket.org/moriyoshi/alembic/get/9fe8d037f71f.zip#egg=alembic-0.3.5moriyoshi', 
         "https://github.com/numpy/numpy/tarball/v1.6.2#egg=numpy-1.6.2",
