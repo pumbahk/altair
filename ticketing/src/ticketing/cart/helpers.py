751479871b2dca2c92170a431559dbc61e1febcf
podhmo <podhmo+altair@beproud.jp>
2012-10-17 11:21:45 +0900

    fakeがおちてた。

--- ticketing/src/ticketing/cart/helpers.py
+++ ticketing/src/ticketing/cart/helpers.py
@@ -38,6 +38,7 @@ def japanese_datetime(dt):
 def mail_date(date):
     return u'{d.year}年 {d.month}月 {d.day}日 {d.hour:02}時 {d.minute:02}分'.format(d=date)
 
+
 # TODO: requestをパラメータから排除
 def error_list(request, form, name):
     errors = form[name].errors
