722cba5002e9d2fde2d1e4e177a4482a8ee2b6d1
Moriyoshi Koizumi <mozo@mozo.jp>
2012-11-06 02:58:11 +0900

    モバイル用のメンテナンスページ. Fixes #1225.

--- maintenance-pages/docroots/#common/body.php
+++ maintenance-pages/docroots/#common/body.php
@@ -6,15 +6,7 @@
                 <tbody>
                   <tr>
                     <td>
-                      <p>ただいま下記のスケジュールにて、定期メンテナンスを行っております。</p>
-                      <p>お客様には大変ご不便とご迷惑をおかけいたしますが、今しばらくお待ちください。</p>
-                      <h4>メンテナンス時間</h4>
-                      <p>2012年11月6日 (火) 4:00〜5:00</p>
-                      <p>(状況により、時刻が前後することがございます)</p>
-                      <h4>利用できなくなるサービス</h4>
-                      <ul>
-                        <li><?php echo htmlspecialchars($_SERVER['HTTP_HOST']); ?>内の各サービス</li>
-                      </ul>
+                      <?php include_once 'maintenance_message.php'; ?>
                     </td>
                   </tr>
                 </tbody>
