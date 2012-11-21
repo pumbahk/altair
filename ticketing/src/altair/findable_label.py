6c92d10bab927a7fa80d8a9b3a3731a1cce73d16
podhmo <podhmo+altair@beproud.jp>
2012-11-21 11:26:52 +0900

    とりあえずhtml要素を追加するtween書いた。#1290 altair.findable_label
    ;; 設定例
    pyramid.includes = pyramid_tm pyramid_mailer altair.findable_label
    ; finding label
    altair.findable_label.label = これはdevelop

---
+++ ticketing/src/altair/findable_label.py
@@ -0,0 +1,45 @@
+from zope.interface import Interface
+from zope.interface import provider
+
+def includeme(config):
+    config.add_tween(".findable_label.findable_label_tween_factory")
+    output = config.maybe_dotted(config.registry.settings.get("altair.findable_label.output", ".findable_label.AppendHeaderElementOutput"))
+    labelname = config.registry.settings.get("altair.findable_label.label", "[unknown]").decode("utf-8")
+    config.registry.registerUtility(provider(IFindableLabelOutput)(output(labelname)))
+
+class IFindableLabelOutput(Interface):
+    def rewrite(response):
+        pass
+    def output(response_body):
+        pass
+
+class AppendHeaderElementOutput(object):
+    def __init__(self, labelname, description=None):
+        self.labelname = labelname
+        self.description = description
+
+    def rewrite(self, response):
+        result = self.output(response.text)
+        response.body = ""
+        response.write(result.encode("utf-8"))
+        return response
+
+    def output(self, html):
+        return html.replace(u"<body>", u"""
+<body>
+<div style="font-size:20px;">
+  %s
+</div>
+""" % self.labelname)
+    
+
+def findable_label_tween_factory(handler, registry):
+    def tween(request):
+        response = handler(request)
+        content_type = response.content_type
+        if content_type and content_type.lower() in ["text/html"]:
+            instance = registry.getUtility(IFindableLabelOutput)
+            response = instance.rewrite(response)
+        return response
+    return tween
+
