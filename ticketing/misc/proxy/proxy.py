2d27eb68df0c8949c5785abbb5828104c191e959
Moriyoshi Koizumi <mozo@mozo.jp>
2012-12-12 17:59:29 +0900

    CONNECTメソッドによるSSL/TLS tunnelに対応した。対応しないとセキュア3Dできない。

--- ticketing/misc/proxy/proxy.py
+++ ticketing/misc/proxy/proxy.py
@@ -2,6 +2,8 @@
 from twisted.web import proxy, http
 from twisted.internet import reactor
 from twisted.python import log
+from twisted.internet import protocol
+from twisted.protocols import portforward
 from urlparse import urlparse, urlunparse
 import sys
 import re
@@ -13,44 +15,96 @@ SUBDOMAINS = [
     'tokyo-cr',
     ]
 
+class ProxyConnectClientDecoder(object):
+    def __init__(self, client):
+        self.client = client
+
+    def dataReceived(self, data):
+        self.client.transport.write(data)
+
+class ProxyConnectClient(protocol.Protocol):
+    def setFather(self, father):
+        self.father = father
+        self.father.channel._transferDecoder = ProxyConnectClientDecoder(self)
+        self.father.channel.setRawMode()
+
+    def connectionMade(self):
+        self.father.setResponseCode(200, 'Connection established')
+        self.father.write('')
+        self.father.transport.resumeProducing()
+
+    def dataReceived(self, data):
+        self.father.transport.write(data)
+
+class ProxyConnectClientFactory(protocol.ClientFactory):
+    protocol = ProxyConnectClient
+
+    def __init__(self, command, rest, version, headers, data, father):
+        self.father = father
+        self.command = command
+        self.rest = rest
+        self.headers = headers
+        self.data = data
+
+    def buildProtocol(self, *args, **kwargs):
+        prot = protocol.ClientFactory.buildProtocol(self, *args, **kwargs)
+        prot.setFather(self.father)
+        return prot
+
+    def clientConnectionFailed(self, connector, reason):
+        self.father.finish()
+
 class MyProxyRequest(proxy.ProxyRequest):
-    def rewrite(self, request_uri, headers):
-        new_request_uri_str = request_uri_str = urlunparse(request_uri)
-        for regexp, replace in self.channel.rewrite_patterns:
-            new_request_uri_str = re.sub(regexp, replace, request_uri_str)
-            if new_request_uri_str != request_uri_str:
-                self.channel.factory.logFile.write("%s => %s\n" % (request_uri_str, new_request_uri_str))
-                break
-            request_uri_str = new_request_uri_str
-        return urlparse(new_request_uri_str)
+    ports = dict(https=443, **proxy.ProxyRequest.ports)
+
+    def rewrite(self):
+        if self.method == 'CONNECT':
+            self.target_host, port = self.uri.split(':')
+            self.target_port = int(port)
+            self.target_parsed = None
+        else:
+            new_request_uri_str = request_uri_str = self.uri
+            for regexp, replace in self.channel.rewrite_patterns:
+                new_request_uri_str = re.sub(regexp, replace, request_uri_str)
+                if new_request_uri_str != request_uri_str:
+                    self.channel.factory.logFile.write("%s => %s\n" % (request_uri_str, new_request_uri_str))
+                    break
+                request_uri_str = new_request_uri_str
+            self.target_uri = new_request_uri_str
+            self.target_parsed = urlparse(new_request_uri_str)
+            if ':' in self.target_parsed.netloc:
+                # TODO: IPv6?
+                self.target_host, port = target_host.split(':')
+                self.target_port = int(port)
+            else:
+                self.target_host = self.target_parsed.netloc
+                self.target_port = self.ports[self.target_parsed.scheme]
+
+    def get_connector_class(self):
+        if self.method == 'CONNECT':
+            return ProxyConnectClientFactory
+        else:
+            return proxy.ProxyClientFactory
 
     def process(self):
-        parsed = urlparse(self.uri)
-        headers = self.getAllHeaders().copy()
+        self.parsed = urlparse(self.uri)
+        self.target_headers = self.getAllHeaders().copy()
+        self.rewrite()
 
-        target = self.rewrite(parsed, headers)
+        rest = None
+        if self.target_parsed is not None:
+            rest = urlunparse(('http', '') + self.target_parsed[2:])[7:]
+            if not rest:
+                rest = rest + '/'
 
-        target_host = target.netloc
-        target_port = self.ports[target.scheme]
-
-        if ':' in target_host:
-            # TODO: IPv6?
-            target_host, target_port = target_host.split(':')
-            target_port = int(target_port)
-
-        rest = urlunparse(('http', '') + target[2:])[7:]
-        if not rest:
-            rest = rest + '/'
-
-        class_ = self.protocols[target.scheme]
-        if 'host' not in headers:
-            headers['host'] = parsed.netloc
+        class_ = self.get_connector_class()
+        if 'host' not in self.target_headers:
+            self.target_headers['host'] = self.parsed.netloc
 
         self.content.seek(0, 0)
         s = self.content.read()
-        clientFactory = class_(self.method, rest, self.clientproto, headers,
-                               s, self)
-        self.reactor.connectTCP(target_host, target_port, clientFactory)
+        client = class_(self.method, rest, self.clientproto, self.target_headers, s, self)
+        self.reactor.connectTCP(self.target_host, self.target_port, client)
 
 class MyProxy(proxy.Proxy):
     requestFactory = MyProxyRequest
