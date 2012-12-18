ae948457c16a09ff4c6b67b84d4224f68ac3acf9
podhmo <podhmo+altair@beproud.jp>
2012-12-18 14:30:32 +0900

    ticketing.log -> altair.log

---
+++ altair_log/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+version = "0.0.0"
+
+requires = [
+    "pyramid"
+]
+
+setup(name='altair.log',
+      version=version,
+      description="",
+      keywords='',
+      packages=find_packages(),
+      namespace_packages=['altair'],
+      test_suite="altair.log.tests", 
+      install_requires=requires)
