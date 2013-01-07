ac7b006a369bfc88b07c2cf965e756a52789ad9a
podhmo <podhmo+altair@beproud.jp>
2013-01-07 15:00:03 +0900

    sej券面のpreviewの時、時間掛かる旨を伝え続行して良いか尋ねることにした

--- ticketing/src/ticketing/static/js/tickets/preview/preview_api.js
+++ ticketing/src/ticketing/static/js/tickets/preview/preview_api.js
@@ -1,16 +1,39 @@
+// use: static/js/spin.js
+// use: static/js/altair/spinner.js
 // api-params: {svg: <>,  ticket_format: <>,  model_name: <>,  model: <>}
-function svg_preview($el, apiurl, params){
-    $.post(apiurl, params).done(function(data){
-        if(data.status){
-            $el.empty();
-            var imgdata = "data:image/png;base64,"+data.data;
-            var preview_img = $("<img title='preview' alt='preview todo upload file'>").attr("src", imgdata);
-            $el.append(preview_img);
-        } else {
-            alert(data.message);
+var svg_preview = (function(){
+    var sejPreviewConnectOK = null;
+    var sejConnectConfirm = function(){
+        if(sejPreviewConnectOK != null){
+            return sejPreviewConnectOK;
         }
-    }).fail(function(s, err){alert(s.responseText)});
-};
+        var sejConfirmMessage = "sejのpreview serverと通信します。preview結果を表示するまで時間が掛かりそうですがよろしいですか？";
+        sejPreviewConnectOK = window.confirm(sejConfirmMessage);
+        return sejPreviewConnectOK;
+    };
+
+    return function($el, apiurl, params){
+        if (params.type == "sej" && !sejConnectConfirm()){
+            return;
+        }
+        $el.spin("large");
+        $.post(apiurl, params).done(function(data){
+            if(data.status){
+                $el.empty();
+                var imgdata = "data:image/png;base64,"+data.data;
+                var preview_img = $("<img title='preview' alt='preview todo upload file'>").attr("src", imgdata);
+                $el.append(preview_img);
+            } else {
+                $el.empty();
+                $el.text("通信に失敗しました。preview画像を表示することができません。");
+            }
+            $el.spin(false);
+        }).fail(function(s, err){
+            $el.empty();
+            $el.text("通信に失敗しました。preview画像を表示することができません。");
+        });
+    }
+})();
 
 // use: static/js/spin.js
 // use: static/js/altair/spinner.js
