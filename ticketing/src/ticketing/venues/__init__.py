9b056df15a200460a46ada975ae1317e8aa349fb
SASAO Takahiro <sasao@p-strain.com>
2013-01-22 13:34:14 +0900

    site viewer for backend view.

--- ticketing/src/ticketing/venues/__init__.py
+++ ticketing/src/ticketing/venues/__init__.py
@@ -1,6 +1,7 @@
 def includeme(config):
     config.add_route("venues.index", "/")
     config.add_route("venues.show", "/show/{venue_id}")
+    config.add_route("venues.checker", "/{venue_id}/checker")
     config.add_route("api.get_drawing", "/{venue_id}/get_drawing")
     config.add_route("api.get_seats", "/{venue_id}/seats/")
     config.add_route("api.get_frontend", "/{venue_id}/frontend/{part}")
