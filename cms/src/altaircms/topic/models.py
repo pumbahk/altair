6b834bfabc2c74a38def84ffa32e90033e3bd18a
podhmo <podhmo+altair@beproud.jp>
2013-01-29 16:42:46 +0900

    [cms] topic,topcontentをpromotionのinterfaceに合わせるfixes #1580

--- cms/src/altaircms/topic/models.py
+++ cms/src/altaircms/topic/models.py
@@ -29,9 +29,12 @@ topicはtopicウィジェットで使われる。
 3. (2.の日時に基づく順序より優先して)表示順序を指定できること
     表示順序の範囲は　1 〜 100 
     デフォルトでは50
-4. 自動的に、ページ、イベントに紐ついたページ, 同カテゴリのものが表示される。
 
-ができる必要がある。
+
+※種々の理由により以下のようなモデルと日本語名の対応
+* Topic -> トピック(テキスト)
+* Topcontent -> トピック(画像)
+* Promotion -> プロモーション枠(カルーセル)
 
 """
 
@@ -79,87 +82,17 @@ class AboutPublishMixin(object):
 
 _where = object()
 
+
+topic2kind = sa.Table(
+    "topic2kind", Base.metadata, 
+    sa.Column("topic_id", sa.Integer, sa.ForeignKey("topic.id")), 
+    sa.Column("kind_id", sa.Integer, sa.ForeignKey("kind.id"))
+    )
+
 class Topic(AboutPublishMixin, 
             BaseOriginalMixin,
             WithOrganizationMixin, 
             Base):
-    """
-    トピック
-
-各フィールドの説明
-----------------------------------------
-
-ページと結びついたフィールド
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-ページが結び付けられるフィールドは２つある。(両者共に任意)
-(各フィールドの名前はpageで終わっているが、実際のところpagesetと結びついている)
-
-+ linked_page: トピックがリンクとして利用される場合の飛び先
-+ bound_page: トピックが表示されるページ
-
-bound_pageに表示されたトピックをクリックするとlinked_pageに飛ぶということ。
-※(kind=特集のトピックは、外部URLをtextに持つことができる。(plugins/widget/topic/sidebar_category_genre.mako, helers.link.get_link_from_topic))
-
-各フィールドとtopic widgetでの表示条件について
---------------------------------------------------------------------------------
-
-表示の絞りこみについては以下のフィールドが使われる
-
-+ kind
-+ subkind
-+ event
-+ bound_page
-+ is_global
-
-kindでトピックの種類を指定する。(e.g. トピックス, 特集, )
-
-subkindでさらに細かく種類を分けることができる。しかし、通常のページではsubkindを使わない。
-subkindを利用するのは、同じページ内で同じ種類のトピックを別の箇所に表示したい場合。
-(subkindを利用するページとして、ヘルプページ、公演中止情報のページなどが挙げられる。)
-
-event, bound_pageは任意。指定した際には、topic widgetでの絞り込みに用いることができる。
-
-ちなみにtopic widgetでの検索条件の指定は以下３つが行える。
-
-+ グローバルトピックを表示
-+ ページに関連したトピックを表示
-+ イベントに関連したトピックを表示
-
-後者２つはand結合。それらとグローバルトピックはor結合。
-
-グローバルトピックを表示の効果
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-グローバルトピックをONにしたとき、掲載期間内のis_global=Trueのトピックが常に表示されるようになる。
-
-例えば、トップページ、音楽ページ(カテゴリトップ), 邦楽ページ(サブカテゴリトップ)などでトピックを共有したい時に使う。
-(音楽ページ、邦楽ページのみで共有したい場合には、サブカテゴリと共に利用する)
-
-各カテゴリページでのトピックの利用方法
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-各カテゴリページのみで表示するトピックを登録したい場合には、
-以下のようなトピックを登録し、以下のようにtopic widgetを利用する。
-
-Topic(kind=kind, page=category_page, is_global=False)で登録(kind は例えばトピックス)
-topic widgetでは
-
-+ グローバルトピックを表示 => OFF
-+ ページに関連したトピックを表示 => ON
-+ イベントに関連したトピックを表示 => OFF
-
-イベントに関連したトピックを表示は各カテゴリページで利用しない。
-
-イベントに関連したトピックを表示を利用するとき
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-イベントに関連したトピックを表示を利用するのは、イベント詳細ページに隣接するページでトピック表示を共有したい場合。
-（隣接するページ=同一イベントを親に持つイベント詳細ページ以外のページ。
-　詳細ページのタブメニューにリンクを持つページ。例えばイベントギャラリー、詳細説明（別ページ）など）
-
-このとき、イベントに関連したトピックを表示のみをONにする。
-    """
-
     query = DBSession.query_property()
 
     __tablename__ = "topic"
@@ -167,72 +100,66 @@ topic widgetでは
     id = sa.Column(sa.Integer, primary_key=True)
     created_at = sa.Column(sa.DateTime, default=datetime.now)
     updated_at = sa.Column(sa.DateTime, default=datetime.now, onupdate=datetime.now)
+    kinds = orm.relationship("Kind", secondary=topic2kind, backref=orm.backref("topics"))
 
-    kind = sa.Column(sa.Unicode(255), index=True)
-    subkind = sa.Column(sa.Unicode(255))
     title = sa.Column(sa.Unicode(255))
     text = sa.Column(sa.UnicodeText)
-    event_id = sa.Column(sa.Integer, sa.ForeignKey("event.id"), nullable=True)
-    event = orm.relationship(Event, backref="topic")
     
     ## topic をlinkとして利用したときの飛び先
     linked_page_id = sa.Column(sa.Integer, sa.ForeignKey("pagesets.id"), nullable=True)
     linked_page = orm.relationship(PageSet, primaryjoin="Topic.linked_page_id==PageSet.id")
 
-    ## topicが表示されるページ
-    bound_page_id = sa.Column(sa.Integer, sa.ForeignKey("pagesets.id"), nullable=True)
-    bound_page = orm.relationship(PageSet, primaryjoin="Topic.bound_page_id==PageSet.id")
-    is_global = sa.Column(sa.Boolean, default=False)
-
-
-    link = sa.Column(sa.Unicode(255), doc="external link")
-    mobile_link = sa.Column(sa.Unicode(255), doc="external mobile_link")
+    link = sa.Column(sa.Unicode(255), doc="external link", nullable=True)
+    mobile_link = sa.Column(sa.Unicode(255), doc="external mobile_link", nullable=True)
 
     def __repr__(self):
         return "topic: %s title=%s" % (self.kind, self.title)
 
     @property
-    def topic_type(self):
-        """ topicがどんな種類で関連付けられているか調べる
-        pageよりeventが優先 => eventが存在＝event詳細ページのどれか
-        """
-        if self.is_global:
-            return "global"
-        elif self.event:
-            return "event:%d" % self.event.id
-        elif self.bound_page:
-            return "page:%d" % self.bound_page.id
-        else:
-            return None
+    def kind_content(self):
+        return u", ".join(k.name for k in self.kinds)
+
+    @kind_content.setter
+    def kind_content(self, v):
+        self._kind_content = v
+
+    import re
+    SPLIT_RX = re.compile("[,、]")
+    def update_kind(self, ks):
+        ## 面倒なのでO(N)
+        splitted = self.SPLIT_RX.split(ks)
+        ks = set(k.strip() for k in splitted)
+        will_updates = []
+        for k in ks:
+            if k: 
+                kind = Kind.query.filter_by(name=k, organization_id=self.organization_id).first()
+                kind = kind or Kind(name=k, organization_id=self.organization_id)
+                will_updates.append(kind)
+        will_deletes = set(self.kinds).difference(will_updates)
+        for k in will_updates:
+            self.kinds.append(k)
+        for k in will_deletes:
+            self.kinds.remove(k)
 
     @classmethod
-    def matched_topic_type(cls, page=None, event=None, qs=None):
-        if qs is None:
-            qs = cls.query
-
-        where = _where
-        if page:
-            where = (Topic.bound_page==page) if where  == _where else where & (Topic.bound_page==page)
-        if event:
-            where = (Topic.event==event) if where   == _where else where & (Topic.event==event)
-
-        if where  == _where: 
-            return qs.filter(cls.is_global==True)
-        else:
-            return qs.filter(where | (cls.is_global==True))
-
-    @classmethod
-    def matched_qs(cls, d=None, page=None, event=None, qs=None, kind=None, subkind=None):
-        """ 下にある内容の通りのtopicsを返す
-        """
+    def matched_qs(cls, d=None, kind=None, qs=None):
         qs = cls.publishing(d=d, qs=qs)
-        qs = cls.matched_topic_type(qs=qs, page=page, event=event)
-
         if kind:
-            qs = qs.filter_by(kind=kind)
-        if subkind:
-            qs = qs.filter_by(subkind=subkind)
+            qs = qs.filter(cls.kinds.any(Kind.name==kind))
+
         return qs
+
+def delete_topic_orphan_kind(mapper, connection, target):
+    Kind.query.filter(~Kind.topics.any()).delete(synchronize_session=False)
+
+sa.event.listen(Topic, "after_delete", delete_topic_orphan_kind)
+
+
+topcontent2kind = sa.Table(
+    "topcontent2kind", Base.metadata, 
+    sa.Column("topcontent_id", sa.Integer, sa.ForeignKey("topcontent.id")), 
+    sa.Column("kind_id", sa.Integer, sa.ForeignKey("kind.id"))
+    )
 
 class Topcontent(AboutPublishMixin,
                  BaseOriginalMixin,
@@ -248,9 +175,8 @@ class Topcontent(AboutPublishMixin,
     id = sa.Column(sa.Integer, primary_key=True)
     created_at = sa.Column(sa.DateTime, default=datetime.now)
     updated_at = sa.Column(sa.DateTime, default=datetime.now, onupdate=datetime.now)
+    kinds = orm.relationship("Kind", secondary=topic2kind, backref=orm.backref("topics"))
 
-    kind = sa.Column(sa.Unicode(255), index=True)
-    subkind = sa.Column(sa.Unicode(255))
     title = sa.Column(sa.Unicode(255))
     text = sa.Column(sa.Unicode(255))
 
@@ -260,10 +186,6 @@ class Topcontent(AboutPublishMixin,
 
     link = sa.Column(sa.Unicode(255), doc="external link")
     mobile_link = sa.Column(sa.Unicode(255), doc="external mobile_link")
-    
-    ## topcontentが表示されるページ
-    bound_page_id = sa.Column(sa.Integer, sa.ForeignKey("pagesets.id"), nullable=True)
-    bound_page = orm.relationship(PageSet, primaryjoin="Topcontent.bound_page_id==PageSet.id")
 
     ## extend
     image_asset_id = sa.Column(sa.Integer, sa.ForeignKey("image_asset.id"), nullable=True)
@@ -271,7 +193,6 @@ class Topcontent(AboutPublishMixin,
     mobile_image_asset_id = sa.Column(sa.Integer, sa.ForeignKey("image_asset.id"), nullable=True)
     mobile_image_asset = orm.relationship(ImageAsset, primaryjoin="Topcontent.mobile_image_asset_id==ImageAsset.id")
     countdown_type = sa.Column(sa.String(255)) #todo: fixme
-    is_global = sa.Column(sa.Boolean, default=True)
 
     def __repr__(self):
         return "topcontent: %s title=%s" % (self.kind, self.title)
@@ -285,44 +206,13 @@ class Topcontent(AboutPublishMixin,
         return getattr(self.linked_page.event, self.countdown_type)
 
     @classmethod
-    def matched_qs(cls, d=None, page=None, qs=None, kind=None, subkind=None):
-        """ 下にある内容の通りのtopcontentsを返す
-        """
+    def matched_qs(cls, d=None, kind=None, qs=None):
         qs = cls.publishing(d=d, qs=qs)
-        qs = cls.matched_topcontent_type(qs=qs, page=page)
         if kind:
-            qs = qs.filter_by(kind=kind)
-        if subkind:
-            qs =  qs.filter_by(subkind=subkind)
+            qs = qs.filter(cls.kinds.any(Kind.name==kind))
+
         return qs
 
-    @property
-    def topcontent_type(self):
-        """ topcontentがどんな種類で関連付けられているか調べる
-        pageよりeventが優先 => eventが存在＝event詳細ページのどれか
-        """
-        if self.is_global:
-            return "global"
-        elif self.bound_page:
-            return "page:%d" % self.bound_page_id
-        else:
-            return None
-
-    @classmethod
-    def matched_topcontent_type(cls, page=None, event=None, qs=None):
-        if qs is None:
-            qs = cls.query
-
-        where = _where
-        if page:
-            where = (Topcontent.bound_page==page) if where  == _where else where & (Topcontent.bound_page==page)
-        if event:
-            where = (Topcontent.event==event) if where   == _where else where & (Topcontent.event==event)
-
-        if where  == _where: 
-            return qs.filter(cls.is_global==True)
-        else:
-            return qs.filter(where | (cls.is_global==True))
 
 ### promotion
 promotion2kind = sa.Table(
@@ -354,14 +244,14 @@ class Promotion(WithOrganizationMixin,
     query = DBSession.query_property()
     __tablename__ = "promotion"
 
+    id = sa.Column(sa.Integer, primary_key=True)
     kinds = orm.relationship("Kind", secondary=promotion2kind, 
                              backref=orm.backref("promotions"))
+    created_at = sa.Column(sa.DateTime, default=datetime.now)
+    updated_at = sa.Column(sa.DateTime, default=datetime.now, onupdate=datetime.now)
 
-    id = sa.Column(sa.Integer, primary_key=True)
     main_image_id = sa.Column(sa.Integer, sa.ForeignKey("image_asset.id"))
     main_image = orm.relationship("ImageAsset", uselist=False, primaryjoin="Promotion.main_image_id==ImageAsset.id")
-    thumbnail_id = sa.Column(sa.Integer, sa.ForeignKey("image_asset.id"))
-    thumbnail = orm.relationship("ImageAsset", uselist=False, primaryjoin="Promotion.thumbnail_id==ImageAsset.id")
     text = sa.Column(sa.UnicodeText, default=u"no message")
 
     ## linkとpagesetは排他的
