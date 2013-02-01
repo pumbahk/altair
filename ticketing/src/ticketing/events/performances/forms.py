73d1913eace293925458d6a65a6fe2218141b447
katosaori <ribontolili@gmail.com>
2013-02-01 19:05:32 +0900

    チケット#535 同じ日時会場の場合のバリデータ変更

--- ticketing/src/ticketing/events/performances/forms.py
+++ ticketing/src/ticketing/events/performances/forms.py
@@ -87,8 +87,8 @@ class PerformanceForm(Form):
     def validate_start_on(form, field):
         if field.data and form.open_on.data and field.data < form.open_on.data:
             raise ValidationError(u'開場日時より過去の日時は入力できません')
- 
-        if field.data and Performance.query.filter(Performance.start_on==field.data).join(Venue).filter(Venue.id==form.venue_id.data).count():
+
+        if field.data and Performance.query.filter(Performance.start_on==field.data).join(Venue).filter(Venue.site_id==Venue.filter(Venue.id==form.venue_id.data).one().site_id).count():
             raise ValidationError(u'指定日時と指定会場で既に登録があります')
 
     def validate_end_on(form, field):
