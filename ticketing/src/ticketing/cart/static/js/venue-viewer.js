be1e4cce0325d544f17f67f5b86fd6dcb55e6b92
dev@ticketstar.jp <dev@ticketstar.jp>
2013-02-08 10:05:05 +0900

    [HOTFIX] アニメーションがタイミングによって悪影響を及ぼすので無効にする

--- ticketing/src/ticketing/cart/static/js/venue-viewer.js
+++ ticketing/src/ticketing/cart/static/js/venue-viewer.js
@@ -1,6 +1,61 @@
 (function () {
 var __LIBS__ = {};
-__LIBS__['iV0YYQTIUOJUJOAX'] = (function (exports) { (function () { 
+__LIBS__['PKMNPEYF2J1WTSWS'] = (function (exports) { (function () { 
+
+/************** CONF.js **************/
+exports.DEFAULT = {
+  ZOOM_RATIO: 0.8,
+  STYLES: {
+    label: {
+      fill: new Fashion.Color('#000'),
+      stroke: null
+    },
+    seat: {
+      fill: new Fashion.Color('#fff'),
+      stroke: new Fashion.Color('#000')
+    },
+    glayout: {
+      fill: new Fashion.FloodFill(new Fashion.Color('#ccc')),
+      stroke: new Fashion.Stroke(new Fashion.Color('#999'), 2)
+    }
+  },
+
+  MASK_STYLE: {
+    fill:   new Fashion.FloodFill(new Fashion.Color("#0064ff80")),
+    stroke: new Fashion.Stroke(new Fashion.Color("#0080FF"), 2)
+  },
+
+  SEAT_STYLE: {
+    text_color: "#000",
+    fill:   { color: "#fff" }
+  },
+
+  OVERLAYS: {
+    highlighted: {
+      fill: null,
+      stroke: { color: "#F63", width: 3, pattern: 'solid' }
+    },
+    highlighted_block: {
+      fill: null,
+      stroke: { color: "#F44", width: 5, pattern: 'solid' }
+    }
+  },
+
+  AUGMENTED_STYLE: {
+    selected: {
+      text_color: "#FFF",
+      fill:   { color: "#009BE1" },
+      stroke: { color: "#FFF", width: 3 }
+    },
+    unselectable: {
+      text_color: "#888",
+      fill:   { color: "#eee" },
+      stroke: { color: "#ccc" }
+    }
+  }
+};
+ })(); return exports; })({});
+__LIBS__['DDMAPDKCN4P9XOTY'] = (function (exports) { (function () { 
 
 /************** util.js **************/
 exports.eventKey = function Util_eventKey(e) {
@@ -127,66 +182,11 @@ exports.makeHitTester = function Util_makeHitTester(a) {
   }
 };
  })(); return exports; })({});
-__LIBS__['HKZY4SP2MAHQFA5D'] = (function (exports) { (function () { 
-
-/************** CONF.js **************/
-exports.DEFAULT = {
-  ZOOM_RATIO: 0.8,
-  STYLES: {
-    label: {
-      fill: new Fashion.Color('#000'),
-      stroke: null
-    },
-    seat: {
-      fill: new Fashion.Color('#fff'),
-      stroke: new Fashion.Color('#000')
-    },
-    glayout: {
-      fill: new Fashion.FloodFill(new Fashion.Color('#ccc')),
-      stroke: new Fashion.Stroke(new Fashion.Color('#999'), 2)
-    }
-  },
-
-  MASK_STYLE: {
-    fill:   new Fashion.FloodFill(new Fashion.Color("#0064ff80")),
-    stroke: new Fashion.Stroke(new Fashion.Color("#0080FF"), 2)
-  },
-
-  SEAT_STYLE: {
-    text_color: "#000",
-    fill:   { color: "#fff" }
-  },
-
-  OVERLAYS: {
-    highlighted: {
-      fill: null,
-      stroke: { color: "#F63", width: 3, pattern: 'solid' }
-    },
-    highlighted_block: {
-      fill: null,
-      stroke: { color: "#F44", width: 5, pattern: 'solid' }
-    }
-  },
-
-  AUGMENTED_STYLE: {
-    selected: {
-      text_color: "#FFF",
-      fill:   { color: "#009BE1" },
-      stroke: { color: "#FFF", width: 3 }
-    },
-    unselectable: {
-      text_color: "#888",
-      fill:   { color: "#eee" },
-      stroke: { color: "#ccc" }
-    }
-  }
-};
- })(); return exports; })({});
-__LIBS__['SRROMF_IQ4KIW5IQ'] = (function (exports) { (function () { 
+__LIBS__['bSKWKCK6PS2HPN41'] = (function (exports) { (function () { 
 
 /************** seat.js **************/
-var util = __LIBS__['iV0YYQTIUOJUJOAX'];
-var CONF = __LIBS__['HKZY4SP2MAHQFA5D'];
+var util = __LIBS__['DDMAPDKCN4P9XOTY'];
+var CONF = __LIBS__['PKMNPEYF2J1WTSWS'];
 
 function clone(obj) {
   return $.extend({}, obj);
@@ -1021,9 +1021,9 @@ function parseTransform(transform_str) {
     throw new Error('invalid transform function: ' + f);
 }
 
-  var CONF = __LIBS__['HKZY4SP2MAHQFA5D'];
-  var seat = __LIBS__['SRROMF_IQ4KIW5IQ'];
-  var util = __LIBS__['iV0YYQTIUOJUJOAX'];
+  var CONF = __LIBS__['PKMNPEYF2J1WTSWS'];
+  var seat = __LIBS__['bSKWKCK6PS2HPN41'];
+  var util = __LIBS__['DDMAPDKCN4P9XOTY'];
 
   var StoreObject = _class("StoreObject", {
     props: {
@@ -1612,20 +1612,24 @@ function parseTransform(transform_str) {
                     clickTimer = setTimeout(singleClickFulfilled,
                                             self.doubleClickTimeout);
                   } else {
-                    // double click
-                    clearTimeout(clickTimer);
-                    clickTimer = 0;
-                    self.drawableMouseDown = false;
-                    self.animating = true;
-                    var e = self.zoomRatio * 2;
-                    var t = setInterval(function () {
-                      var newZoomRatio = Math.min(e, self.zoomRatio * 1.2);
-                      self.zoom(newZoomRatio, evt.logicalPosition);
-                      if (e - self.zoomRatio < self.zoomRatio * 1e-5 || newZoomRatio - self.zoomRatio > self.zoomRatio * 1e-5) {
-                        self.animating = false;
-                        clearInterval(t);
-                      }
-                    }, 50);
+                    if (!self.dragging) {
+                      // double click
+                      clearTimeout(clickTimer);
+                      clickTimer = 0;
+                      self.drawableMouseDown = false;
+                      var e = self.zoomRatio * 2;
+                      self.zoom(e, evt.logicalPosition);
+                      /*
+                      self.animating = true;
+                      var t = setInterval(function () {
+                        var newZoomRatio = Math.min(e, self.zoomRatio * 1.2);
+                        if (e - self.zoomRatio < self.zoomRatio * 1e-5 || newZoomRatio - self.zoomRatio > self.zoomRatio * 1e-5) {
+                          self.animating = false;
+                          clearInterval(t);
+                        }
+                      }, 50);
+                      */
+                    }
                   }
                   break;
                 }
