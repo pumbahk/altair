37b182f7f85db89e52461d8f8c2f9d7b0d7bf034
podhmo <podhmo+altair@beproud.jp>
2013-02-12 20:11:43 +0900

    sqlを吐くscriptでっちあげる

--- cms/misc/genre_fixture_from_json.py
+++ cms/misc/genre_fixture_from_json.py
@@ -1,107 +1,56 @@
-import json
-import sqlahelper
 import sys
-import sqlalchemy as sa
-import altaircms.event.models
-import functools
-import altaircms.page.models
-import tableau as t
-from altaircms.models import Genre
-from altaircms.scripts.misc.dataset import DataSuite, WithCallbackDataSet
-from tableau.sqla import newSADatum
-from tableau import DataWalker
-from tableau.sql import SQLGenerator
+import json
+from collections import namedtuple 
+import codecs
 
-# def parse(params, ancestors, result):
-#     target = Genre(name=params["name"], label=params["label"])
-#     result.append(target)
-#     if "children" in params:
-#         ancestors.insert(0, target)
-#         for sub_params in params["children"]:
-#             child = parse(sub_params, ancestors, result)
-#             for i, p in enumerate(ancestors):
-#                 child._add_parent(p, hop=i+1)
-#     return target
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+Genre = namedtuple("genre", "id name label organization_id")
+GenrePath = namedtuple("genre_path", "genre_id next_id hop")
 
-class FixtureBuilder(object):
-    def __init__(self, Datum):
-        class _Datum(Datum):
-            def __init__(self, schema, **fields):
-                Datum.__init__(self, schema, t.auto("id"), **fields)
-        self.Datum = _Datum
-        self._Datum = Datum
+class Counter(object):
+    def __init__(self, i=0):
+        self.i = i
 
-    def parse(self, params, ancestors, result):
-        target = self.Datum("Genre", name=params["name"], label=params["label"])
-        result.append(target)
-        if "children" in params:
-            ancestors.insert(0, target)
-            for sub_params in params["children"]:
-                child = self.parse(sub_params, ancestors, result)
-                for i, p in enumerate(ancestors):
-                    # child._add_parent(p, hop=i+1)
-                    self.result.append(self.Datum("genre_path"), genre=child, next_genre=p, hop=i+1)
-        return target
+    def inc(self):
+        self.i += 1
+        return self.i
 
-    def build(self, params):
-        result = []
-        ancestors = []
-        self.parse(params, ancestors, result)
-        return self.result
+class SQLObject(object):
+    def __init__(self, out=sys.stdout):
+        self.out = out
 
-class WithOffset(object):
-    """ insert sql id with offset value
-
-    e.g. offset = 10
-    insert statment start from id=11
-    """
-    def __init__(self):
-        self.offset_table = {}
-        self.session = sqlahelper.get_session()
-        self.table_to_declarative = self.collect_declarative(sqlahelper.get_base())
-
-    def collect_declarative(self, base):
-        table_to_declarative = {}
-        if base is not None:
-            for class_name, declarative in base._decl_class_registry.items():
-                table_to_declarative[declarative.__table__.name] = declarative
-        return table_to_declarative
-
-    def _get_offset_value(self, schema, data):
-        cls = self.table_to_declarative[schema]
-        return cls.query.with_entities(sa.func.Max(cls.id)).scalar() or 0
-
-    def get_offset_value(self, schema, data): #memoize function
-        if not schema in self.offset_table:
-            self.offset_table[schema] = self._get_offset_value(schema, data)
-        return self.offset_table[schema]
+    def insert(self, o):
+        name = o.__class__.__name__
+        fields = " ".join(o._fields)
+        values = u" ".join('"%s"' % v if isinstance(v, (str, unicode)) else str(v) for v in list(o))
+        self.out.write(u'INSERT INTO %s (%s) VALUES (%s)\n' % (name, fields, values))
         
-    def setvalue(self, data, datum):
-        schema = datum._tableau_table.name
-        offset = self.get_offset_value(schema, datum)
-        pk = data.seq + offset
-        setattr(datum, datum._tableau_id_fields[0], pk)
-        assert getattr(datum, datum._tableau_id_fields[0], pk)
+
+organization_id = 1
+Genre_id = Counter(0)
+
+def parse(params, ancestors, result):
+    target = Genre(id=Genre_id.inc(), name=params["name"], label=params["label"], organization_id=organization_id)
+    result.append(target)
+    if "children" in params:
+        ancestors.insert(0, target)
+        for sub_params in params["children"]:
+            child = parse(sub_params, ancestors, result)
+            for i, p in enumerate(ancestors):
+                path = GenrePath(genre_id=child.id, next_id=target.id, hop=i+1)
+                result.append(path)
+        ancestors.pop(0)
+    return target
 
 
 def main(args):
-    engine = args[1]
-    jsonfile = args[2]
-    sqlahelper.add_engine(sa.create_engine(engine))
-
-    Base = sqlahelper.get_base()
-    Datum = newSADatum(Base.metadata)
-
-    builder =FixtureBuilder(Datum)
-    suite = DataSuite(dataset_impl=functools.partial(WithCallbackDataSet, on_autoid=WithOffset()))
-    walker = DataWalker(suite)
-
-    with open(jsonfile,"r") as rf:
-        for datum in builder.build(json.load(rf, encoding="utf-8")):
-            walker(datum)
-    SQLGenerator(sys.stdout, encoding='utf-8')(suite)
+    with open(args[1],"r") as rf:
+        result = []
+        ancestors = []
+        sql = SQLObject()
+        parse(json.load(rf, encoding="utf-8"), ancestors, result)
+        for o in result:
+            sql.insert(o)
 
 main(sys.argv)
-
-
     
