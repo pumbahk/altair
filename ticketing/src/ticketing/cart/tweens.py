80e655a9907f0df7182952e5b1d35332f020c23b
Atsushi Odagiri <aodagx@gmail.com>
2013-02-18 18:01:03 +0900

    fixes #1883

--- ticketing/src/ticketing/cart/tweens.py
+++ ticketing/src/ticketing/cart/tweens.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
+
+import os
+import logging
 from datetime import datetime
 from time import mktime
 from email.utils import formatdate
+
+logger = logging.getLogger(__name__)
 
 class CacheControlTween(object):
     def __init__(self, handler, registry):
@@ -24,3 +29,39 @@ class CacheControlTween(object):
 
         return response
 
+class OrganizationPathTween(object):
+    def __init__(self, handler, registry):
+        self.handler = handler
+        self.registry = registry
+
+    def get_hosts(self, request):
+        from ticketing.core.models import Host
+        hosts = Host.query.filter(Host.host_name==unicode(request.host)).all()
+        return list(reversed(sorted(hosts, key=lambda h: h.path)))
+
+    def __call__(self, request):
+        # もしパスつきのHostドメインだったら
+        hosts = self.get_hosts(request)
+        if not hosts:
+            return self.handler(request)
+
+        for host in hosts:
+            if not host.path:
+                return self.handler(request)
+
+            # そのパスであるか request 判定して
+            if request.path_info.startswith(host.path):
+
+                # script_nameにずらす
+                (script_name, path_info) = (request.script_name 
+                                            + request.path_info[:len(host.path)], 
+                                            request.path_info[len(host.path):])
+                logger.debug("{0} {1}".format(script_name, path_info))
+                request.script_name = script_name
+                request.path_info = path_info
+                request.organization = host.organization
+                request.environ['ticketing.cart.organization_id'] = host.organization.id
+                request.environ['ticketing.cart.organization_path'] = host.path
+                return self.handler(request)
+        return self.handler(request)
+        #raise Exception("%s %s" % (request.path_info, hosts))
