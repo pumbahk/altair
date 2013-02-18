179174f4a4038d7d58acc371eddd5138d7b37a73
Atsushi Odagiri <aodagx@gmail.com>
2013-02-18 14:09:09 +0900

    fixes #1830 suppress SAWarning

--- ticketing/src/ticketing/core/api.py
+++ ticketing/src/ticketing/core/api.py
@@ -5,7 +5,7 @@ def get_organization(request, override_host=None):
     reg = request.registry
     host_name = override_host or request.host
     try:
-        host = Host.query.filter(Host.host_name==host_name).one()
+        host = Host.query.filter(Host.host_name==unicode(host_name)).one()
         return host.organization
     except NoResultFound as e:
         raise Exception("Host that named %s is not Found" % host_name)
