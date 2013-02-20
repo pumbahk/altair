52ac3946542639c4c28365cc1dcae04090a82b45
mmatsui <masahiro.matsui@gmail.com>
2013-02-20 10:32:24 +0900

    一時的な確保座席のリリース条件にvenue_idを追加 fixes #1919

--- ticketing/src/ticketing/orders/views.py
+++ ticketing/src/ticketing/orders/views.py
@@ -28,7 +28,7 @@ from ticketing.models import DBSession, merge_session_with_post, record_to_multi
 from ticketing.core.models import (Order, Performance, PaymentDeliveryMethodPair, ShippingAddress,
                                    Product, ProductItem, OrderedProduct, OrderedProductItem, 
                                    Ticket, TicketBundle, TicketFormat, Ticket_TicketBundle,
-                                   DeliveryMethod, TicketFormat_DeliveryMethod, 
+                                   DeliveryMethod, TicketFormat_DeliveryMethod, Venue,
                                    SalesSegment, Stock, StockStatus, Seat, SeatStatus, SeatStatusEnum, ChannelEnum)
 from ticketing.mailmags.models import MailSubscription, MailMagazine, MailSubscriptionStatus
 from ticketing.orders.export import OrderCSV
@@ -744,13 +744,13 @@ class OrderDetailView(BaseView):
 @view_defaults(decorator=with_bootstrap, permission='sales_counter')
 class OrdersReserveView(BaseView):
 
-    def release_seats(self, l0_ids):
+    def release_seats(self, venue, l0_ids):
         # 確保座席があるならステータスを戻す
         logger.info("release seats : %s" % l0_ids)
         if l0_ids:
             seat_statuses = SeatStatus.filter(SeatStatus.status.in_([int(SeatStatusEnum.Keep), int(SeatStatusEnum.InCart)]))\
                 .join(SeatStatus.seat)\
-                .filter(Seat.l0_id.in_(l0_ids))\
+                .filter(and_(Seat.l0_id.in_(l0_ids), Seat.venue_id==venue.id))\
                 .with_lockmode('update').all()
             for seat_status in seat_statuses:
                 logger.info("seat(%s) status InCart to Vacant" % seat_status.seat_id)
@@ -763,7 +763,8 @@ class OrdersReserveView(BaseView):
 
         if inner_cart_session:
             if inner_cart_session.get('seats'):
-                self.release_seats(inner_cart_session.get('seats'))
+                venue = Venue.get(inner_cart_session.get('venue_id'))
+                self.release_seats(venue, inner_cart_session.get('seats'))
             del self.request.session['ticketing.inner_cart']
 
     @view_config(route_name='orders.api.get', renderer='json')
@@ -838,6 +839,7 @@ class OrdersReserveView(BaseView):
 
         # セッションに保存
         self.request.session['ticketing.inner_cart'] = {
+            'venue_id':performance.venue.id,
             'stocks':post_data.get('stocks'),
             'seats':post_data.get('seats'),
         }
@@ -891,7 +893,7 @@ class OrdersReserveView(BaseView):
                 raise ValidationError(u'個数の合計を選択した座席数（%d席）にしてください' % len(seats))
 
             # 選択されたSeatのステータスをいったん戻してカートデータとして再確保する
-            self.release_seats(seats)
+            self.release_seats(performance.venue, seats)
 
             # create cart
             cart = api.order_products(self.request, performance_id, order_items, selected_seats=seats)
