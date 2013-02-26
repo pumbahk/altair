59c5a81ec12db7b26c936d56165f7ca550f72cd8
Moriyoshi Koizumi <mozo@mozo.jp>
2013-02-26 13:47:55 +0900

    販売区分の開始日時、終了日時を修正する SQL 文を追加

--- ticketing/misc/migration/salessegment-migration-20130222.step1.up_to_47f92d43c86d.sql
+++ ticketing/misc/migration/salessegment-migration-20130222.step1.up_to_47f92d43c86d.sql
@@ -12,4 +12,16 @@ id,start_at,end_at,upper_limit,seat_choice,0,NULL AS performance_id, id as sales
 FROM SalesSegmentGroup as ssg
 WHERE ssg.id NOT IN (SELECT id FROM SalesSegment);
 
+-- 販売区分の終了日時がパフォーマンスの終了日時よりも前である場合はそれを修正する
+UPDATE SalesSegment, Performance SET SalesSegment.end_at=Performance.end_on
+WHERE SalesSegment.performance_id=Performance.id AND Performance.end_on < SaleSegment.end_at;
+
+-- 当日券用の販売区分の日時を修正する (on-the-day 対応)
+UPDATE SalesSegment, Performance, SalesSegmentGroup SET SalesSegment.start_at=DATE(Performance.start_on), SalesSegment.end_at=IF(Performance.end_on IS NOT NULL, Performance.end_on, (DATE(Performance.start_on)+INTERVAL 1 DAY-INTERVAL 1 SECOND))
+WHERE SalesSegment.performance_id=Performance.id AND SalesSegment.sales_segment_group_id=SalesSegmentGroup.id AND SalesSegment.start_at <= DATE(Performance.start_on) AND SalesSegmentGroup.kind='sales_counter';
+
+-- 一般販売と先行の販売区分の終了日時を前日の23:59:59までにする
+UPDATE SalesSegment, Performance, SalesSegmentGroup SET SalesSegment.end_at=DATE(Performance.start_on)-INTERVAL 1 SECOND
+WHERE SalesSegment.performance_id=Performance.id AND SalesSegment.sales_segment_group_id=SalesSegmentGroup.id AND SalesSegment.start_at <= DATE(Performance.start_on) AND SalesSegmentGroup.kind IN ('normal', 'early_firstcome', 'other');
+
 UPDATE Cart SET sales_segment_id=sales_segment_group_id WHERE sales_segment_id IS NULL;
