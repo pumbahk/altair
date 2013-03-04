cd1be2a9371d005eed8f3dd10ee37e5d49f859b2
Atsushi Odagiri <aodagx@gmail.com>
2013-03-04 14:19:54 +0900

    fixes #2026

--- ticketing/src/ticketing/cart/views.py
+++ ticketing/src/ticketing/cart/views.py
@@ -573,7 +573,7 @@ class PaymentView(object):
         start_on = cart.performance.start_on
         #payment_delivery_methods = self.context.get_payment_delivery_method_pair(start_on=start_on)
         sales_segment = self.sales_segment
-        payment_delivery_methods = sales_segment.payment_delivery_method_pairs
+        payment_delivery_methods = sales_segment.available_payment_delivery_method_pairs
         user = self.context.get_or_create_user()
         user_profile = None
         if user is not None:
@@ -659,7 +659,7 @@ class PaymentView(object):
         if not self._validate_extras(cart, payment_delivery_pair, shipping_address_params):
             start_on = cart.performance.start_on
             sales_segment = self.sales_segment
-            payment_delivery_methods = sales_segment.payment_delivery_method_pairs
+            payment_delivery_methods = sales_segment.available_payment_delivery_method_pairs
             return dict(form=self.form, payment_delivery_methods=payment_delivery_methods)
 
         cart.payment_delivery_pair = payment_delivery_pair
