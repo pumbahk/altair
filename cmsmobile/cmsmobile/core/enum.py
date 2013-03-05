c8ed1de066af302e04476dff3e13e0694b62d053
okeiichi <keiichi_okada@ticketstar.jp>
2013-03-05 11:50:53 +0900

    地域情報の列挙型を追加

---
+++ cmsmobile/cmsmobile/core/enum.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from standardenum import StandardEnum
+
+class AreaEnum(StandardEnum):
+
+    """
+    NoSelect    = (0, u'選択なし')
+    Syutoken    = (1, u'首都圏')
+    Kinki       = (2, u'近畿')
+    Tokai       = (3, u'東海')
+    Hokkaido    = (4, u'北海道')
+    Tohoku      = (5, u'東北')
+    KitaKanto   = (6, u'北関東')
+    Koshinetsu  = (7, u'甲信越')
+    Hokuriku    = (8, u'北陸')
+    Chugoku     = (9, u'中国')
+    Shikoku     = (10, u'四国')
+    Kyusyu      = (11, u'九州')
+    Okinawa     = (12, u'沖縄')
+
+    """
+    NoSelect    = 0
+    Syutoken    = 1
+    Kinki       = 2
+    Tokai       = 3
+    Hokkaido    = 4
+    Tohoku      = 5
+    KitaKanto   = 6
+    Koshinetsu  = 7
+    Hokuriku    = 8
+    Chugoku     = 9
+    Shikoku     = 10
+    Kyusyu      = 11
+    Okinawa     = 12
