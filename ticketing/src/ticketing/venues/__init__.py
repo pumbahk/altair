33ac91004314d3b117b1f8a0e224a81b509ceb32
SASAO Takahiro <sasao@p-strain.com>
2013-03-07 15:53:40 +0900

    会場図無しの会場登録＆変更機能.

--- ticketing/src/ticketing/venues/__init__.py
+++ ticketing/src/ticketing/venues/__init__.py
@@ -1,5 +1,7 @@
 def includeme(config):
     config.add_route("venues.index", "/")
+    config.add_route("venues.new", '/new')
+    config.add_route("venues.edit", "/edit/{venue_id}")
     config.add_route("venues.show", "/show/{venue_id}")
     config.add_route("venues.checker", "/{venue_id}/checker")
     config.add_route("api.get_drawing", "/{venue_id}/get_drawing")
