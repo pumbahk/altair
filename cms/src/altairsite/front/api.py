f3f9ad52c3f08756ec60a18373c996a2f1c89aae
podhmo <podhmo+altair@beproud.jp>
2013-03-12 13:17:36 +0900

    fix. event is None. performances are []

--- cms/src/altairsite/front/api.py
+++ cms/src/altairsite/front/api.py
@@ -41,7 +41,10 @@ class FrontPageRenderer(object):
         bsettings.blocks["title"] = [page.title]
 
         event = page.event
-        performances = Performance.query.filter(Performance.event_id==event.id).options(orm.joinedload("sales")).all()
+        if event is None:
+            performances = []
+        else:
+            performances = Performance.query.filter(Performance.event_id==event.id).options(orm.joinedload("sales")).all()
         ## 本当はwidget側からpullしていくようにしたい
         bsettings.scan(self.request, page=page, performances=performances, event=event)
         return bsettings
