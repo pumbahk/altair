5a87048860c5dd561f110d7a435945365da8b962
Atsushi Odagiri <aodagx@gmail.com>
2013-03-21 12:13:42 +0900

    日付け公演選択を実装

--- ticketing/src/ticketing/cart/performanceselector.py
+++ ticketing/src/ticketing/cart/performanceselector.py
@@ -40,8 +40,35 @@ class MatchUpPerformanceSelector(object):
         return select_venues
 
 
+@implementer(IPerformanceSelector)
 class DatePerformanceSelector(object):
     """ 日付け -> 会場 """
 
     def __init__(self, request):
         self.request = request
+        self.context = request.context
+        self.sales_segments = self.context.available_sales_segments
+
+    def __call__(self):
+        performances = [ss.performance for ss in self.sales_segments]
+
+        select_venues = OrderedDict()
+        for p in performances:
+            d = p.start_on.strftime("%Y-%m-%d")
+            select_venues[d] = []
+
+        for sales_segment in self.sales_segments:
+            performance = sales_segment.performance
+            d = performance.start_on.strftime("%Y-%m-%d")
+
+            select_venues[d].append(dict(
+                id=performance.id,
+                name=u'{start:%Y-%m-%d %H:%M}開始 {vname} {name}'.format(name=sales_segment.name, start=performance.start_on, vname=performance.venue.name),
+                order_url=self.request.route_url("cart.order", sales_segment_id=sales_segment.id),
+                upper_limit=sales_segment.upper_limit,
+                seat_types_url=self.request.route_url('cart.seat_types',
+                    performance_id=performance.id,
+                    sales_segment_id=sales_segment.id,
+                    event_id=self.context.event.id)))
+
+        return select_venues
