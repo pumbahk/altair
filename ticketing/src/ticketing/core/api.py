b4e3ff1db04e3930c1822e794293e64b7c69a986
Moriyoshi Koizumi <mozo@mozo.jp>
2013-03-26 22:06:36 +0900

    Oops2

--- ticketing/src/ticketing/core/api.py
+++ ticketing/src/ticketing/core/api.py
@@ -3,6 +3,7 @@
 import logging
 from sqlalchemy.orm.exc import NoResultFound
 from .models import Host, OrderNoSequence, ChannelEnum
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -49,7 +50,7 @@ def get_channel(channel=None, request=None):
     else:
         return ChannelEnum.PC
 
-def delete_event(self, event):
+def delete_event(event):
     # 既に販売されている場合は削除できない
     if event.sales_start_on and event.sales_start_on < datetime.now():
         raise Exception(u'既に販売開始日時を経過している為、削除できません')
