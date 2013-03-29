406108f90ca5b1ff76fce10c215cdabf6781a39f
podhmo <podhmo+altair@beproud.jp>
2013-03-29 22:15:00 +0900

    replace for /

--- cms/src/altairsite/feature/views.py
+++ cms/src/altairsite/feature/views.py
@@ -14,7 +14,7 @@ def features_view(context, request):
     prefix = path.split("/", 1)[0]
     logger.debug("prefix: %s" % prefix)
     if prefix == path.rstrip("/"):
-        return HTTPFound(request.route_path("features", path=os.path.join(path.rstrip("/"), "index.html")))
+        return HTTPFound(request.route_path("features", path=os.path.join(path.rstrip("/"), "index.html").replace("%2F", "/")))
     try:
         if prefix:
             static_page = request.allowable(StaticPage).filter(StaticPage.name==prefix, StaticPage.published==True, StaticPage.interceptive==False).first()
