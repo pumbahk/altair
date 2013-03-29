f1b83cf935ddc0cedcc1bf2a24c3101ba2addca6
podhmo <podhmo+altair@beproud.jp>
2013-03-29 10:53:47 +0900

    orderpage -> order

--- cms/src/altairsite/order/views.py
+++ cms/src/altairsite/order/views.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
 
-@view_config(route_name='orderpage', request_method="GET",
+##workaround.
+def pc_access(info, request):
+    return hasattr(request, "is_mobile") and request.is_mobile == False
+
+@view_config(route_name='usersite.order', request_method="GET",
+             custom_predicates=(pc_access, ), 
              renderer='altaircms:templates/usersite/order.html')
 def move_order(request):
     return {}
