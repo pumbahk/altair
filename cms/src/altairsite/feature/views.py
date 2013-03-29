4277177ee13f51ce20d8420a4e7e9c08a1867ecc
podhmo <podhmo+altair@beproud.jp>
2013-03-29 22:12:58 +0900

    features

--- cms/src/altairsite/feature/views.py
+++ cms/src/altairsite/feature/views.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 import os.path
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 
 from altaircms.page.models import StaticPage
 from altaircms.page.api import as_static_page_response, StaticPageNotFound
@@ -13,8 +13,8 @@ def features_view(context, request):
     path = request.matchdict["path"]
     prefix = path.split("/", 1)[0]
     logger.debug("prefix: %s" % prefix)
-    if prefix == path:
-        path = os.path.join(path, "index.html")
+    if prefix == path.rstrip("/"):
+        return HTTPFound(request.route_path("featurse", path=os.path.join(path.rstrip("/"), "index.html")))
     try:
         if prefix:
             static_page = request.allowable(StaticPage).filter(StaticPage.name==prefix, StaticPage.published==True, StaticPage.interceptive==False).first()
