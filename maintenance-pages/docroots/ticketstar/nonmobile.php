0729a8ceb585efe610c6659900249d5f7f747c5b
Moriyoshi Koizumi <mozo@mozo.jp>
2013-04-01 04:27:01 +0900

    c.tstar.jp 用メンテナンスページ

---
+++ maintenance-pages/docroots/ticketstar/nonmobile.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>チケットスター チケット販売サービス</title>
+  <link rel="stylesheet" type="text/css" href="assets/style.css" media="all" />
+  <script language="JavaScript" src="assets/jquery.js"></script>
+  <style type="text/css">
+    .error {
+      margin: 0;
+      color: red;
+      font-weight: bold;
+      font-size: 12px;
+    }
+  </style>
+</head>
+<body>
+  <h1 class="title">チケットスター 購入確認ページ</h1>
+  <div align="center">
+    <img src="/assets/head_title.png" alt="チケットスター" width="950" height="80" />
+    <table class="layout" style="width:760px" class="wrapper">
+      <tbody>
+        <tr>
+          <td>
+             <?php include '../#common/body.php' ?>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="footer">
+    <div class="footer-inner">
+      <img src="assets/tomoni.gif" alt="" />    
+      <div class="footernav">
+        <ul>
+      <li class="first"><a href="http://www.ticketstar.jp/corporate">運営会社</a></li>
+          <li><a href="mailto:vissel@tstar.jp">お問い合わせ</a></li>
+          <li><a href="http://www.ticketstar.jp/privacy">個人情報保護方針</a></li>
+          <li class="last"><a href="http://www.ticketstar.jp/legal">特定商取引法に基づく表示</a></li>
+        </ul>
+      </div>
+      <div class="copyright">
+        Copyright &copy; 2010-2013 TicketStar Inc. All Rights Reserved. 
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+
