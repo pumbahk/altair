e7e93b4991aee4c843b578f1059a60926917ad1b
podhmo <podhmo+altair@beproud.jp>
2013-04-02 17:50:11 +0900

    linklistと検索一覧の表示統一した

--- cms/src/altaircms/plugins/widget/linklist/api.py
+++ cms/src/altaircms/plugins/widget/linklist/api.py
@@ -1,6 +1,7 @@
 from zope.interface import provider
 import sqlalchemy as sa
 from .interfaces import IWFinder
+import sqlalchemy.orm as orm
 from altaircms.event.models import Event
 from altaircms.page.models import PageSet, Page
 
@@ -12,7 +13,7 @@ def near_the_end_events(request, N, today):
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
     qs = qs.filter(Event.is_searchable==True)
     qs = Event.near_the_deal_close_query(today, N=N, qs=qs)
-    qs = qs.order_by(sa.asc(Event.deal_close)).distinct(PageSet.id)
+    qs = qs.order_by(sa.asc(Event.deal_close)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
     return qs
 
 @provider(IWFinder)
@@ -20,5 +21,5 @@ def deal_start_this_week_events(request, N, today):
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
     qs = qs.filter(Event.is_searchable==True)
     qs =  Event.deal_start_this_week_query(today, offset=-today.weekday(), qs=qs)
-    qs = qs.order_by(sa.desc(Event.deal_open)).distinct(PageSet.id)
+    qs = qs.order_by(sa.desc(Event.deal_open)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
     return qs           
