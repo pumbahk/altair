b4cd0e07820ec70b98f650f56c4e3be509f17c4b
okeiichi <keiichi_okada@ticketstar.jp>
2013-04-09 17:21:46 +0900

    mobiletagテーブルを、pagetagテーブルのモバイル版とした

--- cms/alembic/versions/12c784295a2e_create_mobiletag_tab.py
+++ cms/alembic/versions/12c784295a2e_create_mobiletag_tab.py
@@ -17,17 +17,14 @@ import sqlalchemy as sa
 def upgrade():
     op.create_table('mobiletag',
                     sa.Column('id', sa.Integer(), nullable=False),
-                    sa.Column('tag_id', sa.Integer(), nullable=True),
-                    sa.Column('name', sa.Unicode(length=255), nullable=True),
-                    sa.Column('display_order', sa.Integer(), nullable=True),
-                    sa.Column('enablep', sa.Boolean(), nullable=True),
-                    sa.Column('term_begin', sa.DateTime(), nullable=True),
-                    sa.Column('term_end', sa.DateTime(), nullable=True),
+                    sa.Column('label', sa.Unicode(length=255), nullable=True),
+                    sa.Column('publicp', sa.Boolean(), nullable=True),
                     sa.Column('created_at', sa.DateTime(), nullable=True),
                     sa.Column('updated_at', sa.DateTime(), nullable=True),
                     sa.Column('organization_id', sa.Integer(), nullable=True),
-                    sa.ForeignKeyConstraint(['tag_id'], ['pagetag.id'], name="fk_mobiletag_tag_id_to_pagetag_id"),
-                    sa.PrimaryKeyConstraint('id')
+                    sa.CheckConstraint('TODO'),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('label','publicp'),
     )
     op.execute("ALTER TABLE mobiletag ADD INDEX mobiletag_organization_idx(organization_id);")
 
