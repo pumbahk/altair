a782e59e9b35dabbd3f71f61d8a87c342ad0e600
mmatsui <masahiro.matsui@gmail.com>
2013-04-16 20:37:32 +0900

    SeatAdjacencyの生成バッチを修正

--- ticketing/src/ticketing/commands/copy_seat_adjacency.py
+++ ticketing/src/ticketing/commands/copy_seat_adjacency.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 
 from pyramid.paster import bootstrap
 from sqlalchemy import func, or_, and_
+from sqlalchemy.sql.expression import not_
 
 from ticketing.core import models as c
 
@@ -21,29 +22,39 @@ def main(argv=sys.argv):
 
     logger.info('start copy_seat_adjacency batch')
 
-    # 1日以内に更新された、SeatAdjacencySetが1件もないVenueを抽出
     now = datetime.now() - timedelta(days=1)
-    query = c.Venue.query.filter(c.Venue.updated_at>now)
-    query = query.outerjoin(c.SeatAdjacencySet)
-    query = query.group_by(c.Venue.id).having(func.count(c.SeatAdjacencySet.id)==0)
+    venues_to_skip = set()
+    venues_to_commit = set()
 
-    try:
-        for venue in query.all():
+    while True:
+        try:
+            # 1日以内に更新された、SeatAdjacencySetが1件もないVenueを抽出
+            query = c.Venue.query.filter(c.Venue.updated_at>now)
+            query = query.outerjoin(c.SeatAdjacencySet)
+            if venues_to_skip:
+                query = query.filter(not_(c.Venue.id.in_(venues_to_skip)))
+            query = query.group_by(c.Venue.id).having(func.count(c.SeatAdjacencySet.id)==0)
+
+            venue = query.first()
+            if venue is None:
+                logging.info('venue not found')
+                break
             logger.info('copy from=%s, to=%s, %s, %s' % (venue.original_venue_id, venue.id, venue.name, venue.updated_at))
+
             org_venue = c.Venue.get(venue.original_venue_id)
-            if not org_venue:
+            if org_venue is None or not org_venue.adjacency_sets:
+                venues_to_skip.add(venue.id)
                 continue
-            seat_adjacency_map = {}
 
             # copy SeatAdjacencySet - SeatAdjacency
-            if org_venue.adjacency_sets:
-                for org_adjacency_set in org_venue.adjacency_sets:
-                    seat_adjacency_map.update(
-                        c.SeatAdjacencySet.create_from_template(
-                            template=org_adjacency_set,
-                            venue_id=venue.id
-                        )
+            seat_adjacency_map = {}
+            for org_adjacency_set in org_venue.adjacency_sets:
+                seat_adjacency_map.update(
+                    c.SeatAdjacencySet.create_from_template(
+                        template=org_adjacency_set,
+                        venue_id=venue.id
                     )
+                )
 
             # copy Seat_SeatAdjacency
             seats = dict([(seat.l0_id, seat.id) for seat in venue.seats])
@@ -57,10 +68,16 @@ def main(argv=sys.argv):
                         })
                     c.DBSession.execute(c.Seat_SeatAdjacency.__table__.insert(), seat_seat_adjacencies)
 
-        transaction.commit()
-    except Exception, e:
-        logger.error(e.message)
-        transaction.abort()
+            venues_to_commit.append(venue.id)
+            transaction.commit()
+
+            # 最大10件処理したら終了
+            if len(venues_to_commit) > 10:
+                break
+        except Exception, e:
+            logger.error(e.message)
+            transaction.abort()
+            break
 
     logger.info('end copy_seat_adjacency batch')
 
