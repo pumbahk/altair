23e90637dbd19a95b319f54706b2471230e00496
okeiichi <keiichi_okada@ticketstar.jp>
2013-04-22 13:41:58 +0900

    今週販売、販売終了間近の表示条件に、詳細ページの公開条件を追加 refs #3512

--- cms/src/altaircms/plugins/widget/linklist/api.py
+++ cms/src/altaircms/plugins/widget/linklist/api.py
@@ -4,6 +4,7 @@ from .interfaces import IWFinder
 import sqlalchemy.orm as orm
 from altaircms.event.models import Event
 from altaircms.page.models import PageSet, Page
+from datetime import datetime
 
 def get_linklist_candidates_finder(request, key):
     return request.registry.getUtility(IWFinder, key)
@@ -11,6 +12,10 @@ def get_linklist_candidates_finder(request, key):
 @provider(IWFinder)
 def near_the_end_events(request, N, today):
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
+    qs = qs.join(Page, Page.pageset_id == PageSet.id)
+    qs = qs.filter(Page.published == True)
+    qs = qs.filter(Page.publish_begin < datetime.now())
+    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > datetime.now()))
     qs = qs.filter(Event.is_searchable==True)
     qs = Event.near_the_deal_close_query(today, N=N, qs=qs)
     qs = qs.order_by(sa.asc(Event.deal_close)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
@@ -19,6 +24,10 @@ def near_the_end_events(request, N, today):
 @provider(IWFinder)
 def deal_start_this_week_events(request, N, today):
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
+    qs = qs.join(Page, Page.pageset_id == PageSet.id)
+    qs = qs.filter(Page.published == True)
+    qs = qs.filter(Page.publish_begin < datetime.now())
+    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > datetime.now()))
     qs = qs.filter(Event.is_searchable==True)
     qs =  Event.deal_start_this_week_query(today, offset=-today.weekday(), qs=qs)
     qs = qs.order_by(sa.desc(Event.deal_open)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
