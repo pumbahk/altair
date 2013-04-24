c0b0a1493625efab18fa0c4043cf98b18c6dcf61
podhmo <podhmo+altair@beproud.jp>
2013-04-24 10:37:01 +0900

    修正漏れ

--- cms/src/altaircms/asset/viewhelpers.py
+++ cms/src/altaircms/asset/viewhelpers.py
@@ -1,11 +1,11 @@
 from markupsafe import Markup
 import altaircms.helpers as h
 
-def image_asset_layout(request, asset, width="50px", height="50px"):
+def image_asset_layout(request, asset, href=None, width="50px", height="50px"):
     if asset is None:
         u""
     else:
-        params = dict(href=h.asset.rendering_object(request, asset).image_path,
+        params = dict(href=href or h.asset.rendering_object(request, asset).image_path,
                       width=width, 
                       height=height, 
                       alt=asset.title)
