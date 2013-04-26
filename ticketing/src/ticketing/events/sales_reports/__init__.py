0eb72f113201ce6b2fe2e9a47fd940761d9e6ca2
mmatsui <masahiro.matsui@gmail.com>
2013-04-27 02:57:26 +0900

    公演単位でのレポート機能を追加 refs #3475

--- ticketing/src/ticketing/events/sales_reports/__init__.py
+++ ticketing/src/ticketing/events/sales_reports/__init__.py
@@ -4,9 +4,9 @@ def includeme(config):
     config.add_route('sales_reports.index', '/')
     config.add_route('sales_reports.event','/event/{event_id}')
     config.add_route('sales_reports.performance', '/performance/{performance_id}')
-    config.add_route('sales_reports.preview','/preview/{event_id}')
-    config.add_route('sales_reports.mail_body','/mail_body/{event_id}')
-    config.add_route('sales_reports.send_mail','/send_mail/{event_id}')
+    config.add_route('sales_reports.preview','/preview/')
+    config.add_route('sales_reports.mail_body','/mail_body/')
+    config.add_route('sales_reports.send_mail','/send_mail/')
 
-    config.add_route('sales_reports.mail.new', '/mail/new/{event_id}')
+    config.add_route('sales_reports.mail.new', '/mail/new/')
     config.add_route('sales_reports.mail.delete', '/mail/delete/{report_setting_id}')
