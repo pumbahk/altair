cb5c70efe17b200f60bdb7fb4f3d2e4f522519cc
okeiichi <keiichi_okada@ticketstar.jp>
2013-05-07 11:32:31 +0900

    リクエストに在庫データが無い場合、バックエンドと通信するよう変更

--- cms/src/altaircms/plugins/widget/__init__.py
+++ cms/src/altaircms/plugins/widget/__init__.py
@@ -3,3 +3,11 @@ from .installer import WidgetPluginInstaller
 def widget_plugin_install(config, settings):
     pyramid_settings = config.registry.settings
     WidgetPluginInstaller(pyramid_settings).install(settings)
+
+def includeme(config):
+    api_impl = (config.maybe_dotted(".api.StockDataAPI")
+                (config.registry.settings["altaircms.backend.url"],
+                 config.registry.settings["altaircms.backend.apikey"]))
+    config.registry.registerUtility(api_impl,
+                                    config.maybe_dotted("altaircms.plugins.interfaces.IExternalAPI"),
+                                    api_impl.__class__.__name__)
--- cms/src/altaircms/plugins/widget/api.py
+++ cms/src/altaircms/plugins/widget/api.py
@@ -1,8 +1,15 @@
+# -*- coding:utf-8 -*-
 import logging
-logger = logging.getLogger(__name__)
+import json
+import contextlib
+import urllib2
 from ..api import list_from_setting_value
 from altaircms.plugins.api import get_widget_utility
+from collections import defaultdict
+from altaircms.plugins.interfaces import IExternalAPI
+from zope.interface import implementer
 
+logger = logging.getLogger(__name__)
 
 def get_rendering_function_via_page(widget, bname, bsettings, type_=None):
     bsettings.need_extra_in_scan("request")
@@ -95,3 +102,112 @@ class WidgetRegisterViewProxy(object):
     def create(self, name, params, matchdict=None):
         route_name = self._build_route_name(name, "_widget_create")        
         return self._use_view(route_name, params, matchdict=matchdict)
+
+
+"""
+cms calendar:
+
+status :: performance -> stock-status
+ """
+
+def get_stock_data_api(request):
+    return request.registry.getUtility(IExternalAPI, name=StockDataAPI.__name__)
+
+class StockStatus(object):
+    circle = {"class": "circle", "string":u"○"}
+    triangle = {"class": "triangle", "string":u"△"}
+    cross = {"class": "cross", "string":u"×"}
+    unknown = {"class": "unknown", "string": u""}
+
+class MaruBatsuSankaku(object):
+    circle = {"class": "maru", "string":u"○"}
+    triangle = {"class": "sankaku", "string":u"△"}
+    cross = {"class": "batsu", "string":u"×"}
+    unknown = {"class": "hatena", "string": u""}
+
+dummy_data = {"stocks": []}
+def get_performance_status(request, widget, event, status_impl):
+    """
+    call api
+    """
+    data = dummy_data
+    try:
+        data = getattr(request, "_stock_data", None)
+        if not data:
+            data = get_stock_data_api(request).fetch_stock_status(request, event, widget.salessegment)
+            data = json.loads(data)
+            request._stock_data = data
+    except urllib2.HTTPError, e:
+        logger.warn("*calendar widget* api call is failed. url=%s" % e.url)
+    except urllib2.URLError, e:
+        logger.warn("*calendar widget* api call is failed (URLError)")
+    except Exception, e:
+        logger.warn("*calendar widget* api call is failed")
+        logger.exception(str(e))
+    return _get_performance_status(request, CalcResult(rawdata=data, status_impl=status_impl))
+
+def get_performance_status_from_request(request):
+    request.registry.getUtility
+
+
+def _get_performance_status(request, data):
+    for stock in data.rawdata["stocks"]:
+        data.add_stock(stock)
+    return data
+
+###
+
+
+@implementer(IExternalAPI)
+class StockDataAPI(object):
+    def __init__(self, url, apikey):
+        self.external_url = url.rstrip("/")
+        self.apikey = apikey
+
+    def get_fetch_stock_status_api_url(self, event, salessegment_group):
+        fmt = u"%(base)s/api/events/%(event_id)s/stock_statuses"
+        return fmt % dict(base=self.external_url, event_id=event.backend_id)
+
+    def fetch_stock_status(self, request, event, salessegment_group=None):
+        url = self.get_fetch_stock_status_api_url(event, salessegment_group)
+        req = urllib2.Request(url)
+        req.add_header('X-Altair-Authorization', self.apikey)
+        with contextlib.closing(urllib2.urlopen(req)) as res:
+            data = res.read()
+            logger.debug("*calendar widget api* returned value: %s" % data)
+            return data
+
+
+class NotFoundMatchResultException(Exception):
+    pass
+
+## (find-file "api-dummy-data.json")
+class CalcResult(object): #すごい決め打ち
+    def __init__(self, rawdata=None, status_impl=StockStatus):
+        self.status_impl = status_impl
+        self.scores = defaultdict(int)
+        self.counts = defaultdict(int)
+        self.has_soldout = {}
+        self.rawdata = rawdata
+
+    def add_stock(self, stock):
+        k = stock["performance_id"]
+        self.scores[k] += stock["available"]
+        self.counts[k] += stock["assigned"]
+        if stock["available"] <= 0:
+            self.has_soldout[k] = True
+
+    def get_status(self, performance):
+        if performance.purchase_link:
+            return self.status_impl.unknown
+        k = performance.backend_id
+        try:
+            if self.scores[k] <= 0:
+                return self.status_impl.cross
+            elif self.has_soldout.get(k) or (float(self.scores[k]) / self.counts[k]) <= 0.2:
+                return self.status_impl.triangle
+            else:
+                return self.status_impl.circle
+        except KeyError:
+            logger.warn("performance [backend_id=%d] is not found" % performance.backend_id)
+            return self.status_impl.cross
