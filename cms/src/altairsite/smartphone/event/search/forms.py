6fcfa933a1327b457ce0f991da92b6e7b8d2726f
okeiichi <keiichi_okada@ticketstar.jp>
2013-05-14 14:45:04 +0900

    詳細検索のフォーム整理

--- cms/src/altairsite/smartphone/event/search/forms.py
+++ cms/src/altairsite/smartphone/event/search/forms.py
@@ -42,27 +42,27 @@ class DetailSearchForm(TopSearchForm):
 
     cond = RadioField(label = '', validators=[Optional()],
         choices=[("intersection", u'全てを含む'), ("union", u'少なくとも１つを含む')], default="intersection", coerce=str)
-    prefecture_hokkaido = CheckboxListField(u'北海道・東海',
+    prefecture_hokkaido = CheckboxListField(u'北海道・東北',
             choices=[
                 ('', u'全て選択'), ('hokkaido', u'北海道'), ('aomori', u'青森'), ('iwate', u'岩手'), ('akita', u'秋田'), ('miyagi', u'宮城'), ('yamagata', u'山形'), ('fukushima', u'福島')
             ])
-    prefecture_syutoken = CheckboxListField(u'首都圏',
+    prefecture_syutoken = CheckboxListField(u'首都圏・北関東',
             choices=[
                 ('', u'全て選択'), ('tokyo', u'東京'),  ('kanagawa', u'神奈川'), ('chiba', u'千葉'), ('saitama', u'埼玉'), ('ibaraki', u'茨城'), ('tochigi', u'栃木') ,('gunma', u'群馬'),  ('yamanashi', u'山梨')
             ])
-    prefecture_koshinetsu = CheckboxListField(u'',
+    prefecture_koshinetsu = CheckboxListField(u'甲信越・東海',
             choices=[
                 ('', u'全て選択'), ('nagano', u'長野') ,('niigata', u'新潟'), ('gifu', u'岐阜'), ('aichi', u'愛知') ,('mie', u'三重'), ('shizuoka', u'静岡')
             ])
-    prefecture_kinki = CheckboxListField(u'',
+    prefecture_kinki = CheckboxListField(u'近畿・北陸',
             choices=[
                 ('', u'全て選択'), ('kyoto', u'京都'), ('osaka', u'大阪'), ('hyogo', u'兵庫'), ('shiga', u'滋賀'), ('nara', u'奈良'), ('wakayama', u'和歌山'), ('toyama', u'富山'), ('ishikawa', u'石川'), ('fukui', u'福井')
             ])
-    prefecture_chugoku = CheckboxListField(u'',
+    prefecture_chugoku = CheckboxListField(u'中国・四国',
             choices=[
                 ('', u'全て選択'), ('hiroshima', u'広島'), ('okayama', u'岡山'), ('tottori', u'鳥取'), ('shimane', u'島根') ,('yamaguchi', u'山口'), ('tokushima', u'徳島'), ('kagawa', u'香川'), ('ehime', u'愛媛'), ('kouchi', u'高知')
             ])
-    prefecture_kyusyu = CheckboxListField(u'',
+    prefecture_kyusyu = CheckboxListField(u'九州・沖縄',
             choices=[
                 ('', u'全て選択'), ('okinawa', u'沖縄'), ('fukuoka', u'福岡'), ('saga', u'佐賀'), ('nagasaki', u'長崎'), ('kumamoto', u'熊本') ,('oita', u'大分'), ('miyazaki', u'宮崎') ,('kagoshima', u'鹿児島')
             ])
