0b142b443fddc04da3f976637fa8a1e7875f5eb8
Moriyoshi Koizumi <mozo@mozo.jp>
2013-05-15 18:31:18 +0900

    alembic 依存関係修正

--- ticketing/alembic/versions/52e2400a2c14_drop_seat_seatadjace.py
+++ ticketing/alembic/versions/52e2400a2c14_drop_seat_seatadjace.py
@@ -1,14 +1,14 @@
 """drop  Seat_SeatAdjacency
 
 Revision ID: 52e2400a2c14
-Revises: 2a0994d0826
+Revises: 1830c3b36cba
 Create Date: 2013-05-15 13:29:12.503355
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '52e2400a2c14'
-down_revision = '2a0994d0826'
+down_revision = '1830c3b36cba'
 
 from alembic import op
 import sqlalchemy as sa
