2a79bccf574036b7dd64301489ef5b6c88c66fa7
Atsushi Odagiri <aodagx@gmail.com>
2013-05-31 15:20:43 +0900

    テスト復活

--- altair_mq/src/altair/mq/testing.py
+++ altair_mq/src/altair/mq/testing.py
@@ -2,7 +2,7 @@ from pyramid import testing
 
 
 class DummyMessage(testing.DummyResource):
-    def __init__(self, channel=None, method=None, header=[], body="", params={}):
+    def __init__(self, channel=None, method=None, header=[], body="", params={}, request=None):
         testing.DummyResource.__init__(self, 
                                        channel=channel, 
                                        method=method, 
@@ -10,3 +10,7 @@ class DummyMessage(testing.DummyResource):
                                        body=body, 
                                        params=params)
 
+        if request is None:
+            request = testing.DummyRequest()
+
+        self.request = request
