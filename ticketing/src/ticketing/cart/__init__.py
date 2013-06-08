427ff2379d3d63aa8194ac9a6ac921efb3c11337
dev@ticketstar.jp <dev@ticketstar.jp>
2013-06-09 05:05:52 +0900

    ISE の画面を親切にした

--- ticketing/src/ticketing/cart/__init__.py
+++ ticketing/src/ticketing/cart/__init__.py
@@ -24,6 +24,19 @@ from ..api.impl import bind_communication_api ## cmsとの通信
 from altair.mobile import PC_ACCESS_COOKIE_NAME
 PC_SWITCH_COOKIE_NAME = PC_ACCESS_COOKIE_NAME
 
+def exception_message_renderer_factory(show_traceback):
+    def exception_message_renderer(request, exc_info, message):
+        from pyramid.httpexceptions import HTTPInternalServerError
+        from pyramid.renderers import render
+        from .selectable_renderer import selectable_renderer
+        from altair.mobile.api import is_mobile
+        if is_mobile(request):
+            renderer = selectable_renderer('carts_mobile/%(membership)s/error.html')
+        else:
+            renderer = selectable_renderer('carts/%(membership)s/message.html')
+        return HTTPInternalServerError(body=render(renderer, { 'message': u'システムエラーが発生しました。大変お手数ですが、しばらく経ってから再度トップ画面よりアクセスしてください。(このURLに再度アクセスしてもエラーが出続けることがあります)' }, request))
+    return exception_message_renderer
+
 class WhoDecider(object):
     def __init__(self, request):
         self.request = request
