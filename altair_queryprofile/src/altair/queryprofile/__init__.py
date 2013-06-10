df694f36807da22356dd37de5106ba2aa81da604
Atsushi Odagiri <aodagx@gmail.com>
2013-06-10 14:33:19 +0900

    altair.queryprofile

---
+++ altair_queryprofile/src/altair/queryprofile/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+import threading
+import logging
+from pyramid.threadlocal import get_current_request
+from sqlalchemy import event
+from sqlalchemy.engine.base import Engine
+
+logger = logging.getLogger(__name__)
+lock = threading.Lock()
+
+def includeme(config):
+    config.add_tween('altair.queryprofile.QueryCountTween')
+
+
+@event.listens_for(Engine, "before_cursor_execute")
+def _before_cursor_execute(conn, cursor, stmt, params, context, execmany):
+    pass
+
+
+@event.listens_for(Engine, "after_cursor_execute")
+def _after_cursor_execute(conn, cursor, stmt, params, context, execmany):
+    request = get_current_request()
+    if request is not None:
+        with lock:
+            count = request.environ.get('altair.queryprofile.query_count', 0)
+            request.environ['altair.queryprofile.query_count'] = count + 1
+
+
+class QueryCountTween(object):
+    def __init__(self, handler, registry):
+        self.handler = handler
+        self.registry = registry
+
+    def __call__(self, request):
+        try:
+            request.environ['altair.queryprofile.query_count'] = 0
+            return self.handler(request)
+        finally:
+            count = request.environ.get('altair.queryprofile.query_count', -1)
+            if count > -1:
+                url = request.url
+                logger.debug('*' * 80 + '\n' + 'query count during {url}: {count}'.format(url=url, count=count))
---
+++ altair_queryprofile/src/altair/queryprofile/tests.py
@@ -0,0 +1,36 @@
+import unittest
+from pyramid import testing
+from .testing import DummyHandler, setUpDB, tearDownDB
+
+
+class QueryCountTweenTests(unittest.TestCase):
+    def setUp(self):
+        self.request = testing.DummyRequest()
+        self.config = testing.setUp(request=self.request)
+        self.db_session = setUpDB()
+
+    def tearDown(self):
+        testing.tearDown()
+        tearDownDB()
+
+
+    def _getTarget(self):
+        from . import QueryCountTween
+        return QueryCountTween
+
+    def _makeOne(self, *args, **kwargs):
+        return self._getTarget()(*args, **kwargs)
+
+
+    def test_it(self):
+        marker = object()
+        handler = DummyHandler(marker, 99)
+        target = self._makeOne(handler, self.config.registry)
+        request = self.request
+
+        result = target(request)
+
+        self.assertEqual(result, marker)
+
+        self.assertEqual(len(request.environ['altair.queryprofile.testing.results']), 99)
+        self.assertEqual(request.environ['altair.queryprofile.query_count'], 99)
