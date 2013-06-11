d9f6293abdbae303fa2a3d48d6b14bdbe288fe58
Moriyoshi Koizumi <mozo@mozo.jp>
2013-06-11 10:49:15 +0900

    session が変わってしまっているので organization から membership が取れない....

--- ticketing/src/ticketing/fc_auth/api.py
+++ ticketing/src/ticketing/fc_auth/api.py
@@ -1,6 +1,6 @@
 import logging
 from ticketing.users.models import Membership
-
+from ticketing.models import DBSession
 from ticketing.core import api as core_api
 
 logger = logging.getLogger(__name__)
@@ -15,7 +15,7 @@ def get_memberships(request):
 
 def login_url(request):
     organization = core_api.get_organization(request)
-    memberships = organization.memberships
+    memberships = DBSession.query(Membership).filter_by(organization_id=organization.id).all()
     logger.debug("login url %s membership %s" % (request.context, memberships))
     url = request.route_url('fc_auth.login', membership=memberships[0].name)
     logger.debug("login url %s" % url)
