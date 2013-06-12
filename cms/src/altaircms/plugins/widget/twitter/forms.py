c116af08b20f1be2f003724cfe47049740456680
okeiichi <keiichi_okada@ticketstar.jp>
2013-06-12 17:33:26 +0900

    新ツイッターウィジェット対応 refs #4134

--- cms/src/altaircms/plugins/widget/twitter/forms.py
+++ cms/src/altaircms/plugins/widget/twitter/forms.py
@@ -8,4 +8,5 @@ class TwitterSearchDataInputForm(Form):
     search_query = fields.TextField(label=u"検索ワード")
     title = fields.TextField(label=u"タイトル")
     subject = fields.TextField(label=u"見出し") ## caption
-    
+    data_widget_id = fields.TextField(label=u"ツイッターウィジェットID(data-widget-id)")
+
