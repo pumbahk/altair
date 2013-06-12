a766e52c1c687de89367b92033e7d0f654d0d50e
podhmo <podhmo+altair@beproud.jp>
2013-06-12 18:44:05 +0900

    booster review修正. refs #4162

--- ticketing/src/ticketing/booster/panels.py
+++ ticketing/src/ticketing/booster/panels.py
@@ -4,5 +4,5 @@ def input_form(context, request, form, products):
 def review_additional_messages(context, request, pm):
     return {"pm": pm}
 
-def complete_notice(context, request):
-    return {}
+def complete_notice(context, request, is_show_order_review=True):
+    return {"is_show_order_review": is_show_order_review}
