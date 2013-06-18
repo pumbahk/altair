776c2b544a0af679afd06e143cb269ff3dcabace
mmatsui <masahiro.matsui@gmail.com>
2013-06-18 20:10:54 +0900

    SeatStatus.updated_atのINDEX追加 fixes #4046

--- ticketing/src/ticketing/orders/commands.py
+++ ticketing/src/ticketing/orders/commands.py
@@ -33,18 +33,19 @@ def _keep_to_vacant():
         DBSession.bind = DBSession.bind or sqlahelper.get_session().bind
 
         expire_minute = int(registry.settings['altair.inner_cart.expire_minute'])
-        expire_time = datetime.now() - timedelta(minutes=expire_minute)
-        logging.info('expire_time : %s (now - %s minute)' % (expire_time, expire_minute))
+        expire_to = datetime.now() - timedelta(minutes=expire_minute)
+        expire_from = expire_to - timedelta(days=1)
+        logging.info('expire_to : %s (now - %s minute)' % (expire_to, expire_minute))
 
         seats = SeatStatus.filter_by(status=int(SeatStatusEnum.Keep))\
-                          .filter(SeatStatus.updated_at < expire_time)\
+                          .filter(SeatStatus.updated_at.between(expire_from, expire_to))\
                           .with_entities(SeatStatus.seat_id).all()
         logging.info('target seat_id : %s' % seats)
 
         if seats:
             query = SeatStatus.__table__.update().values(
                 {'status': int(SeatStatusEnum.Vacant)}
-            ).where(and_(SeatStatus.status==int(SeatStatusEnum.Keep), SeatStatus.updated_at < expire_time))
+            ).where(and_(SeatStatus.status==int(SeatStatusEnum.Keep), SeatStatus.updated_at.between(expire_from, expire_to))
             DBSession.bind.execute(query)
     except Exception as e:
         logging.error('failed to update SeatStatus (%s)' % e.message)
