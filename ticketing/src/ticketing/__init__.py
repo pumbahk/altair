dfda43a2fd3e93faa66afc4d59a88bb37d3a41d4
mmatsui <masahiro.matsui@gmail.com>
2013-06-27 16:26:20 +0900

    admin -> read committed fixes #3788

--- ticketing/src/ticketing/__init__.py
+++ ticketing/src/ticketing/__init__.py
@@ -56,6 +56,7 @@ def main(global_config, **local_config):
         from sqlalchemy.pool import NullPool
 
         engine = engine_from_config(settings, poolclass=NullPool,
+                                    isolation_level='READ COMMITTED',
                                     pool_recycle=60)
         sqlahelper.add_engine(engine)
 
