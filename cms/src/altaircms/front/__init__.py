f5d73d35c36004c5c8fad0e4cdb01e8bc28c13ac
podhmo <podhmo+altair@beproud.jp>
2013-07-05 13:50:41 +0900

    impl -> resolver

--- cms/src/altaircms/front/__init__.py
+++ cms/src/altaircms/front/__init__.py
@@ -1,7 +1,7 @@
 def includeme(config):
     settings = config.registry.settings
-    from altairsite.front.impl import LayoutModelResolver
-    from altairsite.front.impl import ILayoutModelResolver
+    from altairsite.front.resolver import LayoutModelResolver
+    from altairsite.front.resolver import ILayoutModelResolver
     layout_lookup = LayoutModelResolver(settings["altaircms.layout_directory"], 
                                    checkskip=False)
     config.registry.registerUtility(layout_lookup, ILayoutModelResolver)
