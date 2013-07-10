a20cc1689465aec6e4176f1b54e0ed6f773380a6
podhmo <podhmo+altair@beproud.jp>
2013-07-10 10:55:31 +0900

    push忘れ？ whattime

---
+++ ticketing/src/ticketing/whattime/resources.py
@@ -0,0 +1,15 @@
+from pyramid.security import Allow, Everyone
+from zope.interface import implementer
+from ticketing.login.internal.interfaces import IInternalAuthResource
+
+@implementer(IInternalAuthResource)
+class CartAdminResource(object):
+    def __init__(self, request):
+        self.request = request
+        
+    __acl__ = [
+        (Allow, 'group:cart_admin', 'cart_admin'), 
+        (Allow, Everyone, 'everybody'),]
+
+    def get_after_login_url(self):
+        return self.request.route_path("whattime.nowsetting.form")
