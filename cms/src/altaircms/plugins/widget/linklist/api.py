697ebf30ea6cb3e7280b792ca9beca432f2ca670
Moriyoshi Koizumi <mozo@mozo.jp>
2013-07-22 10:43:07 +0900

    ある日の 0:00:00 以降に公開になったページに紐づくイベントが「今週発売のチケット」に表示されない問題を修正 fixes #4608

--- cms/src/altaircms/plugins/widget/linklist/api.py
+++ cms/src/altaircms/plugins/widget/linklist/api.py
@@ -1,4 +1,5 @@
 from zope.interface import provider
+from datetime import datetime
 import sqlalchemy as sa
 from .interfaces import IWFinder
 import sqlalchemy.orm as orm
@@ -10,27 +11,27 @@ def get_linklist_candidates_finder(request, key):
     return request.registry.getUtility(IWFinder, key)
 
 @provider(IWFinder)
-def near_the_end_events(request, N, today):
+def near_the_end_events(request, N, now):
+    today = datetime(now.year, now.month, now.day)
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
     qs = qs.join(Page, Page.pageset_id == PageSet.id)
     qs = qs.filter(Page.published == True)
-    # XXX: この条件だと、当日の 0:00:00 以降に公開になったページが対象に含まれなくなってしまうので、today より now がほしい
-    qs = qs.filter(Page.publish_begin < today)
-    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > today))
+    qs = qs.filter(Page.publish_begin <= now)
+    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > now))
     qs = qs.filter(Event.is_searchable==True)
     qs = Event.near_the_deal_close_query(today, N=N, qs=qs)
     qs = qs.order_by(sa.asc(Event.deal_close)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
     return qs
 
 @provider(IWFinder)
-def deal_start_this_week_events(request, N, today):
+def deal_start_this_week_events(request, N, now):
+    today = datetime(now.year, now.month, now.day)
     qs = request.allowable(PageSet).filter(PageSet.event_id==Event.id, PageSet.id==Page.pageset_id, Page.in_term(today))
     qs = qs.join(Page, Page.pageset_id == PageSet.id)
     qs = qs.filter(Page.published == True)
-    # XXX: この条件だと、当日の 0:00:00 以降に公開になったページが対象に含まれなくなってしまうので、today より now がほしい
-    qs = qs.filter(Page.publish_begin < today)
-    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > today))
+    qs = qs.filter(Page.publish_begin <= now)
+    qs = qs.filter((Page.publish_end==None) | (Page.publish_end > now))
     qs = qs.filter(Event.is_searchable==True)
-    qs =  Event.deal_start_this_week_query(today, offset=-today.weekday(), qs=qs)
+    qs = Event.deal_start_this_week_query(today, offset=-today.weekday(), qs=qs)
     qs = qs.order_by(sa.desc(Event.deal_open)).options(orm.joinedload(PageSet.event)).distinct(PageSet.id)
     return qs           
