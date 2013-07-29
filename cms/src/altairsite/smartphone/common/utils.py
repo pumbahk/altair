5b798e5958fb261118566568009af1a4e257878a
okeiichi <keiichi_okada@ticketstar.jp>
2013-07-29 17:17:03 +0900

    スマホのSNSボタンのジャンル部分作成

--- cms/src/altairsite/smartphone/common/utils.py
+++ cms/src/altairsite/smartphone/common/utils.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 from altaircms.page.models import PageSet, Page
+from altaircms.models import _GenrePath
+import sqlalchemy as sa
 
 class SnsUtils(object):
     def __init__(self, request):
@@ -11,6 +13,17 @@ class SnsUtils(object):
             .filter(PageSet.event_id == event_id).first()
         if pageset:
             url = url + pageset.url
+        return url
+
+    def get_sns_url_from_genre(self, genre):
+        url = "http://ticket.rakuten.co.jp/"
+        genre_path = self.request.allowable(_GenrePath)\
+            .filter(_GenrePath.genre_id == genre.id)\
+            .order_by(sa.asc(_GenrePath.hop)).all()
+        for path in genre_path:
+            if not path.next_genre.is_root:
+                url = url + path.next_genre.name + "/"
+        url = url + genre.name
         return url
 
     def get_sns_title(self, event_id):
