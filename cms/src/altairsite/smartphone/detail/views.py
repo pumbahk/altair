973630aa6ddec870753f204074ce6758b8d2bf1d
okeiichi <keiichi_okada@ticketstar.jp>
2013-07-31 19:46:58 +0900

    イベント詳細ページで、存在しないイベントIDが入力された場合エラーになるのを修正 refs #4773

--- cms/src/altairsite/smartphone/detail/views.py
+++ cms/src/altairsite/smartphone/detail/views.py
@@ -6,12 +6,19 @@ from altaircms.event.event_info import get_event_notify_info
 from altairsite.config import smartphone_site_view_config
 from altairsite.mobile.core.helper import get_performances_month_unit, get_purchase_links \
     , get_tickets, get_sales_date
+from altairsite.exceptions import UsersiteException
+
+class ValidationFailure(UsersiteException):
+    pass
 
 @smartphone_site_view_config(route_name='smartphone.detail',request_type="altairsite.tweens.ISmartphoneRequest"
              , renderer='altairsite.smartphone:templates/detail/detail.html')
 def moveDetail(context, request):
     form = DetailForm(request.GET)
     event = context.get_event(form.data['event_id'])
+    if not event:
+        raise ValidationFailure
+
     purchase_links = get_purchase_links(request=request, event=event)
     month_unit = get_performances_month_unit(event=event)
     keys = month_unit.keys()
@@ -36,3 +43,8 @@ def moveDetail(context, request):
             'title':utils.get_sns_title(event_id=event.id)
         }
     }
+
+@smartphone_site_view_config(route_name='smartphone.detail', context=ValidationFailure
+    , request_type="altairsite.tweens.ISmartphoneRequest", renderer='altairsite.smartphone:templates/common/error.html')
+def failed_validation(request):
+    return {}
