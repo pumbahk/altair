4ac316df16360a938d7ea3723f4bbb0051e476db
Atsushi Odagiri <aodagx@gmail.com>
2013-08-01 11:43:21 +0900

    passed altair.app.ticketing.fc_auth

--- ticketing/src/altair/app/ticketing/fc_auth/testing.py
+++ ticketing/src/altair/app/ticketing/fc_auth/testing.py
@@ -1,10 +1,10 @@
-def add_credential(membership, membergroup, username, password):
+def add_credential(membership, membergroup, username, password, organization_short_name="testing"):
     import altair.app.ticketing.core.models
     from altair.app.ticketing.core.models import Organization, Host
     from altair.app.ticketing.users.models import Membership, User, UserCredential, MemberGroup, Member
     
     
-    org = Organization(short_name="testing")
+    org = Organization(short_name=organization_short_name)
     host = Host(host_name="example.com:80", base_url="/login", organization=org)
     ms = Membership(name=membership, organization=org)
     mg = MemberGroup(membership=ms, name=membergroup)
