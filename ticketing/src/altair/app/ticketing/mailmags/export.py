cf9ff52ff3ea4eeaa1272771714669299bfdabe5
Moriyoshi Koizumi <mozo@mozo.jp>
2013-08-14 13:34:58 +0900

    不要コードを削除

--- ticketing/src/altair/app/ticketing/mailmags/export.py
+++ ticketing/src/altair/app/ticketing/mailmags/export.py
@@ -7,25 +7,10 @@ import helpers
 from paste.util.multidict import MultiDict
 from sqlalchemy.sql.expression import or_
 
-from altair.app.ticketing.cart.helpers import format_number as _format_number
 from altair.app.ticketing.mailmags.models import MailSubscription, MailMagazine, MailSubscriptionStatus
 from altair.app.ticketing.utils import dereference
 from altair.app.ticketing.csvutils import CSVRenderer, PlainTextRenderer, CollectionRenderer, AttributeRenderer, SimpleRenderer
 from altair.app.ticketing.orders.api import get_metadata_provider_registry
-
-def format_number(value):
-    return _format_number(float(value))
-
-def _create_mailsubscription_cache(organization_id):
-    D = dict()
-    query = MailSubscription.query\
-        .filter(MailSubscription.segment_id == MailMagazine.id)\
-        .filter(or_(MailSubscription.status == None, MailSubscription.status == MailSubscriptionStatus.Subscribed.v))
-    if organization_id:
-        query = query.filter(MailMagazine.organization_id == organization_id)
-    for ms in query:
-        D[ms.email] = True
-    return D
 
 def one_or_empty(b):
     return u'1' if b else u''
@@ -45,83 +30,6 @@ def get_japanese_columns(request):
             metadata = provider[key]
             retval[u'attribute[%s]' % metadata.key] = metadata.get_display_name('ja_JP')
     return retval
-
-class MarginRenderer(object):
-    def __init__(self, key, column_name):
-        self.key = key
-        self.column_name = column_name
-
-    def __call__(self, record):
-        order = dereference(record, self.key)
-        rendered_value = 0
-        for ordered_product in order.ordered_products:
-            margin = (ordered_product.product.sales_segment.margin_ratio / 100) if ordered_product.product.sales_segment is not None else 0
-            rendered_value += (ordered_product.price * ordered_product.quantity) * margin
-        return [
-            ((u"", self.column_name, u""), unicode(rendered_value))
-        ]
-
-class PerSeatQuantityRenderer(object):
-    def __init__(self, key, column_name):
-        self.key = key
-        self.column_name = column_name
-
-    def __call__(self, record):
-        ordered_product_item = dereference(record, self.key)
-        if ordered_product_item.seats:
-            rendered_value = u"1"
-        else:
-            rendered_value = unicode(ordered_product_item.quantity)
-        return [
-            (
-                (u"", self.column_name, u""),
-                rendered_value
-                )
-            ]
-
-class MailMagazineSubscriptionStateRenderer(object):
-    def __init__(self, key, column_name):
-        self.key = key
-        self.column_name = column_name
-        self.outer = None
-
-    def __call__(self, record):
-        assert self.outer is not None
-        emails = dereference(record, self.key, True) or []
-        return [
-            (
-                (u"", self.column_name, u""),
-                one_or_empty(any(self.outer.mailsubscription_cache.get(email, False) for email in emails))
-                )
-            ]
-
-class CurrencyRenderer(SimpleRenderer):
-    def __call__(self, record):
-        return [
-            ((u'', self.name, u''), unicode(format_number(dereference(record, self.key))))
-            ]
-
-class ZipRenderer(SimpleRenderer):
-    def __call__(self, record):
-        zip = dereference(record, self.key, True)
-        zip = ('%s-%s' % (zip[0:3], zip[3:])) if zip else ''
-        return [
-            ((u'', self.name, u''), zip)
-        ]
-
-class PrintHistoryRenderer(object):
-    def __init__(self, key, column_name):
-        self.key = key
-        self.column_name = column_name
-
-    def __call__(self, record):
-        ordered_product_item = dereference(record, self.key)
-        return [
-            (
-                (u"", self.column_name, u""),
-                u", ".join(list(OrderedDict([(print_history.operator.name, True) for print_history in ordered_product_item.print_histories if print_history.operator is not None])))
-                )
-            ]
 
 class MailMagCSV(object):
     EXPORT_TYPE_MAILMAG = 1
@@ -143,10 +51,7 @@ class MailMagCSV(object):
             raise ValueError('export_type')
 
         def bind(column_renderer):
-            if isinstance(column_renderer, MailMagazineSubscriptionStateRenderer):
-                column_renderer = MailMagazineSubscriptionStateRenderer(column_renderer.key, column_renderer.column_name)
-                column_renderer.outer = self
-            elif isinstance(column_renderer, PlainTextRenderer) and column_renderer.fancy and not excel_csv:
+            if isinstance(column_renderer, PlainTextRenderer) and column_renderer.fancy and not excel_csv:
                 column_renderer = PlainTextRenderer(
                     column_renderer.key,
                     column_renderer.name,
@@ -156,14 +61,7 @@ class MailMagCSV(object):
 
         self.column_renderers = map(bind, column_renderers)
         self.organization_id = organization_id
-        self._mailsubscription_cache = None
         self.localized_columns = localized_columns
-
-    @property
-    def mailsubscription_cache(self):
-        if self._mailsubscription_cache is None:
-            self._mailsubscription_cache = _create_mailsubscription_cache(self.organization_id)
-        return self._mailsubscription_cache
 
     def iter_records(self, mail_subscription):
         common_record = {
