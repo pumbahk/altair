83cebd775ef0d7e4dce0ceb216c6bb53aeaf4e1d
podhmo <podhmo+altair@beproud.jp>
2013-08-14 17:08:38 +0900

    coverが存在しない時警告を出して、表紙を印刷キューに追加しない

--- ticketing/src/altair/app/ticketing/orders/utils.py
+++ ticketing/src/altair/app/ticketing/orders/utils.py
@@ -35,6 +35,7 @@ def enqueue_cover(operator, order):
     cover = TicketCover.get_from_order(order)
     if cover is None:
         logger.error("cover is not found. order = {order.id} organization_id = {operator.organization_id}".format(order=order, operator=operator))
+        return 
     dict_ = build_cover_dict_from_order(order)
     TicketPrintQueueEntry.enqueue(
         operator=operator, 
