6caae746ef19572c2504a557625c113a966dfdb8
Hisashi HATAKEYAMA <me@hatak.jp>
2013-08-17 23:03:10 +0900

    fix maintenance message.

---
+++ maintenance-pages/docroots/RK/mobile.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>琉球ゴールデンキングス キングスチケット 購入履歴確認ページ</title>
+</head>
+<body>
+  <img src="assets/head_title_mobile.jpg" alt="琉球ゴールデンキングス" width="100%" height="108" />
+<div align="center" bgcolor="#9b0020" style="background-color:#9b0020"><font color="#ffffff" size="+1">購入確認ページ</font></div>
+  <?php include '../#common/maintenance_message.php'; ?>
+  <hr />
+  <a href="mailto:kings@tstar.jp">お問い合わせ</a> / <a href="http://www.ticketstar.jp/privacy">個人情報保護方針</a> / <a href="http://www.ticketstar.jp/legal">特定商取引法に基づく表示</a>
+  <div>
+    Copyright (C) 2010-2013 TicketStar, Inc. All Rights Reserved. 
+  </div>
+</body>
+</html>
+
