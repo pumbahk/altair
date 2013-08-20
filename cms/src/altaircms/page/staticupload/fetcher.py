9ea5d0e3b79551882565f84fd665d361ee101d81
dev@ticketstar.jp <dev@ticketstar.jp>
2013-08-20 16:45:31 +0900

    Squashed commit of the following: refs #5011
    commit 13eaa8c3bfa0a5b53e9dab03b7b5450eed908123
    Author: podhmo <podhmo+altair@beproud.jp>
    Date:   Tue Aug 20 14:14:30 2013 +0900
    
        ValueError: insecure stringが出る場合もあった refs #5011

--- cms/src/altaircms/page/staticupload/fetcher.py
+++ cms/src/altaircms/page/staticupload/fetcher.py
@@ -131,7 +131,7 @@ class CachedFetcher(object):
             self.fetcher.check_skip_fetch(url, path)
         try:
             return self.cache_impl[k]
-        except KeyError:
+        except (KeyError, ValueError):
             def fetch_function(cache, k):
                 data = self.fetcher.fetch(url, path)
                 content_type = data.content_type
@@ -203,7 +203,8 @@ class StaticPageCache(object):
         try:
             logger.debug("StaticPageCache: getitem -- {k}".format(k=k))
             return self.file_data[k]
-        except KeyError:
+        except (KeyError, ValueError):
+            ## value error is "insecure string pickle"
             logger.debug("StaticPageCache: notfound -- {k}".format(k=k))
             if k in self.fetching:
                 logger.debug("StaticPageCache: fetching is exists -- {k}".format(k=k))
