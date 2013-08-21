44396bb9300074dd9644f622d3391eb810651aae
podhmo <podhmo+altair@beproud.jp>
2013-08-21 14:36:58 +0900

    使われていない引数を削除

--- ticketing/src/altair/app/ticketing/tickets/cleaner/api.py
+++ ticketing/src/altair/app/ticketing/tickets/cleaner/api.py
@@ -72,13 +72,12 @@ def skip_needless_content(inp):
 #xxx: cleanup_svgはxmltreeを書き換えてしまう。なので２回呼び出すとおかしな結果になってしまう
 def get_validated_svg_cleaner(svgio, exc_class=TicketCleanerValidationError, sej=False):
     xmltree = get_validated_xmltree(svgio, exc_class=exc_class)
-    result = TicketSVGValidator(exc_class=exc_class, sej=sej).validate(svgio, xmltree)
-    return TicketSVGCleaner(svgio, xmltree, result=result)
+    result = TicketSVGValidator(exc_class=exc_class, sej=sej).validate(xmltree)
+    return TicketSVGCleaner(xmltree, result=result)
 
 class TicketSVGCleaner(object):
-    def __init__(self, svgio, xmltree, io_create=StringIO, result=None):
+    def __init__(self, xmltree, io_create=StringIO, result=None):
         self.io_create = io_create
-        self.svgio = svgio
         self.xmltree = xmltree
         self.result = result
 
@@ -115,7 +114,7 @@ class TicketSVGValidator(object):
         self.io_create = io_create
         self.sej = sej
 
-    def validate(self, svgio, xmltree):
+    def validate(self, xmltree):
         default_io = None
         try:
             default_io = io = self._validated_io_on_cleanup_phase(xmltree)
