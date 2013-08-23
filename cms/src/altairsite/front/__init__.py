81c20a2db08829b59a0ca2a7066956dcd6f45f16
podhmo <podhmo+altair@beproud.jp>
2013-08-23 11:19:18 +0900

    読み込み先などを差し替え可能なように修正

--- cms/src/altairsite/front/__init__.py
+++ cms/src/altairsite/front/__init__.py
@@ -8,6 +8,14 @@ def install_resolver(config):
                                    checkskip=True)
     config.registry.registerUtility(layout_lookup, ILayoutModelResolver)
 
+def install_lookupwrapper(config, name="intercept"):
+    from pyramid.mako_templating import IMakoLookup
+    from altairsite.front.renderer import ILookupWrapperFactory
+    from altairsite.front.renderer import LayoutModelLookupWrapperFactory
+    settings = config.registry.settings
+    factory = LayoutModelLookupWrapperFactory(directory_spec=settings["altaircms.layout_directory"], 
+                                              s3prefix="http://tstar-dev.s3.amazonaws.com/cms-layout-templates/")
+    config.registry.adapters.register([IMakoLookup], ILookupWrapperFactory, name=name, value=factory)
    
 def includeme(config):
     """
@@ -16,6 +24,7 @@ def includeme(config):
     """
     config.add_route('front', '{page_name:.*}', factory=".resources.PageRenderingResource")
     config.include(install_resolver)
+    config.include(install_lookupwrapper, "intercept")
     config.scan('.views')
     config.add_view("altairsite.mobile.staticpage.views.staticpage_view", route_name="front", request_type="altairsite.tweens.IMobileRequest")
 
