03c6943fe855b57ef8d4a6e09b2cdafb7a9b606b
podhmo <podhmo+altair@beproud.jp>
2013-09-18 16:08:31 +0900

    asset repository for widget

---
+++ cms/src/altaircms/modelmanager/repository.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+class AssetRepository(object):
+    def __init__(self, request, Model, offset=5):
+        self.request = request
+        self.Model = Model
+        self.offset = offset
+
+    def list_of_asset(self, asset_id, i=1):
+        if asset_id is None:
+            return self.list_of_asset_any(i)
+        return self.list_of_asset_with_selected(asset_id, i)
+
+    def list_of_asset_any(self, i, j=0):
+        qs = self.request.allowable(self.Model)
+        if i > 1:
+            qs = qs.offset(self.offset*(i-1)+j)
+        return list(qs.limit(self.offset))
+        
+    def list_of_asset_with_selected(self, asset_id, i):
+        if i > 1:
+            return self.list_of_asset_any(i, -1)
+        qs = self.request.allowable(self.Model)
+        rest = list(qs.limit(self.offset-1))
+        r = [self.request.allowable(self.Model).get(asset_id)]
+        r.extend(rest)
+        return r
+
+class WidgetRepository(object):
+    def __init__(self, request, Model):
+        self.request = request
+        self.Model = Model
+
+    def _get_or_create(self, model, pk):
+        if pk is None or pk=="null":
+            return model()
+        else:
+            return self.request.allowable(self.Model).get(pk)
+        
+    def get_widget(self, pk):
+        return self._get_or_create(self.Model, pk)
---
+++ cms/src/altaircms/modelmanager/test_repository.py
@@ -0,0 +1,122 @@
+# -*- coding:utf-8 -*-
+import unittest
+from collections import namedtuple
+
+class FakeQuery(object):
+    def __init__(self, xs):
+        self.xs = xs
+
+    def offset(self, i):
+        return self.__class__(self.xs[i:])
+
+    def filter_by(self, id):
+        xs = [x for x in self.xs if x.id == id]
+        return self.__class__(xs)
+
+    def limit(self, N):
+        r = []
+        try:
+            for i in range(N):
+                r.append(self.xs[i])
+        except:
+            return r
+        return r
+
+    def get(self, i):
+        for x in self.xs:
+            if x.id == i:
+                return x
+
+class Request:
+    def __init__(self, xs):
+        self.xs = xs
+    def allowable(self, model):
+        return FakeQuery(self.xs)
+
+FakeAsset = namedtuple("FakeAsset", "id")
+
+class FakeQueryTests(unittest.TestCase):
+    def test_it(self):
+        target = FakeQuery(range(10))
+        self.assertEqual(len(target.xs), 10)
+        self.assertEqual(target.limit(3), [0, 1, 2])        
+
+        paged = target.offset(3)
+        self.assertEqual(len(paged.xs), 7)
+        self.assertEqual(paged.limit(3), [3, 4, 5])
+
+        last_paged = target.offset(9)
+        self.assertEqual(len(last_paged.xs), 1)
+        self.assertEqual(last_paged.limit(3), [9])
+
+def get_assets(N):
+    return [FakeAsset(id=i) for i in range(1, N+1)]    
+
+class AssetRepositoryTests(unittest.TestCase):
+    def _getTarget(self):
+        from altaircms.modelmanager.repository import AssetRepository
+        return AssetRepository
+        
+    def _makeOne(self, *args, **kwargs):
+        return self._getTarget()(*args, **kwargs)
+
+    def test_first__without_selected__page1(self):
+        """[1o, 2o, 3o, 4, 5, 6, 7, 8, 9, 10]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        result = target.list_of_asset(None, 1)
+
+        self.assertEqual(len(result), 3)
+        self.assertEqual([e.id for e in result], [1, 2, 3])
+
+    def test_first__without_selected__page2(self):
+        """[1, 2, 3, 4o, 5o, 6o, 7, 8, 9, 10]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        result = target.list_of_asset(None, 2)
+
+        self.assertEqual(len(result), 3)
+        self.assertEqual([e.id for e in result], [4, 5, 6])
+        
+    def test_first__without_selected__last(self):
+        """[1, 2, 3, 4, 5, 6, 7, 8, 9, 10o]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        result = target.list_of_asset(None, 4)
+
+        self.assertEqual(len(result), 1)
+        self.assertEqual([e.id for e in result], [10])
+
+    def test_first__with_selected__page1(self):
+        """[5o, 1o, 2o, 3, 4, 5, 6, 7, 8, 9, 10]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        asset_id = 5
+        result = target.list_of_asset(asset_id, 1)
+
+        self.assertEqual(len(result), 3)
+        self.assertEqual([e.id for e in result], [5, 1, 2])
+
+    def test_first__with_selected__page2(self):
+        """[5, 1, 2, 3o, 4o, 5o, 6, 7, 8, 9, 10]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        asset_id = 5
+        result = target.list_of_asset(asset_id, 2)
+
+        self.assertEqual(len(result), 3)
+        self.assertEqual([e.id for e in result], [3, 4, 5])
+        
+    def test_first__with_selected__last(self):
+        """[5, 1, 2, 3, 4, 5, 6, 7, 8, 9o, 10o]"""
+        request = Request(get_assets(10))
+        target = self._makeOne(request, FakeAsset, offset=3)
+        asset_id = 5
+        result = target.list_of_asset(asset_id, 4)
+
+        self.assertEqual(len(result), 2)
+        self.assertEqual([e.id for e in result], [9, 10])
+        
+
+if __name__ == "__main__":
+    unittest.main()
