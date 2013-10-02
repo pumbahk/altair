b9042f2ee5f7f78c3960f3bacea6e7f04a15e55d
takeshishimada <takeshishimada@takeshishimada-no-MacBook-Pro.local>
2013-10-02 20:00:12 +0900

    refs #5576 Orderにspecial_fee関連のカラムを追加するようにalembicを修正

--- ticketing/alembic/versions/1c5a9494dd9a_yet_another_system_f.py
+++ ticketing/alembic/versions/1c5a9494dd9a_yet_another_system_f.py
@@ -28,7 +28,16 @@ def upgrade():
                   sa.Column('special_fee_type',
                             sa.Integer(), nullable=False, default=0))
 
+    op.add_column('Order',
+                  sa.Column('special_fee_name',
+                            sa.String(length=255), nullable=False, default=""))
+    op.add_column('Order',
+                  sa.Column('special_fee',
+                            sa.Integer(), nullable=False, default=0))
+
 def downgrade():
     op.drop_column('PaymentDeliveryMethodPair', 'special_fee_name')
     op.drop_column('PaymentDeliveryMethodPair', 'special_fee')
     op.drop_column('PaymentDeliveryMethodPair', 'special_fee_type')
+    op.drop_column('Order', 'special_fee_name')
+    op.drop_column('Order', 'special_fee')
