a527b73efe73333bf747e8dabd0b6ba1f12b45aa
mmatsui <masahiro.matsui@gmail.com>
2013-10-07 12:14:04 +0900

    予約一覧、予約詳細で不正の疑いがある場合はアラート表示する fixes #5359

--- ticketing/src/altair/app/ticketing/orders/__init__.py
+++ ticketing/src/altair/app/ticketing/orders/__init__.py
@@ -29,6 +29,8 @@ def includeme(config):
     config.add_route('orders.refund.confirm'            , '/refund/confirm/')
     config.add_route('orders.refund.immediate'          , '/refund/immediate/{order_id}')
 
+    config.add_route('orders.fraud.clear'               , '/fraud/clear/{order_id}')
+
     config.add_route("orders.cover.preview"             , "/cover/preview/{order_id}/cover")
     config.add_route("orders.item.preview"              , "/item/preview/{order_id}/item/{item_id}")
     config.add_route("orders.item.preview.getdata"      , "/api/item/{item_id}/ticket/{ticket_format_id}")
