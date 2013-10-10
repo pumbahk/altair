3448f5807c9174aab4e2036f10baec604e6c53f5
okeiichi <keiichi_okada@ticketstar.jp>
2013-10-10 15:47:36 +0900

    イーグルスの楽天スーパーポイントPC対応 refs #5598

--- ticketing/src/altair/app/ticketing/users/api.py
+++ ticketing/src/altair/app/ticketing/users/api.py
@@ -39,3 +39,56 @@ def get_or_create_user(authenticated_user):
     credential = user_models.UserCredential(user=user, auth_identifier=auth_identifier, membership=membership)
     DBSession.add(user)
     return user
+
+def get_or_create_user_from_point_no(point):
+    if not point:
+        return None
+
+    credential = user_models.UserCredential.query.filter(
+        user_models.UserCredential.auth_identifier==point
+    ).filter(
+        user_models.UserCredential.membership_id==user_models.Membership.id
+    ).filter(
+        user_models.Membership.name=='rakuten'
+    ).first()
+    if credential:
+        return credential.user
+
+    user = user_models.User()
+    membership = user_models.Membership.query.filter(user_models.Membership.name=='rakuten').first()
+    if membership is None:
+        membership = user_models.Membership(name='rakuten')
+        DBSession.add(membership)
+    credential = user_models.UserCredential(user=user, auth_identifier=point, membership=membership)
+    DBSession.add(user)
+
+    credential = user_models.UserCredential.query.filter(
+        user_models.UserCredential.auth_identifier==point
+    ).first()
+    return credential.user
+
+def create_user_point_account_from_point_no(user_id, point):
+    account_number = ""
+    if point != "":
+        format = "%s-%s-%s-%s"
+        account_number = format % (point[:4], point[4:8], point[8:12], point[12:16])
+
+    acc = user_models.UserPointAccount.query.filter(
+        user_models.UserPointAccount.user_id==user_id
+    ).first()
+
+    if not acc:
+        acc = user_models.UserPointAccount()
+
+    acc.user_id = user_id
+    acc.account_number = account_number
+    acc.type = user_models.UserPointAccountTypeEnum.Rakuten.v
+    acc.status = user_models.UserPointAccountStatusEnum.Valid.v
+    DBSession.add(acc)
+    return acc
+
+def get_user_point_account(user_id):
+    acc = user_models.UserPointAccount.query.filter(
+        user_models.UserPointAccount.user_id==user_id
+    ).first()
+    return acc
