0bc8ad797d432054bcd8f5553b58d7ffd66bf6fd
podhmo <podhmo+altair@beproud.jp>
2013-10-18 19:28:14 +0900

    mail test追加: 共通部分の文面がレンダリング可能かどうか調べるテスト refs #5744
    - 注文番号(受付番号)が含まれているかだけチェックしている
    - PDMPごとの分岐箇所はtest_create_main__pdmp_part.pyで

--- ticketing/src/altair/app/ticketing/printqr/test_functional.py
+++ ticketing/src/altair/app/ticketing/printqr/test_functional.py
@@ -128,6 +128,7 @@ def setup_ordered_product_items(quantity, quantity_only, organization, order_no=
                   system_fee=100, 
                   transaction_fee=200, 
                   delivery_fee=300, 
+                  special_fee=400, 
                   multicheckout_approval_no=":multicheckout_approval_no", 
                   order_no=order_no, 
                   paid_at=datetime(2000, 1, 1, 1, 10), 
