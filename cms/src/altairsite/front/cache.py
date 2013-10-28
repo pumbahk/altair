4d569405cde87db2e178efafddecb015924d7f39
podhmo <podhmo+altair@beproud.jp>
2013-10-28 17:35:30 +0900

    insecure string対応 refs #5881

--- cms/src/altairsite/front/cache.py
+++ cms/src/altairsite/front/cache.py
@@ -5,6 +5,9 @@ from zope.interface import implementer
 from beaker.cache import Cache
 from pyramid.response import Response
 
+import logging
+logger = logging.getLogger(__name__)
+
 class IFrontPageCache(Interface):
     cache = Attribute("cache")
 
@@ -13,14 +16,33 @@ class FrontPageCacher(object):
     def __init__(self, kwargs):
         self.cache = Cache._get_cache("frontpage", kwargs)
 
+    def clear_cache(self, k):
+        try:
+            self.cache.remove_value(k)
+        except (ValueError, EOFError): #insecure string
+            logger.warn("clear_cache: k={k} insecure string found. front page remove".format(k=k))
+            handler = self.cache._get_value(k).namespace
+            handler.do_remove()
+        except Exception as e:
+            logger.error(repr(e))
+            logger.warn("clear_cache: k={k} insecure string found. remove".format(k=k))
+            handler = self.cache._get_value(k).namespace
+            handler.do_remove()
+  
     def __getitem__(self, k):
         try:
             return self.cache[k]
-        except (KeyError, ValueError, EOFError):
+        except (KeyError, ValueError, EOFError) as e:
+            logger.warn(repr(e))
+            self.clear_cache(k)
             return None
 
     def __setitem__(self, k, v):
-        self.cache[k] = v
+        try:
+            self.cache[k] = v
+        except (KeyError, ValueError, EOFError) as e:
+            logger.warn(repr(e))
+            self.clear_cache(k)
 
 def with_cached_view(prefix):
     def _with_cached_view(view):
