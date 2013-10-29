34558eb14b211b065fa0a8f950e3f620578a776c
podhmo <podhmo+altair@beproud.jp>
2013-10-29 18:11:32 +0900

    add test

--- cms/src/altairsite/front/tests/test_cache.py
+++ cms/src/altairsite/front/tests/test_cache.py
@@ -1,7 +1,46 @@
 # -*- coding:utf-8 -*-
+import mock
 import unittest
 from pyramid import testing
 from pyramid.response import Response
+
+def make_smartphone_request(request):
+    from altair.mobile.interfaces import ISmartphoneRequest
+    from zope.interface import directlyProvides
+    directlyProvides(request, ISmartphoneRequest)
+    return request
+
+def make_mobile_request(request):
+    from altair.mobile.interfaces import IMobileRequest
+    from zope.interface import directlyProvides
+    directlyProvides(request, IMobileRequest)
+    return request
+
+
+class LookupTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+        self.config.include("altairsite.front.install_page_key_generator")
+
+    def _callFUT(self, request):
+        from ..cache import get_key_generator
+        return get_key_generator(request)
+
+    def test_pc_request(self):
+        request = testing.DummyRequest()
+        result = self._callFUT(request)
+        self.assertEquals(result.prefix, "P:")
+
+    def test_mobile_request(self):
+        request = make_mobile_request(testing.DummyRequest())
+        result = self._callFUT(request)
+        self.assertEquals(result.prefix, "M:")
+
+    def test_smartphone_request(self):
+        request = make_smartphone_request(testing.DummyRequest())
+        result = self._callFUT(request)
+        self.assertEquals(result.prefix, "S:")
+        
 
 class Tests(unittest.TestCase):
     def setup_fixture(self, config):
