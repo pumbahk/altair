c8431e089cbc258bcaf0c91f9d94935e0df0543b
podhmo <podhmo+altair@beproud.jp>
2013-11-01 15:13:16 +0900

    add somehting for dict tree

---
+++ ticketing/src/altair/app/ticketing/core/test_utils.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import unittest
+
+def _split_by_dot(s):
+    return s.split(".")
+
+class TreeDictFromFlattenTests(unittest.TestCase):
+    def _callFUT(self, *args, **kwargs):
+        from .utils import tree_dict_from_flatten
+        return tree_dict_from_flatten(*args, **kwargs)
+
+    def test_simple0(self):
+        result = self._callFUT({"a": "x", "b": "y"}, _split_by_dot)
+        self.assertEquals(result, {"a": "x", "b": "y"})
+
+    def test_it0(self):
+        d = {"a": "x", "b.c.d.e": "y"}
+        result = self._callFUT(d, _split_by_dot)
+        self.assertEquals(result, {"a": "x", "b": {"c": {"d": {"e": "y"}}}})
+
+    def test_it(self):
+        d = {"a": "x", "b.c.d.e": "y", "b.c.d2": "z"}
+        result = self._callFUT(d, _split_by_dot)
+        self.assertEquals(result, {"a": "x", "b": {"c": {"d": {"e": "y"}, "d2": "z"}}})
+
+class MergeDictRecursiveTests(unittest.TestCase):
+    def _callFUT(self, *args, **kwargs):
+        from .utils import merge_dict_recursive
+        return merge_dict_recursive(*args, **kwargs)
+
+    def test_simple0(self):
+        result = self._callFUT({}, {})
+        self.assertEqual(result, {})
+
+    def test_simple1(self):
+        d1 = {"k": "v"}
+        d2 = {}
+        result = self._callFUT(d1, d2)
+        self.assertEqual(result, {"k": "v"})
+
+    def test_simple2(self):
+        d1 = {}
+        d2 = {"k": "v"}
+        result = self._callFUT(d1, d2)
+        self.assertEqual(result, {"k": "v"})
+
+    def test_it(self):
+        d1 = {'k1': {'k2': 2, 'k5': 5}}
+        d2 = {'k1': {'k2': {'k3': 3}}, 'k4': 4}
+        result = self._callFUT(d1, d2)
+        self.assertEqual(result, {'k1': {'k2': {'k3': 3}, 'k5': 5}, 'k4': 4})
+
+    def test__list_is_replace(self):
+        d1 = {'k1': {'k2': [2]}}
+        d2 = {'k1': {'k2': [3]}}
+        result = self._callFUT(d1, d2)
+        self.assertEqual(result, {'k1': {'k2': [3]}})
--- ticketing/src/altair/app/ticketing/core/utils.py
+++ ticketing/src/altair/app/ticketing/core/utils.py
@@ -10,6 +10,31 @@ from .modelmanage import (
     IssuedPrintedAtSetter, 
 )
 
+def tree_dict_from_flatten(d, sep):
+    result = {}
+    for master_key in d:
+        ks = sep(master_key)
+        if len(ks) <= 1:
+            result[master_key] = d[master_key]
+        else:
+            target = result
+            for k in ks[:-1]:
+                if not k in target:
+                    target[k] = {}
+                target = target[k]
+            target[ks[-1]] = d[master_key]
+    return result
+
+def merge_dict_recursive(d1, d2): #muttable!. not support list. only atom and dict.
+    target = d1
+    for k, v in d2.iteritems():
+        sub = target.get(k, {})
+        if hasattr(v, "iteritems") and hasattr(sub, "iteritems"):
+            merge_dict_recursive(sub, v)
+        else:
+            target[k] = v
+    return d1
+
 class PageURL_WebOb_Ex(object):
     def __init__(self, request, encode_type="utf-8", qualified=False):
         self.request = request
