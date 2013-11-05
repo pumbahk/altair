4447e0bb95851a6a2c8b82178ab358b9381ceae1
podhmo <podhmo+altair@beproud.jp>
2013-11-05 13:45:36 +0900

    メールからのリンクの場合にgateの情報が表示されていなかった refs #6005

--- ticketing/src/altair/app/ticketing/orderreview/views.py
+++ ticketing/src/altair/app/ticketing/orderreview/views.py
@@ -101,6 +101,11 @@ def order_review_qr_html(context, request):
     
     if ticket == None or ticket.sign != sign:
         raise HTTPNotFound()
+
+    if ticket.seat is None:
+        gate = None
+    else:
+        gate = ticket.seat.attributes.get("gate", None)
     
     return dict(
         sign = sign,
@@ -109,6 +114,7 @@ def order_review_qr_html(context, request):
         performance = ticket.performance,
         event = ticket.event,
         product = ticket.product,
+        gate = gate
     )
 
 @view_config(route_name='order_review.qrdraw', xhr=False, permission="view")
