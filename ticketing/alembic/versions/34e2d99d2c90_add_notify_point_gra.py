1f9f93bff241de3a45a3836fdcefb81a18983828
okeiichi <keiichi_okada@ticketstar.jp>
2013-12-04 20:13:31 +0900

    alembicが動作していなかったので修正

--- ticketing/alembic/versions/34e2d99d2c90_add_notify_point_gra.py
+++ ticketing/alembic/versions/34e2d99d2c90_add_notify_point_gra.py
@@ -1,14 +1,14 @@
 """add_notify_point_grainting_failure_to_organization_setting
 
 Revision ID: 34e2d99d2c90
-Revises: f0a2ca58baa
+Revises: 1748d0eee30b
 Create Date: 2013-11-12 20:27:34.333856
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '34e2d99d2c90'
-down_revision = 'f0a2ca58baa'
+down_revision = '1748d0eee30b'
 
 from alembic import op
 import sqlalchemy as sa
