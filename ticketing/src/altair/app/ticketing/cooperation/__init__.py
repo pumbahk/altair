c37e3dbcec2fac0fcbc380151436937b14961d76
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2013-12-05 17:42:04 +0900

    refs #5874 オーガス連携 add_configに対してテストが記述できるように修正

--- ticketing/src/altair/app/ticketing/cooperation/__init__.py
+++ ticketing/src/altair/app/ticketing/cooperation/__init__.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from altair.app.ticketing import newRootFactory
-from .resources import DownloadResource
+from .resources import (
+    DownloadResource,
+    UploadResource,
+    )
+
+
+ROUTE_URL_RESOURCE = {
+    'cooperation.index': ('/', None),
+    'cooperation.show': ('/show/{venue_id}', None),
+    'cooperation.upload': ('/upload/{venue_id}', UploadResource),
+    'cooperation.download': ('/download/{venue_id}', DownloadResource),
+    }
+
 
 def includeme(config):
-    config.add_route('cooperation.index', '/')
-    config.add_route('cooperation.show', '/show/{venue_id}')
-    config.add_route('cooperation.upload', '/upload/{venue_id}')
-
-    download_factory = newRootFactory(DownloadResource)
-    config.add_route('cooperation.download', '/download/{venue_id}', factory=download_factory)
+    for route, values in ROUTE_URL_RESOURCE.items():
+        url, resource_class = values
+        kwds = {}
+        if resource_class:
+            kwds['factory'] = newRootFactory(resource_class)
+        config.add_route(route, url, **kwds)
     config.scan('.')
