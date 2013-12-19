4bdccf49d5248ea9cda750e4d68dee5dafb36d23
okeiichi <keiichi_okada@ticketstar.jp>
2013-12-19 16:57:04 +0900

    抽選スマホ　乃木坂キーワードを修正 refs #6157

--- ticketing/src/altair/app/ticketing/lots/smartphone_views.py
+++ ticketing/src/altair/app/ticketing/lots/smartphone_views.py
@@ -134,7 +134,7 @@ class EntryLotView(object):
 
     @view_config(route_name='lots.entry.index', renderer=selectable_renderer("smartphone/%(membership)s/index.html"), request_method="POST", custom_predicates=(is_nogizaka, ))
     def nogizaka_auth(self):
-        KEYWORD = '1AkdIcUy923eaCak7Q64kDu9ydjGe8S3'
+        KEYWORD = 'thYsXctE2q8UbnZKCqs7QZjBdcgpVq3a'
         keyword = self.request.POST.get('keyword', None)
         if keyword or self.request.session.get('lots.passed.keyword') != KEYWORD:
             if keyword != KEYWORD:
