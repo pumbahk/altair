7c1c86eccbd39fce29bf2c735649716b049e3915
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2013-12-24 22:54:01 +0900

    refs #6347 追券時に枠を作成するように修正

--- ticketing/src/altair/app/ticketing/cooperation/scripts/augus_distribution.py
+++ ticketing/src/altair/app/ticketing/cooperation/scripts/augus_distribution.py
@@ -1,41 +1,90 @@
 #! /usr/bin/env python
 #-*- coding: utf-8 -*-
 import os
-import argparse
+import itertools
+import transaction
+from pyramid.paster import bootstrap
+from altair.app.ticketing.core.models import (
+    Performance,
+    StockHolder,
+    AugusPerformance,
+    AugusSeat,
+    AugusVenue,
+    )
 from altair.augus.protocols import (
     DistributionSyncRequest,
     DistributionSyncResponse,
     )
 from altair.augus.parsers import AugusParser
+from .utils import (
+    mkdir_p,
+    get_argument_parser,
+    get_settings,
+    )
 
-def get_settings(conf=None): 
-    if conf:
-        raise NotImplementedError()
-    else:
-        from pit import Pit
-        return Pit.get('augus_ftp',
-                       {'require': {'staging': '',
-                                    'pending': '',
-                                    }})
-def mkdir_p(path):
-    if not os.path.isdir(path):
-        os.makedirs(path)
+class Importer(object):
+    def import_(self, record):
+        ap = AugusPerformance.query\
+                .filter(AugusPerformance.event_code==record.event_code)\
+                .filter(AugusPerformance.code==record.performance_code)\
+                .one() # no exist error
+        performance = ap.performance
+        event = performance.event
+        if not performance:
+            raise 'NO PERFORMANCE'
+        stock_holder = StockHolder()
+        stock_holder
+        style = '{"text": "\u8ffd", "text_color": "#a62020"}'
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--conf', default=None)
+    parser = get_argument_parser()
     args = parser.parse_args()
+    bootstrap(args.conf)    
     settings = get_settings(args.conf)
     staging = settings['staging']
     pending = settings['pending']
     
     mkdir_p(staging)
     mkdir_p(pending)
-
+    #importer = Impoter()
     for name in filter(DistributionSyncRequest.match_name, os.listdir(staging)):
         path = os.path.join(staging, name)
         request = AugusParser.parse(path)
-        for record in request:
-            pass
+        _get_key = lambda rec: (rec.event_code, rec.performance_code)
+        records = sorted(request, key=lambda rec: _get_key)
+        for (ag_event_code, ag_performance_code), recs in itertools.groupby(records, _get_key):
+            ap = AugusPerformance.query\
+                     .filter(AugusPerformance.augus_event_code==ag_event_code)\
+                     .filter(AugusPerformance.code==ag_performance_code)\
+                     .one() # raise no exist error
+            if not ap.performance:
+                raise ValueError('No link to performance: AugusPerformance.code={}'\
+                                 .format(ap.code))
+            stock_holder = StockHolder()
+            stock_holder.name = u'オーガス追券 12/24'
+            stock_holder.event_id = ap.performance.event.id
+            stock_holder.stype = u'{"text": "\u8ffd", "text_color": "#a62020"}'
+            stock_holder.account_id = 35
+            stock_holder.save()
+
+            # for rec in recs:
+            #     augus_venue = AugusVenue.query.filter(AugusVenue.venue_id==rec.venue_code).one()
+            #     augus_seat = AugusSeat.query\
+            #                      .filter(AugusSeat.augus_venue_id==augus_venue.id)\
+            #                      .filter(AugusSeat.area_code==rec.area_code)\
+            #                      .filter(AugusSeat.info_code==rec.info_code)\
+            #                      .filter(AugusSeat.floor==rec.floor.decode('sjis'))\
+            #                      .filter(AugusSeat.column==rec.column==rec.column.decode('sjis'))\
+            #                      .filter(AugusSeat.number==rec.number.decode('sjis'))\
+            #                      .one()
+            #     seat = Seat.query\
+            #                .filter(Seat.l0_id==augus_seat.seat.l0_id)\
+            #                .filter(Seat.venue_id==ap.performance.venue.id)\
+            #                .one()
+            #     stock = seat.stock
+            #     stock.stock_holder_id = stock_holder.id
+            #     stock.save()
+    transaction.commit()
+
 if __name__ == '__main__':
     main()
