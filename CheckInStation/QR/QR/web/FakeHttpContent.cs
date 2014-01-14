05283d089d5136dce296b073933846d0e2c68fbb
podhmo <podhmo@be.jp>
2014-01-14 18:03:39 +0900

    http requestのfake objectがデフォルトではutf8ではなくasciiとして利用されていた

--- CheckInStation/QR/QR/web/FakeHttpContent.cs
+++ CheckInStation/QR/QR/web/FakeHttpContent.cs
@@ -17,10 +17,11 @@ namespace QR
 		}
 
 		protected async override Task SerializeToStreamAsync (Stream stream,
-		                                                     TransportContext context)
+		                                                      TransportContext context)
 		{
-			byte[] byteArray = Encoding.ASCII.GetBytes (Content);
-			await stream.WriteAsync (byteArray, 0, Content.Length);
+			//byte[] byteArray00 = Encoding.ASCII.GetBytes (Content); //we need UTF-8 string, usually
+			byte[] byteArray = Encoding.UTF8.GetBytes (Content); //without BOM 
+			await stream.WriteAsync (byteArray, 0, byteArray.Length);
 		}
 
 		protected override bool TryComputeLength (out long length)
