65ed1012832b500ad72f8e41894ba328aaede04f
dev@ticketstar.jp <dev@ticketstar.jp>
2014-01-16 11:37:47 +0900

    HOTFIX: 購入情報ダウンロードの制限外しの修正 refs #6650

--- ticketing/src/altair/app/ticketing/orders/views.py
+++ ticketing/src/altair/app/ticketing/orders/views.py
@@ -617,9 +617,10 @@ class Orders(BaseView):
             ordered_term = None
             if form_search.ordered_from.data and form_search.ordered_to.data:
                 ordered_term = form_search.ordered_to.data - form_search.ordered_from.data
-            if query.count() > 5000 and not form_search.performance_id.data and (not ordered_term or ordered_term.days <= 1):
-                self.request.session.flash(u'対象件数が多すぎます。(予約期間を1日にするか、公演を指定すれば制限はありません)')
-                raise HTTPFound(location=route_path('orders.index', self.request))
+            if not form_search.performance_id.data and (ordered_term is None or ordered_term.days > 0):
+                if query.count() > 5000:
+                    self.request.session.flash(u'対象件数が多すぎます。(予約期間を1日にするか、公演を指定すれば制限はありません)')
+                    raise HTTPFound(location=route_path('orders.index', self.request))
     
         # XXX: JOINしたら逆に遅くなった
         #query = query.options(
