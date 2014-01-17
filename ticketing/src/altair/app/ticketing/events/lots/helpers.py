34df63a973b25cc4157d1b69e9b884c9c8e206cd
okeiichi <keiichi_okada@ticketstar.jp>
2014-01-17 11:17:12 +0900

    抽選予定時間帯の表示 refs #5784

--- ticketing/src/altair/app/ticketing/events/lots/helpers.py
+++ ticketing/src/altair/app/ticketing/events/lots/helpers.py
@@ -1,5 +1,5 @@
 from webhelpers.html.tags import link_to
-from .api import get_lotting_announce_timezone
+from altair.app.ticketing.lots.helpers import timezone_label
 
 class Link(object):
     def __init__(self, label, url, **attrs):
@@ -12,12 +12,3 @@ class Link(object):
 
     def __str__(self):
         return self.__html__()
-
-def timezone_label(lot):
-    label = ""
-    if lot.custom_timezone_label:
-        label = lot.custom_timezone_label
-    else:
-        if lot.lotting_announce_timezone:
-            label = get_lotting_announce_timezone(lot.lotting_announce_timezone)
-    return label
--- ticketing/src/altair/app/ticketing/lots/api.py
+++ ticketing/src/altair/app/ticketing/lots/api.py
@@ -524,3 +524,10 @@ def create_client_form(context):
             formdata = None
 
         return schemas.ClientForm(formdata=formdata)
+
+def get_lotting_announce_timezone(timezone):
+    label = u""
+    labels = dict(morning=u'午前', day=u'昼以降', evening=u'夕方以降', night=u'夜', next_morning=u'明朝')
+    if timezone in labels:
+        label = labels[timezone]
+    return label
