efd32a323834e6c3a7e16c9575f48c2b3ef949d5
podhmo <podhmo@be.jp>
2014-01-20 13:44:46 +0900

    ordernoの方も

--- CheckInStation/QR/QR/flow/CaseOrdernoConfirmAll.cs
+++ CheckInStation/QR/QR/flow/CaseOrdernoConfirmAll.cs
@@ -17,16 +17,17 @@ namespace QR
 
 		public TicketDataCollection Collection { get; set; }
 
-		protected bool fetchStatus;
+		protected TokenStatus tokenStatus;
 
 		public CaseOrdernoConfirmForAll (IResource resource, VerifiedOrdernoRequestData requestData) : base (resource)
 		{
 			this.RequestData = requestData;
+			this.tokenStatus = TokenStatus.valid;
 		}
 
 		public override async Task PrepareAsync (IInternalEvent ev)
 		{
-			await base.PrepareAsync (ev).ConfigureAwait(false);
+			await base.PrepareAsync (ev).ConfigureAwait (false);
 			try {
 				var data = new TicketDataCollectionRequestData () {
 					order_no = this.RequestData.order_no,
@@ -35,27 +36,33 @@ namespace QR
 				ResultTuple<string, TicketDataCollection> result = await Resource.TicketDataCollectionFetcher.FetchAsync (data);
 				if (result.Status) {
 					this.Collection = result.Right;
-					this.fetchStatus = true;
+					this.tokenStatus = result.Right.status;
 				} else {
 					//modelからpresentation層へのメッセージ
 					PresentationChanel.NotifyFlushMessage ((result as Failure<string,TicketDataCollection>).Result);
-					this.fetchStatus = false;
+					this.tokenStatus = TokenStatus.unknown;
 				}
 			} catch (Exception ex) {
 				logger.ErrorException (":", ex);
 				PresentationChanel.NotifyFlushMessage (MessageResourceUtil.GetTaskCancelMessage (Resource));
-				this.fetchStatus = false;
+				this.tokenStatus = TokenStatus.unknown;
 			}
 		}
 
 		public override Task<bool> VerifyAsync ()
 		{
-			return Task.Run (() => this.fetchStatus);
+			return Task.Run (() => this.tokenStatus == TokenStatus.valid);
 		}
 
 		public override ICase OnSuccess (IFlow flow)
 		{
 			return new CasePrintForAll (Resource, this.Collection);
+		}
+
+		public override ICase OnFailure (IFlow flow)
+		{
+			flow.Finish ();
+			return new CaseFailureRedirect (this.Resource, this.Resource.GetTokenStatusMessage (this.tokenStatus));
 		}
 	}
 }
