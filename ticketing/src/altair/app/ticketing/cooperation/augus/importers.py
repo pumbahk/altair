e024184623fa6a0f7c640263ffc30917e5d104a9
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-01-21 21:03:23 +0900

    refs #6682 AugusDistributionImpoterを追加

--- ticketing/src/altair/app/ticketing/cooperation/augus/importers.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/importers.py
@@ -83,3 +83,42 @@ class AugusTicketImpoter(object):
     def import_(self, protocol):
         return self.import_record_all(protocol)
 
+
+class AugusDistributionImpoter(object):
+    def import_record(self, record):
+        ag_ticket = AugusTicket.get(augus_event_code=record.event_code,
+                                    augus_performance_code=record.performance_code,
+                                    augus_seat_type_code=record.seat_type_code,
+                                    )
+        ag_performance = None
+        if not ag_ticket:
+            ag_ticket = AugusTicket()
+            ag_performance = AugusPerformance.get(augus_event_code=record.event_code,
+                                                  augus_performance_code=record.performance_code,
+                                                  )
+            if not ag_performance:
+                raise AugusDataImportError('AugusPerformance not found: event_code={} performance_code'.format(
+                    record.event_code, record.performance_code))
+        else:
+            ag_performance = ag_ticket.ag_performance
+        ag_ticket.augus_venue_code = record.venue_code
+        ag_ticket.seat_type_code = record.seat_type_code
+        ag_ticket.seat_type_name = record.seat_type_name
+        ag_ticket.unit_value_name = record.unit_value_name
+        ag_ticket.augus_seat_type_classif = record.seat_type_classif
+        ag_ticket.avlue = record.value
+        ag_ticket.augus_performance_id = ag_performance.id
+        ag_ticket.save()
+        return ag_ticket
+        
+        
+    def import_record_all(self, records):
+        elms = []
+        for record in records:
+            elm = self.import_record(record)
+            elms.append(elm)
+        return elms
+
+    def import_(self, protocol):
+        return self.import_record_all(protocol)
+
