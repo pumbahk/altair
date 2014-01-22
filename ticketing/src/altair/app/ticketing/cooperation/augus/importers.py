a09d6f76f4f6cafaa6a13a4b5a40773e639cc627
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-01-22 11:52:01 +0900

    refs #6682 Augus追券処理の割当時の不具合を修正

--- ticketing/src/altair/app/ticketing/cooperation/augus/importers.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/importers.py
@@ -1,13 +1,22 @@
 #-*- coding: utf-8 -*-
+import itertools
 from altair.app.ticketing.core.models import (
+    Event,
+    Performance,
+    StockHolder,
     AugusPerformance,
     AugusVenue,
     AugusTicket,
+    AugusSeat,
     )
-from .errors import AugusDataImportError
+from .errors import (
+    AugusDataImportError,
+    NoSeatError,
+    )
 
 class AugusPerformanceImpoter(object):
     def import_record(self, record):
+        
         ag_venue = AugusVenue\
             .query.filter(AugusVenue.code==record.venue_code)\
                   .filter(AugusVenue.version==record.venue_version)\
@@ -58,7 +67,7 @@ class AugusTicketImpoter(object):
                                                   augus_performance_code=record.performance_code,
                                                   )
             if not ag_performance:
-                raise AugusDataImportError('AugusPerformance not found: event_code={} performance_code'.format(
+                raise AugusDataImportError('AugusPerformance not found: event_code={} performance_code={}'.format(
                     record.event_code, record.performance_code))
         else:
             ag_performance = ag_ticket.ag_performance
@@ -85,40 +94,115 @@ class AugusTicketImpoter(object):
 
 
 class AugusDistributionImpoter(object):
-    def import_record(self, record):
-        ag_ticket = AugusTicket.get(augus_event_code=record.event_code,
-                                    augus_performance_code=record.performance_code,
-                                    augus_seat_type_code=record.seat_type_code,
-                                    )
-        ag_performance = None
-        if not ag_ticket:
-            ag_ticket = AugusTicket()
-            ag_performance = AugusPerformance.get(augus_event_code=record.event_code,
-                                                  augus_performance_code=record.performance_code,
-                                                  )
-            if not ag_performance:
-                raise AugusDataImportError('AugusPerformance not found: event_code={} performance_code'.format(
-                    record.event_code, record.performance_code))
-        else:
-            ag_performance = ag_ticket.ag_performance
-        ag_ticket.augus_venue_code = record.venue_code
-        ag_ticket.seat_type_code = record.seat_type_code
-        ag_ticket.seat_type_name = record.seat_type_name
-        ag_ticket.unit_value_name = record.unit_value_name
-        ag_ticket.augus_seat_type_classif = record.seat_type_classif
-        ag_ticket.avlue = record.value
-        ag_ticket.augus_performance_id = ag_performance.id
-        ag_ticket.save()
-        return ag_ticket
+    def import_record(self, record, stock, ag_performance):
+        ag_seat = ge_augus_seat(ag_venue=ag_performance.augus_venue,
+                                area_code=record.area_code,
+                                info_code=record.info_code,
+                                floor=record.floor,
+                                column=record.column,
+                                number=record.number,
+                                )
+        seat = augus_seat_to_real_seat(ag_performance, ag_seat)
+        old_stock = seat.stock
         
+        if old_stock.stock_holder == None and seat.status not in [SeatStatusEnum.NotOnSale.v, SeatStatusEnum.Vacant.v]:
+            # 未割当 かつ 配席可能な状態            
+            seat.stock_id = stock.id
+            old_stock.quantity -= 1
+            seat.save()
+            return seat
+        else:
+            raise AugusDataImportError('Cannot seat allocation: seat_id={}'.format(seat.id))
+        
+    def _create_stock_holder(self, event):
+        stock_holder = StockHolder()
+        stock_holder.name = u'オーガス連携:' + time.strftime('%Y-%m-%d-%H-%M-%S')
+        stock_holder.event_id = event.id
+        stock_holder.stype = u'{"text": "\u8ffd", "text_color": "#a62020"}'
+        stock_holder.save()
+        return stock_holder
         
     def import_record_all(self, records):
-        elms = []
-        for record in records:
-            elm = self.import_record(record)
-            elms.append(elm)
-        return elms
+        allcated_seats = []
+        for augus_event_code, _records in itertools.groupby(records, lambda record: record.augus_event_code):
+            ag_performance = AugusPerformance\
+                .query.filter(AugusPerformance.augus_event_code==augus_event_code)\
+                      .first()
+            if not ag_performance:
+                raise AugusDataImportError(
+                    'AugusPerformance not found: event_code={}'.format(
+                        record.event_code))
+            if not  ag_performance.performance:
+                raise AugusDataImportError(
+                    'AugusPerformance not linked to Performance: AugusPerformance.id={}, '.format(
+                        ag_performance.id))
+            stock_holder = self._create_stock_holder(ag_performance.performance.event)
+            stocks = filter(lambda stock: stock.performance_id==ag_performance.performance.id, stock_holder.stocks)
+            
+            for augus_performance_code, _n_records in itertools.groupby(_records, lambda record: record.performance_code):
+                ag_performance = AugusPerformance.get(augus_event_code=augus_event_code,
+                                                      augus_performance_code=augus_performance_code,
+                                                      )
+                if not  ag_performance:
+                    raise AugusDataImportError(
+                        'AugusPerformance not found: event_code={} performance_code={}'.format(
+                            augus_event_code, augus_performance_code))
+                    
+                if not  ag_performance.performance:
+                    raise AugusDataImportError(
+                        'AugusPerformance not linked to Performance: AugusPerformance.id={}, '.format(
+                            ag_performance.id))
 
+                for augus_seat_type_code, _p_records in itertools.groupby(_n_records, lambda record: record.seat_type_code):
+                    ag_ticket = None
+                    try:
+                        ag_ticket = AugusTicket.query.filter(AugusTicket.augus_performance_id==ag_performance.performance.id)\
+                                                     .filter(AugusTicket.augus_seat_type_code==augus_seat_type_code)\
+                                                     .one()
+                    except:
+                        raise
+                    if not ag_ticket.stock_type:
+                        raise AugusDataImportError(
+                            'AugusTicket not linked to StockType: AugusTicket.id={}, '.format(
+                                ag_ticket.id))
+                    
+                    stock = None
+                    for stock in stocks:
+                        if stock.stock_type_id == ag_ticket.stock_type.id:
+                            break
+                    else:
+                        raise AugusDataImportError(
+                            'AugusTicket not linked to StockType: AugusTicket.id={}, '.format(
+                                ag_ticket.id))
+
+                    for record in _p_records:
+                        seat = self.import_record(record, stock, ag_performance)
+                        allocated_seats.append(seat)
+                    if stock:
+                        stock.save()
+        return allocated_seats
+        
     def import_(self, protocol):
         return self.import_record_all(protocol)
 
+    @staticmethod
+    def get_augus_seat(ag_venue, area_code, info_code, floor, column, number):
+        return AugusSeat.query.filter(AugusSeat.augus_venue_id==ag_venue.id)\
+                              .filter(AugusSeat.area_code==area_code)\
+                              .filter(AugusSeat.info_code==info_code)\
+                              .filter(AugusSeat.floor==floor)\
+                              .filter(AugusSeat.column==column)\
+                              .filter(AugusSeat.number==number)\
+                              .one()
+
+    @staticmethod
+    def augus_seat_to_real_seat(ag_performance, ag_seat):
+        base_seat = ag_seat.seat
+        performance = ag_performance.performance
+        seat = Seat.get(l0_id=base_seat.l0_id,
+                        venue_id=performance.venue_id,
+                        )
+        if seat:
+            return seat
+        else:
+            raise NoSeatError()
