e45a7905e0dcf1ac701ed17f53ffd5866fa910e4
dev@ticketstar.jp <dev@ticketstar.jp>
2014-02-01 01:46:39 +0900

    HOTFIX: #6791

--- ticketing/src/altair/app/ticketing/orderreview/api.py
+++ ticketing/src/altair/app/ticketing/orderreview/api.py
@@ -58,11 +58,10 @@ def safe_get_contact_url(request, default=""):
         return default
 
 def send_to_orion(request, context, recipient):
-    settings = get_current_registry().settings
+    settings = request.registry.settings
     api_url = settings.get('orion.create_url')
     if api_url is None:
-        raise Exception()
-    
+        raise Exception("orion.api_uri is None")
     
     # request.params['order_no']
     data = OrderedProductItemToken.filter_by(id = request.params['token']).one()
@@ -77,45 +76,39 @@ def send_to_orion(request, context, recipient):
     seat = data.seat
     orion = performance.orion
     
-    try:
-        obj = dict()
-        obj['recipient'] = dict(mail = recipient)
-        obj['order'] = dict(number = order.order_no, created_at = str(order.paid_at))
-        obj['performance'] = dict(code = performance.code, name = performance.name, open_on = str(performance.open_on), start_on = str(performance.start_on), end_on = str(performance.end_on))
-        obj['performance']['event'] = dict(code = event.code, title = event.title, abbreviated_title = event.abbreviated_title)
-        obj['performance']['site'] = dict(name = site.name, prefecture = site.prefecture, address = (site.city or u'')+(site.address_1 or u'')+(site.address_2 or u''), phone = site.tel_1)
-        obj['performance']['organization'] = dict(code = org.code, name = org.name)
-        obj['performance']['web'] = orion.web
-        obj['segment'] = dict(name = segment.name)
-        obj['product'] = dict(name = product.name, price = int(product.price))
-        obj['seat'] = dict(name = seat.name, type = seat.stock.stock_type.name, number = seat.seat_no)
-        for k, v in seat.attributes.items():
-            if k not in obj['seat']:
-                obj['seat'][k] = v
-        obj['coupons'] = list()
-        obj['coupons'].append(dict(name = data.seat.name, qr = (orion.qr_enabled==1), pattern = orion.pattern))
-        if (not orion.coupon_2_name is None) and (not orion.coupon_2_name == u''):
-            obj['coupons'].append(dict(name = orion.coupon_2_name, qr = (orion.coupon_2_qr_enabled==1), pattern = orion.coupon_2_pattern))
-        obj['instruction'] = dict(general = orion.instruction_general,
-                                  performance = orion.instruction_performance)
-        obj['theme'] = dict(header = orion.header_url,
-                            background = orion.background_url,
-                            icon = orion.icon_url)
-    except Exception as e:
-        return "data exception[%s]" % str(e)
+    obj = dict()
+    obj['recipient'] = dict(mail = recipient)
+    obj['order'] = dict(number = order.order_no, created_at = str(order.paid_at))
+    obj['performance'] = dict(code = performance.code, name = performance.name, open_on = str(performance.open_on), start_on = str(performance.start_on), end_on = str(performance.end_on))
+    obj['performance']['event'] = dict(code = event.code, title = event.title, abbreviated_title = event.abbreviated_title)
+    obj['performance']['site'] = dict(name = site.name, prefecture = site.prefecture, address = (site.city or u'')+(site.address_1 or u'')+(site.address_2 or u''), phone = site.tel_1)
+    obj['performance']['organization'] = dict(code = org.code, name = org.name)
+    obj['performance']['web'] = orion.web
+    obj['segment'] = dict(name = segment.name)
+    obj['product'] = dict(name = product.name, price = int(product.price))
+    obj['seat'] = dict(name = seat.name, type = seat.stock.stock_type.name, number = seat.seat_no)
+    for k, v in seat.attributes.items():
+        if k not in obj['seat']:
+            obj['seat'][k] = v
+    obj['coupons'] = list()
+    obj['coupons'].append(dict(name = data.seat.name, qr = (orion.qr_enabled==1), pattern = orion.pattern))
+    if (not orion.coupon_2_name is None) and (not orion.coupon_2_name == u''):
+        obj['coupons'].append(dict(name = orion.coupon_2_name, qr = (orion.coupon_2_qr_enabled==1), pattern = orion.coupon_2_pattern))
+    obj['instruction'] = dict(general = orion.instruction_general,
+                              performance = orion.instruction_performance)
+    obj['theme'] = dict(header = orion.header_url,
+                        background = orion.background_url,
+                        icon = orion.icon_url)
     
-    try:
-        data = json.dumps(obj)
-        print data
-        req = urllib2.Request(api_url, data, headers={ u'Content-Type': u'text/json; charset="UTF-8"' })
-        stream = urllib2.urlopen(req);
-        headers = stream.info()
-        if stream.code == 200:
-            res = unicode(stream.read(), 'utf-8')
-            return res
-        return "failed[%u]" % stream.code
-    except Exception as e:
-        return "http exception[%s]" % str(e)
+    data = json.dumps(obj)
+    req = urllib2.Request(api_url, data, headers={ u'Content-Type': u'text/json; charset="UTF-8"' })
+    stream = urllib2.urlopen(req);
+    headers = stream.info()
+    if stream.code == 200:
+        res = unicode(stream.read(), 'utf-8')
+        return res
+    else:
+        raise Exception("server returned unexpected status: %d (payload) %r" % (stream.code, stream.read()))
 
 def is_mypage_organization(context, request):
     organization = c_api.get_organization(request)
