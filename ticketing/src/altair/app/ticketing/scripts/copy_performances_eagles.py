0bc139c4b1e71fe7970f83b476781132a167c0dd
dev@ticketstar.jp <dev@ticketstar.jp>
2014-02-07 07:40:12 +0900

    SalesSegmentの値をコピー後更新するように修正 (スクリプトのみ)

--- ticketing/src/altair/app/ticketing/scripts/copy_performances_eagles.py
+++ ticketing/src/altair/app/ticketing/scripts/copy_performances_eagles.py
@@ -21,6 +21,8 @@ from sqlalchemy.orm.exc import NoResultFound
 
 import sqlahelper
 
+from altair.app.ticketing.events.sales_segments.resources import SalesSegmentAccessor
+
 logger = logging.getLogger(__name__)
 
 formats = {
@@ -166,6 +168,9 @@ def do_performance_copy(request, session, file_, encoding, format, dry_run=False
                 order_limit=new_performance_max_orders,
                 max_quantity_per_user=new_performance_max_quantity_per_user
                 )
+            accessor = SalesSegmentAccessor()
+            for sales_segment in new_performance.sales_segments:
+                accessor.update_sales_segment(sales_segment)
             new_performance.save()
             session.flush()
             message('new performance: Performance(id=%d, title=%s, code=%s)' % (new_performance.id, new_performance.name, new_performance.code))
