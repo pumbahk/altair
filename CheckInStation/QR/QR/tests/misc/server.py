42300da9d5530161d59fb21e28814fc88e568311
podhmo <podhmo@be.jp>
2014-02-12 15:27:42 +0900

    微妙なwait timeが必要

--- CheckInStation/QR/QR/tests/misc/server.py
+++ CheckInStation/QR/QR/tests/misc/server.py
@@ -3,17 +3,19 @@ import SimpleHTTPServer
 import SocketServer
 import logging
 import cgi
-
+import time
 PORT = 8000
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        logging.error(self.headers)
+        logging.info(self.headers)
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
-        logging.error(self.headers)
+        logging.info(self.headers)
+        import time
+        time.sleep(0.2)
         # form = cgi.FieldStorage(
         #     fp=self.rfile,
         #     headers=self.headers,
