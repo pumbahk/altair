5638ae89bf2e2aa242734aa7baae22decaaaf228
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-02-18 10:32:34 +0900

    コミット漏れ

---
+++ ticketing/alembic/versions/521506639053_add_unit_value_code.py
@@ -0,0 +1,25 @@
+"""add_unit_value_code
+
+Revision ID: 521506639053
+Revises: 2cda4724cc40
+Create Date: 2014-02-18 10:24:37.310366
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '521506639053'
+down_revision = '2cda4724cc40'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import functions as sqlf
+
+Identifier = sa.BigInteger
+
+def upgrade():
+    op.add_column('AugusStockInfo',
+                  sa.Column('unit_value_code', sa.Integer(), nullable=True))
+
+def downgrade():
+    op.drop_column('AugusStockInfo', 'unit_value_code')
