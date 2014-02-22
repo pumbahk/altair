d4fe22794dee0ff68f4ffa3d5f8f0e830a857cd3
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-02-22 11:25:23 +0900

    販売実勢戻しの修正

--- ticketing/src/altair/app/ticketing/cooperation/augus/exporters.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/exporters.py
@@ -136,7 +136,59 @@ class AugusPutbackExporter(object):
         return responses
 
 class AugusAchievementExporter(object):
-    def create_record(self, stock_info):
+    def create_record(self, seat):
+        if seat.status in [SeatStatusEnum.NotOnSale.v, SeatStatusEnum.Vacant.v, SeatStatusEnum.Canceled.v]:
+            return
+
+        ordered_product_item = self.seat2opitem(seat)
+        if not ordered_product_item:
+            raise AugusDataExportError('No such OrderedProductItem: Seat.id={}'.formamt(stock_info.seat))
+
+        augus_ticket= ordered_product_item.ordered_product.product.augus_ticket
+        if not augus_ticket:
+            return
+
+        augus_stock_detail = None
+        for augus_stock_detail in augus_ticket.augus_stock_detals:
+            if augus_stock_detail.augus_stock_info.seat.id == seat.id:
+                break
+        else:
+            raise AugusDataExportError('No such AugusStockDetail Seat.id={}'.formamt(stock_info.seat))
+
+        record = AchievementResponse.record()
+        record.event_code = augus_stock_detail.augus_event_code
+        record.performance_code = augus_stock_detail.augus_peformance_code
+        record.distribution_code = augus_stock_detail.augus_distribution_code
+        record.seat_type_code = augus_stock_detail.augus_ticket.augus_seat_type_code
+        record.unit_value_code = augus_stock_detail.augus_ticket.unit_value_code
+        record.date = augus_stock_detail.augus_performance.start_on.strftime(DateType.FORMAT)
+        record.start_on = augus_stock_detail.augus_performance.start_on.strftime(HourMinType.FORMAT)
+        record.reservation_number = self.get_order_no(ordered_product_item)
+        record.block = augus_stock_detail.augus_seat.block
+        record.coordy = augus_stock_detail.augus_seat.coordy
+        record.coordx = augus_stock_detail.augus_seat.coordx
+        record.area_code = augus_stock_detail.augus_seat.area_code
+        record.info_code = augus_stock_detail.augus_seat.info_code
+        record.floor = augus_stock_detail.augus_seat.floor
+        record.column = augus_stock_detail.augus_seat.column
+        record.number = augus_stock_detail.augus_seat.num
+        record.seat_type_classif = augus_stock_detail.seat_type_classif
+        record.seat_count = augus_stock_detail.quantity
+        record.unit_value = augus_stock_detail.augus_ticket.value
+        #record.unit_value = self.get_unit_price(opitem) # こっちはaltairで実際に販売した単価(おおよそ (altairでは席の単価は厳密には出せない))
+        record.processed_at = time.strftime('%Y%m%d%H%M%S')
+        record.achievement_status = str(AugusSeatStatus.get_status(seat))
+        return record
+
+    def export(self, performance):
+        res = AchievementResponse()
+        for seat in performance.seats:
+            record = self.create_record(seat)
+            if record:
+                res.append(record)
+        return res
+
+    def _create_record(self, stock_info):
         opitem = self.seat2opitem(stock_info.seat)
         if not opitem:
             raise AugusDataExportError('No such OrderedProductItem: Seat.id={}'.formamt(stock_info.seat))
@@ -169,7 +221,7 @@ class AugusAchievementExporter(object):
         return record
 
 
-    def export_from_augus_performance(self, augus_performance):
+    def _export_from_augus_performance(self, augus_performance):
         res = AchievementResponse()
         res.event_code = augus_performance.augus_event_code
         res.date = augus_performance.start_on
@@ -188,7 +240,7 @@ class AugusAchievementExporter(object):
         return res
 
 
-    def export_from_augus_event_code(self, augus_event_code):
+    def _export_from_augus_event_code(self, augus_event_code):
         augus_performances = AugusPerformance\
             .query\
             .filter(AugusPerformance.augus_event_code==augus_event_code)\
