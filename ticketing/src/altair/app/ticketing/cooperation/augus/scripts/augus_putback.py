ceaedffb0a9f983bad9232b9e1c71a2c7ee7bbd2
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-02-26 21:17:02 +0900

    refs #7225 Augus連携関連メール通知 返券処理

--- ticketing/src/altair/app/ticketing/cooperation/augus/scripts/augus_putback.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/scripts/augus_putback.py
@@ -2,10 +2,15 @@
 #-*- coding: utf-8 -*-
 import os
 import argparse
+import transaction
+from pyramid.paster import bootstrap
+from pyramid.renderers import render_to_response
+from altair.app.ticketing.core.models import (
+    Mailer,
+    AugusPutback,
+    )
 from altair.augus.protocols import TicketSyncRequest
 from altair.augus.parsers import AugusParser
-from pyramid.paster import bootstrap
-import transaction
 from ..exporters import AugusPutbackExporter
 from ..errors import AugusDataImportError
 from .. import multilock
@@ -26,8 +31,10 @@ def export_putback_all(settings):
     mkdir_p(ko_staging)
 
     exporter = AugusPutbackExporter()
+    putback_codes = []
     try:
-        request = exporter.export(ko_staging, consumer_id)
+        responses = exporter.export(ko_staging, consumer_id)
+        putback_codes = [res[0].putback_code for res in responses if len(res)]
     except AugusDataImportError as err:
         transaction.abort()
         raise
@@ -36,6 +43,7 @@ def export_putback_all(settings):
         raise
     else:
         transaction.commit()
+    return putback_codes
 
 def main():
     parser = argparse.ArgumentParser()
@@ -43,15 +51,34 @@ def main():
     args = parser.parse_args()
     env = bootstrap(args.conf)
     settings = env['registry'].settings
-
-
+    putback_codes = []
     try:
         with multilock.MultiStartLock('augus_putback'):
-            export_putback_all(settings)
+            putback_codes = export_putback_all(settings)
     except multilock.AlreadyStartUpError as err:
         logger.warn('{}'.format(repr(err)))
-        return
+    else:
+        ticket_count = len(augus_ticket_ids)
+        if 0 == ticket_count:
+            return
+        sender = settings['mail.augus.sender']
+        recipient = settings['mail.augus.recipient']
 
+        augus_putbacks = AugusPutback\
+            .query\
+            .filter(AugusPutback.augus_putback_code.in_(augus_putback_codes))\
+            .order_by(AugusPutback.augus_performance_id)\
+            .all()
+        mailer = Mailer(settings)
+        params = {'augus_putbacks': augus_putbacks}
+        body = render_to_response('altair.app.ticketing:templates/cooperation/augus/mails/augus_putback.html', params)
+        mailer.create_message(
+            sender=sender,
+            recipient=recipient,
+            subject=u'【オーガス連携】返券のお知らせ',
+            body=body.text,
+        )
+        mailer.send(sender, [recipient])
 
 if __name__ == '__main__':
     main()
