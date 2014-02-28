19b50a4ef3cccef740e481d5ec90f231954f9443
Moriyoshi Koizumi <mozo@mozo.jp>
2014-02-28 17:42:27 +0900

    SalesSegment.start_at が None のときに予約詳細が見れない fixes #7281

--- ticketing/src/altair/app/ticketing/core/helpers.py
+++ ticketing/src/altair/app/ticketing/core/helpers.py
@@ -10,7 +10,7 @@ def build_sales_segment_list_for_inner_sales(sales_segments, now=None):
     def sales_segment_sort_key_func(ss):
         return (
             ss.kind == u'sales_counter',
-            ss.start_at <= now,
+            ss.start_at is None or ss.start_at <= now,
             ss.end_at is None or now <= ss.end_at,
             -ss.start_at.toordinal() if ss.start_at else 0,
             ss.id
