0a56d0b2cb5688c69bfeecab4f31e945dfaef192
podhmo <podhmo@be.jp>
2014-03-10 18:23:47 +0900

    assert追加

--- ticketing/src/altair/app/ticketing/print_progress/progress.py
+++ ticketing/src/altair/app/ticketing/print_progress/progress.py
@@ -29,9 +29,11 @@ class PrintProgressGetter(object):
         self.organization = organization
 
     def get_event_progress(self, event):
+        assert event.organization_id == self.organization_id
         return EventPrintProgress(event)
 
     def get_performance_progress(self, performance):
+        assert performance.event.organization_id == self.organization_id
         return PerformancePrintProgress(performance)
 
 class DummyPrintProgress(object):
