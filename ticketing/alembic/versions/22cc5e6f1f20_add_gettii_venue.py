782008404cadbf05bccb1309e1b33c7eb4bb1a86
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-03-10 11:48:39 +0900

    refs #7310 gettii <-> altair 用のseatの対応表追加

--- ticketing/alembic/versions/22cc5e6f1f20_add_gettii_venue.py
+++ ticketing/alembic/versions/22cc5e6f1f20_add_gettii_venue.py
@@ -22,10 +22,36 @@ def upgrade():
     op.create_table(
         'GettiiVenue',
         sa.Column('id', Identifier, primary_key=True),
+        sa.Column('code', sa.Integer, nullable=False),
+        # links
+        sa.Column('venue_id', Identifier, nullable=False),
         )
+
     op.create_table(
         'GettiiSeat',
         sa.Column('id', Identifier, primary_key=True),
+        sa.Column('no', sa.Unicode(32), nullable=False),
+        sa.Column('coordx', sa.Unicode(32), nullable=False),
+        sa.Column('coordy', sa.Unicode(32), nullable=False),
+        sa.Column('posx', sa.Unicode(32), nullable=False),
+        sa.Column('posy', sa.Unicode(32), nullable=False),
+        sa.Column('floor', sa.Unicode(32), nullable=False),
+        sa.Column('column', sa.Unicode(32), nullable=False),
+        sa.Column('num', sa.Unicode(32), nullable=False),
+        sa.Column('block', sa.Unicode(32), nullable=False),
+        sa.Column('gate', sa.Unicode(32), nullable=False),
+        sa.Column('priority_floor', sa.Unicode(32), nullable=False),
+        sa.Column('priority_area_code', sa.Unicode(32), nullable=False),
+        sa.Column('priority_block', sa.Unicode(32), nullable=False),
+        sa.Column('priority_seat', sa.Unicode(32), nullable=False),
+        sa.Column('seat_flag', sa.Unicode(32), nullable=False),
+        sa.Column('seat_classif', sa.Unicode(32), nullable=False),
+        sa.Column('net_block', sa.Unicode(32), nullable=False),
+        sa.Column('modified_by', sa.Unicode(32), nullable=False),
+        sa.Column('modified_at', sa.Unicode(32), nullable=False),
+        # links
+        sa.Column('gettii_venue_id', Identifier, nullable=False),
+        sa.Column('seat_id', Identifier, nullable=False),
         )
 
 def downgrade():
