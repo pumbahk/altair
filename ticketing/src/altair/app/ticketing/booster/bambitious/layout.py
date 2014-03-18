23952c916e19386de0279c4738cbfbfda90cccf3
okeiichi <keiichi_okada@ticketstar.jp>
2014-03-18 16:44:31 +0900

    バンビシャスブースターのメンテナンスページ refs #7322

--- ticketing/src/altair/app/ticketing/booster/bambitious/layout.py
+++ ticketing/src/altair/app/ticketing/booster/bambitious/layout.py
@@ -6,7 +6,7 @@ class Layout(object):
         self.context = context
         self.request = request
 
-    title = u"入力フォーム"
+    title = u"入会受付"
     mini_title = u"バンビシャス奈良 ブースタークラブ クラブバンビシャス入会申込ページ"
     orderreview_title = u"バンビシャス奈良 ブースタークラブ クラブバンビシャス申込受付確認ページ"
     complete_title = u"バンビシャス奈良 ブースタークラブ クラブバンビシャス入会申込"
