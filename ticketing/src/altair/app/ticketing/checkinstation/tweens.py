ebe17041f1feb7bb6540a4085848ba52c668087f
podhmo <podhmo@be.jp>
2014-03-25 11:36:19 +0900

    指定した値と証明書から取得した値が一致するか比較するようにした refs #7551

--- ticketing/src/altair/app/ticketing/checkinstation/tweens.py
+++ ticketing/src/altair/app/ticketing/checkinstation/tweens.py
@@ -1,20 +1,25 @@
 # -*- coding:utf-8 -*-
 import logging
 logger = logging.getLogger(__name__)
-from pyramid.settings import asbool
+
 from pyramid.httpexceptions import HTTPBadRequest 
 from altair.app.ticketing.login.main.utils import get_auth_identifier_from_client_certified_request
 
 def client_certified_tween(handler, registry):
     ## todo: enable, disable
-    if asbool(registry.settings.get("altair.checkinstation.enable.client_certificate", False)):
-        logger.info("altair.checkinstation.enable.client_certificate is enabled.")
+    expected_identifier = registry.settings.get("altair.checkinstation.client.certificate.identifier", None)
+    if expected_identifier:
+        logger.info("altair.checkinstation.client.certificate.identifier found. using client certificate.")
         def tween(request):
             auth_identifier = get_auth_identifier_from_client_certified_request(request)
             if auth_identifier is None:
                 logger.warn("client certificate is not found")
                 return HTTPBadRequest(u"E@:クライアント証明書が掲示されていません")
-            return handler(request)
+            elif auth_identifier.startswith(expected_identifier):
+                return handler(request)
+            else:
+                logger.info("certificate fail expected = actual : %s = %s", expected_identifier, auth_identifier)
+                return HTTPBadRequest(u"E@:クライアント証明書が不正です")
         return tween
     else:
         def disable_tween(request):
