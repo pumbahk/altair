32dae0f7e963f229c75142165f8eaac661f3a4d9
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-03-26 11:18:39 +0900

    refs #7450 未連携会場に対する考慮漏れ

--- ticketing/src/altair/app/ticketing/cooperation/augus/utils.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/utils.py
@@ -11,7 +11,7 @@ from altair.app.ticketing.core.models import (
 from .errors import (
     NoSeatError,
     BadRequest,
-    IllegalDataError,    
+    IllegalDataError,
     )
 logger = logging.getLogger(__name__)
 
@@ -52,19 +52,20 @@ class SeatAugusSeatPairs(object):
         return self.__iter__
 
     def __iter__(self):
-        logger.debug('AUGUS VENUE: start creating external seat pairs')        
+        logger.debug('AUGUS VENUE: start creating external seat pairs')
+        venue = self._venue
         ex_venue = self._augus_venue
         seat_id__ex_seat = {}
         if ex_venue:
             if ex_venue.venue.id != self._venue.id:
                 raise IllegalDatabaseError('Illegal data of AugusVenue: Venue.id={} AugusVenue.id={}'.format(self._venue.id, ex_venue.id))
             venue = ex_venue.venue
-            logger.debug('AUGUS VENUE: create seat_id ex_seat dictionary')                
+            logger.debug('AUGUS VENUE: create seat_id ex_seat dictionary')
             seat_id__ex_seat = dict([(ex_seat.seat_id, ex_seat)
                                      for ex_seat in ex_venue.augus_seats if ex_seat.seat_id is not None])
             logger.debug('AUGUS VENUE: create seat_id ex_seat dictionary')
         for seat in venue.seats:
-            yield seat, seat_id__ex_seat.get(seat.id, None) 
+            yield seat, seat_id__ex_seat.get(seat.id, None)
 
     def _find_augus_seat(self): # co routine
         augus_seats = self.get_augus_seats()
@@ -257,5 +258,3 @@ def get_settings(conf=None):
                                     'staging': '',
                                     'pending': '',
                                     }})
-
-
