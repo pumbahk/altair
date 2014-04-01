51ce80a1dafd775d430741951bd283c35eddf9e3
podhmo <podhmo@be.jp>
2014-04-02 07:25:10 +0900

    not add_route. add_endpoint_route refs #7627

--- ticketing/src/altair/app/ticketing/checkinstation/refresh/__init__.py
+++ ticketing/src/altair/app/ticketing/checkinstation/refresh/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
 def includeme(config):
     config.add_route("refresh.token", "/refresh/token/{token_id}")
-    config.add_route("refresh.order", "/refresh/order_no/{order_no}")
+    config.add_endpoint_route("refresh.order", "/refresh/order")
     config.scan(".views")
