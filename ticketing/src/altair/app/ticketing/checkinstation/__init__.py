a6f6bc17f3e68d64ef190c08cecdc7d9396a3956
Moriyoshi Koizumi <mozo@mozo.jp>
2014-04-10 11:53:19 +0900

    CS動いてなかった... すいません

--- ticketing/src/altair/app/ticketing/checkinstation/__init__.py
+++ ticketing/src/altair/app/ticketing/checkinstation/__init__.py
@@ -55,6 +55,8 @@ def main(global_config, **local_config):
 
     config = Configurator(settings=settings, 
                           root_factory=".resources.CheckinStationResource")
+    config.include('altair.app.ticketing.setup_beaker_cache')
+    config.include('altair.httpsession.pyramid')
     config.add_renderer('.html' , 'pyramid.mako_templating.renderer_factory')
     config.add_renderer('lxml'  , 'altair.app.ticketing.renderers.lxml_renderer_factory')
 
