197a8506ad8f41d1037050c389d1c4b2c217e20d
podhmo <podhmo@be.jp>
2014-04-15 15:47:03 +0900

    add test

---
+++ ticketing/src/altair/app/ticketing/tickets/preview/test_zipdownload.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+import tempfile
+import logging
+import zipfile
+import os
+logger = logging.getLogger(__name__)
+
+import unittest
+
+class ZipDownloadTests(unittest.TestCase):
+    def _getTarget(self):
+        from altair.app.ticketing.tickets.preview.views import DownloadListOfPreviewImage
+        return DownloadListOfPreviewImage
+
+    def test_zip_create(self):
+        context = None
+        request = None
+        target = self._getTarget()(context, request)
+        tmpdir = tempfile.mkdtemp()
+        with open(os.path.join(tmpdir, "sample.txt"), "w") as wf:
+            wf.write("sample")
+        walk = target.crate_zip_file_creator("sample.zip", tmpdir)
+        walk()
+
+        self.assertTrue(os.path.exists(os.path.join(tmpdir, "sample.zip")))
+        self.assertTrue(zipfile.iszipfile(os.path.join(tmpdir, "sample.zip")))
+
+    def test_zip_create__with_monkey_patched(self):
+        import altair.app.ticketing.sej.zip_file
+        import zipfile
+        self.assertNotEqual(zipfile.structCentralDir, "<4s4B4HL2L5H2L")
+
+        context = None
+        request = None
+        target = self._getTarget()(context, request)
+        tmpdir = tempfile.mkdtemp()
+        with open(os.path.join(tmpdir, "sample.txt"), "w") as wf:
+            wf.write("sample")
+        walk = target.crate_zip_file_creator("sample.zip", tmpdir)
+        walk()
+
+        self.assertTrue(os.path.exists(os.path.join(tmpdir, "sample.zip")))
+        self.assertTrue(zipfile.iszipfile(os.path.join(tmpdir, "sample.zip")))
