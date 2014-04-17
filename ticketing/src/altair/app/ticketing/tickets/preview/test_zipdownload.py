187938b942568489bad78feaee483cc90903dfac
Moriyoshi Koizumi <mozo@mozo.jp>
2014-04-17 17:08:41 +0900

    fix test

--- ticketing/src/altair/app/ticketing/tickets/preview/test_zipdownload.py
+++ ticketing/src/altair/app/ticketing/tickets/preview/test_zipdownload.py
@@ -25,21 +25,3 @@ class ZipDownloadTests(unittest.TestCase):
 
         self.assertTrue(os.path.exists(zippath))
         self.assertTrue(zipfile.is_zipfile(zippath))
-
-    def test_zip_create__with_monkey_patched(self):
-        import altair.app.ticketing.sej.zip_file
-        import zipfile
-        self.assertNotEqual(zipfile.structCentralDir, "<4s4B4HL2L5H2L")
-
-        context = None
-        request = None
-        target = self._getTarget()(context, request)
-        tmpdir = tempfile.mkdtemp()
-        with open(os.path.join(tmpdir, "sample.txt"), "w") as wf:
-            wf.write("sample")
-        zippath = tempfile.mktemp("sample.zip")
-        walk = target.create_zip_file_creator(zippath, tmpdir)
-        walk()
-
-        self.assertTrue(os.path.exists(zippath))
-        self.assertTrue(zipfile.is_zipfile(zippath))
