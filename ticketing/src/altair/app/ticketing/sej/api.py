f1e23957fb1b0e2851444a686e7ee97c30b881a5
ticketstar-dev <dev@ticketstar.jp>
2014-04-17 11:26:07 +0900

    DBエラーなど未ハンドルの例外が発生したときにSejErrorでラップするようにした

--- ticketing/src/altair/app/ticketing/sej/api.py
+++ ticketing/src/altair/app/ticketing/sej/api.py
@@ -17,7 +17,7 @@ from .models import SejNotification, SejOrder, SejTicket, SejRefundEvent, SejRef
 from .models import _session
 from .helpers import create_hash_from_x_start_params
 from .interfaces import ISejTenant
-from .exceptions import SejServerError, SejError
+from .exceptions import SejServerError, SejError, SejErrorBase
 from .payment import request_cancel_order, request_order, request_update_order
 from pyramid.threadlocal import get_current_registry
 from pyramid.interfaces import IRequest
@@ -26,19 +26,31 @@ def do_sej_order(request, tenant, sej_order, now=None, session=None):
     if session is None:
         session = _session
     try:
-        return request_order(request, tenant, sej_order)
-    finally:
-        session.merge(sej_order)
-        session.commit()
+        try:
+            return request_order(request, tenant, sej_order)
+        finally:
+            session.merge(sej_order)
+            session.commit()
+    except SejErrorBase:
+        raise
+    except Exception as e:
+        logger.exception(u'unhandled exception')
+        raise SejError(u'generic failure (reason: %s)' % unicode(e), sej_order.order_no)
 
 def refresh_sej_order(request, tenant, sej_order, update_reason, now=None, session=None):
     if session is None:
         session = _session
     try:
-        return request_update_order(request, tenant, sej_order, update_reason)
-    finally:
-        session.merge(sej_order)
-        session.commit()
+        try:
+            return request_update_order(request, tenant, sej_order, update_reason)
+        finally:
+            session.merge(sej_order)
+            session.commit()
+    except SejErrorBase:
+        raise
+    except Exception as e:
+        logger.exception(u'unhandled exception')
+        raise SejError(u'generic failure (reason: %s)' % unicode(e), sej_order.order_no)
 
 def cancel_sej_order(request, tenant, sej_order, now=None, session=None):
     if session is None:
@@ -48,15 +60,21 @@ def cancel_sej_order(request, tenant, sej_order, now=None, session=None):
     if sej_order.shop_id != tenant.shop_id:
         raise SejError(u'SejOrder.shop_id (%s) != SejTenant.shop_id (%s)' % (sej_order.shop_id, tenant.shop_id), sej_order.order_no)
     try:
-        request_cancel_order(
-            request,
-            tenant=tenant,
-            sej_order=sej_order,
-			now=now
-        )
-    finally:
-        session.merge(sej_order)
-        session.commit()
+        try:
+            request_cancel_order(
+                request,
+                tenant=tenant,
+                sej_order=sej_order,
+                now=now
+            )
+        finally:
+            session.merge(sej_order)
+            session.commit()
+    except SejErrorBase:
+        raise
+    except Exception as e:
+        logger.exception(u'unhandled exception')
+        raise SejError(u'generic failure (reason: %s)' % unicode(e), sej_order.order_no)
 
 def refund_sej_order(request, tenant, sej_order, performance_name, performance_code, performance_start_on, per_order_fee, per_ticket_fee, ticket_price_getter, refund_start_at, refund_end_at, ticket_expire_at, now, session=None):
     if session is None:
@@ -69,52 +87,57 @@ def refund_sej_order(request, tenant, sej_order, performance_name, performance_c
         raise SejError(u'No tickets associated', sej_order.order_no)
 
     try:
-        # create SejRefundEvent
-        re = session.query(SejRefundEvent).filter(and_(
-            SejRefundEvent.shop_id==tenant.shop_id,
-            SejRefundEvent.event_code_01==performance_code
-        )).first()
-        if not re:
-            re = SejRefundEvent()
-
-        re.available = 1
-        re.shop_id = tenant.shop_id
-        re.event_code_01 = performance_code
-        re.title = performance_name
-        re.event_at = performance_start_on
-        re.start_at = refund_start_at
-        re.end_at = refund_end_at
-        re.event_expire_at = refund_end_at
-        re.ticket_expire_at = ticket_expire_at
-        re.refund_enabled = 1
-        re.need_stub = 1
-        session.merge(re)
-
-        # create SejRefundTicket
-        for i, sej_ticket in enumerate(sej_tickets):
-            rt = session.query(SejRefundTicket).filter(and_(
-                SejRefundTicket.order_no==sej_order.order_no,
-                SejRefundTicket.ticket_barcode_number==sej_ticket.barcode_number
+        try:
+            # create SejRefundEvent
+            re = session.query(SejRefundEvent).filter(and_(
+                SejRefundEvent.shop_id==tenant.shop_id,
+                SejRefundEvent.event_code_01==performance_code
             )).first()
-            if not rt:
-                rt = SejRefundTicket()
-                session.add(rt)
+            if not re:
+                re = SejRefundEvent()
 
-            rt.available = 1
-            rt.refund_event_id = re.id
-            rt.event_code_01 = performance_code
-            rt.order_no = sej_order.order_no
-            rt.ticket_barcode_number = sej_ticket.barcode_number
-            rt.refund_ticket_amount = ticket_price_getter(sej_ticket)
-            rt.refund_other_amount = per_ticket_fee
-            # 手数料などの払戻があったら1件目に含める
-            if per_order_fee > 0 and i == 0:
-                rt.refund_other_amount += per_order_fee
+            re.available = 1
+            re.shop_id = tenant.shop_id
+            re.event_code_01 = performance_code
+            re.title = performance_name
+            re.event_at = performance_start_on
+            re.start_at = refund_start_at
+            re.end_at = refund_end_at
+            re.event_expire_at = refund_end_at
+            re.ticket_expire_at = ticket_expire_at
+            re.refund_enabled = 1
+            re.need_stub = 1
+            session.merge(re)
 
-            session.merge(rt)
-    finally:
-        session.commit()
+            # create SejRefundTicket
+            for i, sej_ticket in enumerate(sej_tickets):
+                rt = session.query(SejRefundTicket).filter(and_(
+                    SejRefundTicket.order_no==sej_order.order_no,
+                    SejRefundTicket.ticket_barcode_number==sej_ticket.barcode_number
+                )).first()
+                if not rt:
+                    rt = SejRefundTicket()
+                    session.add(rt)
 
+                rt.available = 1
+                rt.refund_event_id = re.id
+                rt.event_code_01 = performance_code
+                rt.order_no = sej_order.order_no
+                rt.ticket_barcode_number = sej_ticket.barcode_number
+                rt.refund_ticket_amount = ticket_price_getter(sej_ticket)
+                rt.refund_other_amount = per_ticket_fee
+                # 手数料などの払戻があったら1件目に含める
+                if per_order_fee > 0 and i == 0:
+                    rt.refund_other_amount += per_order_fee
+
+                session.merge(rt)
+        finally:
+            session.commit()
+    except SejErrorBase:
+        raise
+    except Exception as e:
+        logger.exception(u'unhandled exception')
+        raise SejError(u'generic failure (reason: %s)' % unicode(e), sej_order.order_no)
 
 def get_sej_order(order_no, session=None):
     if session is None:
