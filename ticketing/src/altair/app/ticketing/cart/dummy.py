8d599b3d0c2ce70016782c7c049036b08ac256b8
podhmo <podhmo@be.jp>
2014-04-23 15:26:46 +0900

    fix dummy.py

--- ticketing/src/altair/app/ticketing/cart/dummy.py
+++ ticketing/src/altair/app/ticketing/cart/dummy.py
@@ -85,6 +85,7 @@ def _dummy_order():
     order.transaction_fee = 100
     order.system_fee = 200
     order.delivery_fee = 300
+    order.special_fee = 0
     order.total_amount = 500
     order.payment_delivery_pair.payment_method.payment_plugin_id = 1
     order.payment_delivery_pair.delivery_method.delivery_plugin_id = 1
@@ -98,6 +99,7 @@ def _dummy_cart():
     cart.transaction_fee = 100
     cart.system_fee = 200
     cart.delivery_fee = 300
+    cart.special_fee = 0
     cart.total_amount = 500
     cart.payment_delivery_pair.payment_method.payment_plugin_id = 1
     cart.payment_delivery_pair.delivery_method.delivery_plugin_id = 1
