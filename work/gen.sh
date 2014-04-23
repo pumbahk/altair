d4aab0c7441fd651fdcabfc9f0172ed88cde1878
podhmo <podhmo@be.jp>
2014-04-23 11:09:10 +0900

    s3://tstarにuploadしようとするdefault良くない

--- work/gen.sh
+++ work/gen.sh
@@ -17,7 +17,7 @@ gensh="python `pwd`/work/replace.py"
 ($gensh $dst/cart.json > $dst/cart.sh)
 
 
-bucket="tstar/"
+bucket="tstar-internal-dev/"
 gen_upload_s3cmd_sh="python `pwd`/work/upload.py"
 ($gen_upload_s3cmd_sh $dst/fc_auth.json $bucket > $dst/s3_fc_auth.sh)
 ($gen_upload_s3cmd_sh $dst/orderreview.json $bucket > $dst/s3_orderreview.sh)
