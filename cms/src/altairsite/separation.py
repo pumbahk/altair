55a6e695aa17a664b4a1648cb3fca4ad90238a7f
dev@ticketstar.jp <dev@ticketstar.jp>
2014-05-01 11:53:34 +0900

    Revert "Merge remote-tracking branch 'origin/fix/okada/7920' into develop"
    This reverts commit 1273cfc395385c457caefcd22643974a01a4233b, reversing
    changes made to 754d83d74be5305261e8f6252c2bfefdc32ddf18.

--- cms/src/altairsite/separation.py
+++ cms/src/altairsite/separation.py
@@ -60,7 +60,9 @@ tstar_mobile_or_not_renderer = SelectableRendererSetup(
 
 def enable_search_function(info, request):
     use_full_usersite = request.organization.use_full_usersite if request.organization else False
-    return bool(use_full_usersite and request.organization.short_name == 'RT')
+    if use_full_usersite and request.organization.short_name == 'RT':
+        return True
+    return False
 
 ## todo: もっと細かく用途制限
 def enable_full_usersite_function(info, request):
