db645126c28c15b5cb99fc723a0be5d08caf6cfc
podhmo <podhmo@be.jp>
2014-05-01 17:08:10 +0900

    発券枚数の総計もresetする感じに refs #8015

--- ticketing/src/altair/app/ticketing/core/models.py
+++ ticketing/src/altair/app/ticketing/core/models.py
@@ -3313,7 +3313,7 @@ class OrderedProductItem(Base, BaseModel, WithTimestamp, LogicallyDeleted):
     @property
     def printed_at_status(self):
         total = len(self.tokens)
-        printed_count = len([i for i in self.tokens if i.printed_at])
+        printed_count = len([i for i in self.tokens if i.is_printed()])
         return dict(printed=printed_count, total=total)
 
     def iterate_serial_and_seat(self):
