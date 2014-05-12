48fdfcc9a9e1b3ecffaa2ece9d4931373684f793
mmatsui <masahiro.matsui@gmail.com>
2014-05-12 14:32:00 +0900

    在庫増減追跡の為のdebug文追加 refs #8040

--- ticketing/src/altair/app/ticketing/cart/stocker.py
+++ ticketing/src/altair/app/ticketing/cart/stocker.py
@@ -74,6 +74,7 @@ class Stocker(object):
             quantity = require_quantities[status.stock_id]
             if status.quantity < quantity:
                 raise NotEnoughStockException(status.stock, status.quantity, quantity)
+            logger.info('take stock (id=%s, quantity=%d) - %d' % (status.stock_id, status.quantity, quantity))
             status.quantity -= quantity
             results.append((status, quantity))
         return results
