4ca478f6f3eec2cffecf28198a22c20748b9a27b
podhmo <podhmo@be.jp>
2014-05-22 11:51:01 +0900

    Revert "Revert "preview_typeをsvgを取得する際に渡すように修正。sej versionとinner version両方のpreview見れる様になった refs #8139""
    This reverts commit ec5ca7db5e115a98b7343dc6a386e46a4cad60fc.

--- ticketing/src/altair/app/ticketing/static/js/tickets/preview/gateway.js
+++ ticketing/src/altair/app/ticketing/static/js/tickets/preview/gateway.js
@@ -153,11 +153,12 @@ preview.ApiCommunicationGateway = core.ApiCommunicationGateway.extend({
         var params = {
             fillvalues_resource: {
                 modelname: holder.name, 
-                model: holder.pk, 
+                model: holder.pk 
             }, 
             svg_resource: {
                 modelname: "TicketFormat", 
-                model: this.params.get("ticket_format").pk
+                model: this.params.get("ticket_format").pk,
+                type: this.params.get("ticket_format").type || "default"
             }
         };
         if(!!holder.sub){
