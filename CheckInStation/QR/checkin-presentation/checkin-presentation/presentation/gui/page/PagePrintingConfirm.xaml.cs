b411a75f55b5c169038339c88cbead7bcdc3d449
podhmo podhmo <podhmo@beproud.jp>
2014-06-13 16:56:33 +0900

    1枚だけ選択した場合の確認画面+まとめて印刷への遷移画面追加

---
+++ CheckInStation/QR/checkin-presentation/checkin-presentation/presentation/gui/page/PagePrintingConfirm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace checkin_presentation.presentation.gui.page
+{
+    /// <summary>
+    /// Interaction logic for PagePrintingConfirm.xaml
+    /// </summary>
+    public partial class PagePrintingConfirm : Page
+    {
+        public PagePrintingConfirm()
+        {
+            InitializeComponent();
+        }
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            OnGotoNextPageStart();
+        }
+
+        private void OnGotoNextPageStart()
+        {
+
+        }
+    }
+}
