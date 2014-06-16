dde4aeee14fa9e60ff4c29c50c62784b23e66da6
dev@ticketstar.jp <dev@ticketstar.jp>
2014-06-16 17:23:50 +0900

    rebase alembic script

--- ticketing/alembic/versions/e6d2bfcc31e_add_ticket_base_template_id.py
+++ ticketing/alembic/versions/e6d2bfcc31e_add_ticket_base_template_id.py
@@ -1,14 +1,14 @@
 """add Ticket.base_template_id
 
 Revision ID: e6d2bfcc31e
-Revises: 379d8f5fb25
+Revises: 44517507b667
 Create Date: 2014-05-30 02:53:54.410841
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'e6d2bfcc31e'
-down_revision = '379d8f5fb25'
+down_revision = '44517507b667'
 
 from alembic import op
 import sqlalchemy as sa
