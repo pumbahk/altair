6282c41b6a7ffeb3180f5691fdf9c2a5a39d1de1
podhmo <podhmo@be.jp>
2014-06-20 17:02:10 +0900

    広告画像をorganizationごとに分ける

--- ticketing/src/altair/app/ticketing/checkinstation/adimagecollector.py
+++ ticketing/src/altair/app/ticketing/checkinstation/adimagecollector.py
@@ -1,36 +1,38 @@
 # -*- coding:utf-8 -*-
 from zope.interface import implementer
 from .interfaces import IAdImageCollector
+from collections import defaultdict
 
 @implementer(IAdImageCollector)
 class AdImageCollector(object):
     def __init__(self, registry):
         self.registry = registry
-        self.urls = []
-        self.specs = []
+        self.urls = defaultdict(list)
+        self.specs =defaultdict(list)
 
-    def add_url(self, url):
-        self.urls.append(url)
+    def add_url(self, org_short_name, url):
+        self.urls[org_short_name].append(url)
 
-    def add_spec(self, spec):
-        self.specs.append(spec)
+    def add_spec(self, org_short_name, spec):
+        self.specs[org_short_name].append(spec)
 
     def get_images(self, request):
         r = []
-        r.extend(self.urls)
-        r.extend(request.static_url(s) for s in self.specs)
+        short_name = request.context.organization.short_name
+        r.extend(self.urls.get(short_name) or self.urls.get("default"))
+        r.extend(request.static_url(s) for s in (self.specs.get(short_name) or self.specs.get("default")))
         return r
 
 
-def add_ad_image(config, url=None, imagespec=None):
+def add_ad_image(config, short_name, url=None, imagespec=None):
     collector = config.registry.queryUtility(IAdImageCollector)
     if collector is None:
         collector = AdImageCollector(config.registry)
         config.registry.registerUtility(collector, IAdImageCollector)
     if url:
-        collector.add_url(url)
+        collector.add_url(short_name, url)
     if imagespec:
-        collector.add_spec(imagespec)
+        collector.add_spec(short_name, imagespec)
 
 def includeme(config):
     config.add_directive("add_ad_image", add_ad_image)
