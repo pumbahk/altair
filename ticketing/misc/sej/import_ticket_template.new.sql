4fbfac92a46d5b966aafbbe7907b0aa50ca7c960
ticketstar-dev <dev@ticketstar.jp>
2014-07-04 16:00:44 +0900

    NTTS にしてほしいとのこと as per the request of murata-san refs #5920

--- ticketing/misc/sej/import_ticket_template.new.sql
+++ ticketing/misc/sej/import_ticket_template.new.sql
@@ -6,7 +6,7 @@ INSERT INTO SejTicketTemplateFile (
     ticket_css
 ) VALUES (
     '4',
-    'TTTS000002',
+    'NTTS000002',
     '新台紙用テンプレート',
     '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
