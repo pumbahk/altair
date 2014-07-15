3e452585dde4a5b1a829127b268ae831e6490894
TakesxiSximada <takeshi.shimada@ticketstar.jp>
2014-07-15 11:16:43 +0900

    こっちにもデバッグ文が残ってた このファイルはtest用にAugusVenueを全てDownloadするスクリプトだからデバッグ文が残っていても別に問題ないんだけど なんか気持ち悪いから削除しておく

--- ticketing/src/altair/app/ticketing/cooperation/augus/tools/augus_venue_download_all.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/tools/augus_venue_download_all.py
@@ -17,7 +17,7 @@ class Headers(dict):
     def __init__(self, *args, **kwds):
         super(Headers, self).__init__(*args, **kwds)
         self._cookies = {}
-        
+
     def load(self, res):
         for header in res.keys():
             if header.lower() == 'set-cookie':
@@ -51,12 +51,12 @@ class Browser(object):
             headers = Headers()
             headers.chrome_on_mac_mode()
             self.headers = headers
-    
+
     def get(self, url, *args, **kwds):
         return self.request(url, 'GET', headers=self.headers, *args, **kwds)
 
     def post(self, url, *args, **kwds):
-        return self.request(url, 'POST', headers=self.headers, *args, **kwds) 
+        return self.request(url, 'POST', headers=self.headers, *args, **kwds)
 
     def request(self, *args, **kwds):
         res, contents = self._http.request(*args, **kwds)
@@ -80,14 +80,14 @@ def _main():
                                       }})
 
     url_login = server_data['url_login']
-    url_venue = server_data['url_venue']    
+    url_venue = server_data['url_venue']
 
     backend_login_data = {'login_id': server_data['username'].encode('utf8'),
                           'password': server_data['password'].encode('utf8'),
                           'submit': u'ログイン'.encode('utf8'),
                           }
     params = urllib.urlencode(backend_login_data)
-    
+
     browser = Browser()
     browser.basic_auth(bauth_data['username'], bauth_data['password'])
     browser.start()
@@ -112,7 +112,7 @@ def main():
                                        }})
 
     url_login = server_data['url_login']
-    url_venue = server_data['url_venue']    
+    url_venue = server_data['url_venue']
 
     http = httplib2.Http('.cache',
                          disable_ssl_certificate_validation=True)
@@ -139,8 +139,8 @@ def main():
         ('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'),
         ('Referer', 'https://backend.stg2.rt.ticketstar.jp/login/'),
         ))
-               
-           
+
+
     backend_login_data = {'login_id': server_data['username'].encode('utf8'),
                           'password': server_data['password'].encode('utf8'),
                           'submit': u'ログイン'.encode('utf8'),
@@ -160,7 +160,7 @@ def main():
         if not atag:
             venue_ids.append(venue_id)
 
-    
+
     for venue_id in venue_ids:
         print venue_id,
         res, content = http.request(
@@ -173,14 +173,14 @@ def main():
         continue
 
         old = StringIO(content)
-        old.seek(0)        
+        old.seek(0)
         new = StringIO()
-        
+
         reader = csv.reader(old)
         writer = csv.writer(new, delimiter=',')
-        
+
         writer.writerow(reader.next()) # header
-        
+
         for row in reader:
             name = row[1]
             seat_no = row[2]
@@ -193,10 +193,7 @@ def main():
         res, content = http.request(
             'https://backend.stg2.rt.ticketstar.jp/cooperation/augus/venues/{}/download'.format(venue_id),
             'POST', headers=headers, body=new.read())
-        import ipdb; ipdb.set_trace()
-        pass
-        
-            
+
+
 if __name__ == '__main__':
     main()
-
