7c7c383d67e088f30e5a7adc7f83e7c59e537f2c
dev@ticketstar.jp <dev@ticketstar.jp>
2014-07-24 15:31:06 +0900

    #9085 を部分 revert

--- ticketing/src/altair/app/ticketing/cart/helpers.py
+++ ticketing/src/altair/app/ticketing/cart/helpers.py
@@ -4,7 +4,6 @@
 TODO: cart取得はリソースの役目
 """
 import functools
-from decimal import Decimal
 from pyramid.view import render_view_to_response
 from pyramid.compat import escape
 from markupsafe import Markup
@@ -216,22 +215,13 @@ def product_name_with_unit(product_items):
             u"%s:%d枚" % (escape(product_item.stock_type.name), product_item.quantity)
             for product_item in product_items))
 
-def get_availability_text(quantity, all_quantity, middle_stock_threshold, middle_stock_threshold_percent):
-    if not middle_stock_threshold and not middle_stock_threshold_percent:
-        middle_stock_threshold_percent = 50
-
+def get_availability_text(quantity):
     if quantity <= 0:
         return u'×'
-
-    if middle_stock_threshold_percent:
-        if quantity < (Decimal(all_quantity) / 100 * middle_stock_threshold_percent):
-            return u'△'
-
-    if middle_stock_threshold:
-        if quantity < middle_stock_threshold:
-            return u'△'
-
-    return u'◎'
+    elif quantity < 20:
+        return u'△'
+    else:
+        return u'◎'
 
 def cart_url(request, event=None, performance=None, sales_segment=None):
     if sales_segment is not None:
