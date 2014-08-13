fe8b2497b349ecfb2f485a0da1a993cadc712c82
okeiichi <keiichi_okada@ticketstar.jp>
2014-08-13 09:55:53 +0900

    チェックインステーションの本番で、接続ができなかった refs #7652

--- CheckInStation/QR/checkin-presentation/checkin-presentation/Properties/AssemblyInfo.cs
+++ CheckInStation/QR/checkin-presentation/checkin-presentation/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@
 [assembly: System.Reflection.AssemblyProduct("CheckinStation")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright @ 2014")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
-[assembly: System.Reflection.AssemblyInformationalVersion("0.0.0.08e27086a3559ebcd5c43bbae4f84ce6e0030ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersion("0.0.0.811f2cf5120e31bcac9da97c2eba10da5086b1d2")]
 [assembly: System.Reflection.AssemblyVersion("0.0.0")]
 [assembly: System.Reflection.AssemblyFileVersion("0.0.0")]
 
