c1a8d6dd4d6b9c5c9665199aeb55bebcfa47e9f2
TicketStar <ticketstar@apze1.1a.vpc.altr>
2014-11-26 15:33:11 +0900

    masterへの接続が正しく設定されていなかったのを修正

--- ticketing/src/altair/app/ticketing/project_specific/zea/__init__.py
+++ ticketing/src/altair/app/ticketing/project_specific/zea/__init__.py
@@ -1,8 +1,14 @@
 from pyramid.config import Configurator
+import sqlahelper
+from sqlalchemy import engine_from_config
 
 def main(global_config, **local_config):
     settings = dict(global_config)
     settings.update(local_config)
+    from sqlalchemy.pool import NullPool
+    engine = engine_from_config(settings, poolclass=NullPool, isolation_level='READ COMMITTED')
+    sqlahelper.add_engine(engine)
+    from altair.app.ticketing import install_ld
     config = Configurator(
         settings=settings,
         root_factory='.resources.root_factory'
