a817d31fbd87f4a89055e2aca5ba9ee39a5bf903
knskstar <kensuke_ohta@ticketstar.jp>
2015-01-09 12:37:17 +0900

    FeatureSettingManagerにget_string_value()を追加。 refs: #10570

--- cms/src/altaircms/api.py
+++ cms/src/altaircms/api.py
@@ -2,14 +2,21 @@ from .models import FeatureSetting
 
 class FeatureSettingManager:
     def __init__(self, request):
-        self.organization = request.organization
+        self.organization_id = request.organization.id
 
-    def get_boolean_value(self, name):
-        str_value = FeatureSetting.get_value_by_name(organization_id=self.organization.id, name=name)
+    def get_string_value(self, name, default_value = ""):
+        str_value = FeatureSetting.get_value_by_name(organization_id=self.organization_id, name=name)
+        if str_value is not None:
+            return str_value
+        else:
+            return default_value
+
+    def get_boolean_value(self, name, default_value = False):
+        str_value = self.get_string_value(name)
         if str_value is not None and str_value.lower() == "true":
             return True
         else:
-            return False;
+            return default_value;
 
 def get_featuresettingmanager(request):
     return FeatureSettingManager(request)
