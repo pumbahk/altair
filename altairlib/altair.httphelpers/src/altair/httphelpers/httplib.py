8a69853ea48909297421340c917d7ed0bed05e5a
Moriyoshi Koizumi <moriyoshi@ticketstar.jp>
2015-01-26 13:39:15 +0900

    debug log に cipher などを出力 refs #10719

--- altairlib/altair.httphelpers/src/altair/httphelpers/httplib.py
+++ altairlib/altair.httphelpers/src/altair/httphelpers/httplib.py
@@ -4,6 +4,10 @@ import ssl
 import socket
 import httplib
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class OurHTTPSConnection(httplib.HTTPSConnection):
     def __init__(self, *args, **kwargs):
         ca_certs = kwargs.pop('ca_certs', None)
@@ -17,6 +21,7 @@ class OurHTTPSConnection(httplib.HTTPSConnection):
         self.ssl_version = ssl_version
 
     def connect(self):
+        logger.debug('key_file=%s, cert_file=%s, cert_reqs=%d, ca_certs=%s, ssl_version=%d' % (self.key_file, self.cert_file, self.cert_reqs, self.ca_certs, self.ssl_version))
         sock = socket.create_connection((self.host, self.port),
                                         self.timeout, self.source_address)
         if self._tunnel_host:
@@ -30,4 +35,4 @@ class OurHTTPSConnection(httplib.HTTPSConnection):
             ca_certs=self.ca_certs,
             ssl_version=self.ssl_version
             )
-
+        logger.debug('cipher=%r' % (self.sock.cipher(), ))
