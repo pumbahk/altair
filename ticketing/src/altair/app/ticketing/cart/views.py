04e14985ac2e35821fa22c96279e660ab19e9c61
okeiichi <keiichi_okada@ticketstar.jp>
2015-02-15 18:22:55 +0900

    JCFのコピーを公開する。 refs #10880

--- ticketing/src/altair/app/ticketing/cart/views.py
+++ ticketing/src/altair/app/ticketing/cart/views.py
@@ -307,7 +307,7 @@ class CompatAgreementView(object):
 def jump_maintenance_page_for_trouble(organization):
     """https://redmine.ticketstar.jp/issues/10878
     """
-    if organization is None or organization.code not in ['RT']:
+    if organization is None or organization.code not in ['RT', 'ZZ']:
         raise HTTPFound('/maintenance.html')
 
 
