53ef4d14c2649fd9806da337a3464d11f62ca3d4
Moriyoshi Koizumi <moriyoshi@ticketstar.jp>
2015-03-02 23:21:00 +0900

    backend の URL は inner と outer と2種類あるので、それをケアする refs #11008

--- cms/src/altaircms/plugins/extra/__init__.py
+++ cms/src/altaircms/plugins/extra/__init__.py
@@ -1,6 +1,14 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 def includeme(config):
     from .stockstatus import StockDataAPI
     from altaircms.plugins.interfaces import IExternalAPI
-    api_impl = StockDataAPI(config.registry.settings["altaircms.backend.url"],
+    backend_url = config.registry.settings.get("altaircms.backend.inner.url")
+    if backend_url is None:
+        logger.warning('altaircms.backend.inner.url is not given. Using deprecated altaircms.backend.url instead')
+        backend_url = config.registry.setting.get('altaircms.backend.url')
+    api_impl = StockDataAPI(backend_url,
                             config.registry.settings["altaircms.backend.apikey"])
     config.registry.registerUtility(api_impl, IExternalAPI, api_impl.__class__.__name__)
