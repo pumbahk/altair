a9168a2df1d8940d41b692d7bb6c084d7a9e94ab
TakesxiSximada <takesxi.sximada@gmail.com>
2015-03-25 11:43:56 +0900

    refs #11196 バンビシャスブースターフォーム文言修正

--- ticketing/src/altair/app/ticketing/cart/schemas.py
+++ ticketing/src/altair/app/ticketing/cart/schemas.py
@@ -422,7 +422,7 @@ class BambitiousExtraForm(ExtraForm):
             DynSwitchDisabled(u'{cont} <> "yes"'),
             Regexp(r'\d{7}$', message=u'半角数字7ケタで入力してください。'),
             ],
-        description=Markup(u'[継続]を選択した方は2014-2015シーズンの会員番号をご入力ください'),
+        description=Markup(u'[継続]を選択した方は2014-2015シーズンの会員番号をご入力ください。<br />継続入会の方で会員種別の変更がない方は、現在お持ちの会員証を引き続きご使用いただきます。'),
         note=Markup(u'(半角数字7ケタ)')
         )
     member_type = OurSelectField(u"会員種別選択", validators=[Required()])
