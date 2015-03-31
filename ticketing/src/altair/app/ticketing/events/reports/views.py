e05e721eeb0bbbe0223c52e1d4ac2510310582d6
okeiichi <keiichi_okada@ticketstar.jp>
2015-03-31 10:12:37 +0900

    エラーの意味が違っていた。販売日程管理票 refs #10521

--- ticketing/src/altair/app/ticketing/events/reports/views.py
+++ ticketing/src/altair/app/ticketing/events/reports/views.py
@@ -66,7 +66,7 @@ class Reports(BaseView):
         if event is None:
             return HTTPNotFound('event id %d is not found' % event_id)
         if not event.performances:
-            return HTTPNotFound('event id %d is not found' % event_id)
+            return HTTPNotFound('performances is not found')
 
         # CSVファイル生成
         exporter = reporting.export_for_sales(event)
