94f5b7e0a334024bcffe2b97bd172a00270d0ee7
SASAO Takahiro <sasao@ticketstar.jp>
2015-04-13 19:08:52 +0900

    CSVのファイル名はイベント名にする. refs #11382

--- ticketing/src/altair/app/ticketing/utils.py
+++ ticketing/src/altair/app/ticketing/utils.py
@@ -437,3 +437,8 @@ class DateTimeRange(object):
             return True
         else:
             return False
+
+def get_safe_filename(s):
+    # euc-jpにして、全角はスペース以外全部OK、半角の大半の記号を置換する
+    s = s.replace(u'　', u'_')
+    return re.sub(r'[^-_0-9a-zA-Z\x80-\xFF]', '_', s.encode('euc-jp', 'replace').replace('?', '_')).decode('euc-jp')
