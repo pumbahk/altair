81f1d2e0aa75d3b1385420e349ce358fd81c3f78
TakesxiSximada <takesxi.sximada@gmail.com>
2015-04-23 09:57:04 +0900

    refs #11492 inner channelの予約はメールアドレスなどが入らないケースがあるためラグーナカスタムには送らない

--- ticketing/src/altair/app/ticketing/cooperation/laguna/scripts/laguna_csv_export.py
+++ ticketing/src/altair/app/ticketing/cooperation/laguna/scripts/laguna_csv_export.py
@@ -315,7 +315,7 @@ def export_csv_for_laguna(request, fileobj, organization_id):
         else:
             rec += ['' for ii in range(len(synagy_header_lots))]
 
-        if order:
+        if order and not order.is_inner_channel:
             if order.channel:
                 channel = order.channel
             sex = order.user.user_profile.sex if order.user and order.user.user_profile else ''
