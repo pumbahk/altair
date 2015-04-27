b3cc8b1884805f3813c36c42edd1d67295200439
TakesxiSximada <takesxi.sximada@gmail.com>
2015-04-27 22:23:39 +0900

    refs #11514 カート設定のタイプが抽選の時にorderreviewが見れない問題を修正

--- ticketing/src/altair/app/ticketing/orderreview/panels.py
+++ ticketing/src/altair/app/ticketing/orderreview/panels.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
 from pyramid_layout.panel import panel_config
 from altair.app.ticketing.cart.rendering import selectable_renderer
 
+
 @panel_config('order_detail.standard', renderer=selectable_renderer('order_review/_order_detail_standard.html'))
+@panel_config('order_detail.lots', renderer=selectable_renderer('order_review/_order_detail_lots.html'))
 def order_detail_standard(context, request, order, user_point_accounts=None):
-    return { 'order': order, 'user_point_accounts': user_point_accounts }
+    return {'order': order, 'user_point_accounts': user_point_accounts}
+
 
 @panel_config('order_detail.booster.89ers', renderer=selectable_renderer('order_review/_order_detail_booster.html'))
 @panel_config('order_detail.booster.bambitious', renderer=selectable_renderer('order_review/_order_detail_booster.html'))
 @panel_config('order_detail.booster.bigbulls', renderer=selectable_renderer('order_review/_order_detail_booster.html'))
 def order_detail_booster(context, request, order, user_point_accounts=None):
-    return { 'order': order, 'user_point_accounts': user_point_accounts }
+    return {'order': order, 'user_point_accounts': user_point_accounts}
+
 
 @panel_config('order_detail.fc', renderer=selectable_renderer('order_review/_order_detail_fc.html'))
 def order_detail_fc(context, request, order, user_point_accounts=None):
-    return { 'order': order, 'user_point_accounts': user_point_accounts }
+    return {'order': order, 'user_point_accounts': user_point_accounts}
