8bfc60b9f1da1cb78a451ab5c9c25e85815851d3
Moriyoshi Koizumi <moriyoshi@ticketstar.jp>
2015-05-08 15:01:31 +0900

    5b000792d1e2bae1ea629b7af5a2b0cfeeb1a1ba のマージミス、すいません

--- ticketing/src/altair/app/ticketing/organizations/views.py
+++ ticketing/src/altair/app/ticketing/organizations/views.py
@@ -245,6 +245,7 @@ class OrganizationSettings(BaseView):
         organization_setting.enable_smartphone_cart = f.enable_smartphone_cart.data
         organization_setting.entrust_separate_seats = f.entrust_separate_seats.data
         organization_setting.sales_report_type = f.sales_report_type.data
+        organization_setting.sitecatalyst_use = f.sitecatalyst_use.data
         organization_setting.enable_mypage = f.enable_mypage.data
         organization_setting.augus_use = f.augus_use.data
         organization_setting.mail_refund_to_user = f.mail_refund_to_user.data
