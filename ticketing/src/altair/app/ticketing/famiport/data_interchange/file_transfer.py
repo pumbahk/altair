5f97995993376f61d741f90e4c911a6e9e58dfce
knskstar <kensuke_ohta@ticketstar.jp>
2015-05-20 10:55:37 +0900

    send_file_over_ftps()の微修正 refs: #11663

--- ticketing/src/altair/app/ticketing/famiport/data_interchange/file_transfer.py
+++ ticketing/src/altair/app/ticketing/famiport/data_interchange/file_transfer.py
@@ -1,11 +1,11 @@
 from ftplib import FTP_TLS
-import logging
+import logging, os
 
 logger = logging.getLogger(__name__)
 
 def send_file_over_ftps(file_path, host, username = None, password = None, upload_dir_path = '/'): # TODO Make it possible to send multiple files?
     ftp = FTP_TLS() # FTP over SSL/TLS
-    ftp.set_debuglevel(2) # Maximum amount of debugging output for now
+    ftp.set_debuglevel(1) # Moderate amount of debugging output for now
 
     ftp.connect(host=host, port=21, timeout=600)
     ftp.login(user=username, passwd=password)
@@ -15,7 +15,8 @@ def send_file_over_ftps(file_path, host, username = None, password = None, uploa
     ftp.prot_p() # Secure data connection
 
     file = open(file_path, 'rb')
-    ftp.storbinary("STOR %s" % file.name, file)
+    file_name = os.path.basename(file.name)
+    ftp.storbinary("STOR %s" % file_name, file) # STOR the file with file_name. Override if same file_name exists.
     file.close()
 
     ftp.quit()
