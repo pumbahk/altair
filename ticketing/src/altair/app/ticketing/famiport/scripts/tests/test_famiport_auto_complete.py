b42f157a85d20e80b55c06bb81c71accf8c3b8e3
TakesxiSximada <takesxi.sximada@gmail.com>
2015-06-12 07:43:23 +0900

    refs #9850 testの修正

--- ticketing/src/altair/app/ticketing/famiport/scripts/tests/test_famiport_auto_complete.py
+++ ticketing/src/altair/app/ticketing/famiport/scripts/tests/test_famiport_auto_complete.py
@@ -34,21 +34,21 @@ class FamiPortOrderAutoCopleterTest(TestCase):
         target = self._get_target(session)
         self.assertEqual(target.time_point, nine_minutes_ago)
 
-    @mock.patch('altair.app.ticketing.famiport.scripts.famiport_auto_complete.FamiPortOrderAutoCompleter._fetch_target_famiport_orders')
+    @mock.patch('altair.app.ticketing.famiport.scripts.famiport_auto_complete.FamiPortOrderAutoCompleter._fetch_target_famiport_receipts')
     @mock.patch('altair.app.ticketing.famiport.scripts.famiport_auto_complete.FamiPortOrderAutoCompleter._do_complete')
-    def test_complete(self, _do_complete, _fetch_target_famiport_orders):
+    def test_complete(self, _do_complete, _fetch_target_famiport_receipts):
         from ..famiport_auto_complete import AutoCompleterStatus
         session = mock.Mock()
-        famiport_orders = [mock.Mock() for ii in range(10)]
-        _fetch_target_famiport_orders.return_value = famiport_orders
+        famiport_receipts = [mock.Mock() for ii in range(10)]
+        _fetch_target_famiport_receipts.return_value = famiport_receipts
         target = self._get_target(session)
         res = target.complete()
         self.assertEqual(res, AutoCompleterStatus.success.value)
 
         do_complete_call_args_list = [call_args[0][0] for call_args in _do_complete.call_args_list]
         session_call_args_list = [call_args[0][0] for call_args in session.add.call_args_list]
-        self.assertEqual(do_complete_call_args_list, famiport_orders)
-        self.assertEqual(session_call_args_list, famiport_orders)
+        self.assertEqual(do_complete_call_args_list, famiport_receipts)
+        self.assertEqual(session_call_args_list, famiport_receipts)
 
     @skip('not yet implemented')
     def test_do_complete(self):
