ca6231d7ab98fa182bcf8a71fe70d558afbffea7
Moriyoshi Koizumi <moriyoshi@ticketstar.jp>
2015-06-23 05:10:24 +0900

    alembic を develop から cherry-pick して rebase

--- ticketing/alembic/versions/4855569c078d_famiport_payment_plugin.py
+++ ticketing/alembic/versions/4855569c078d_famiport_payment_plugin.py
@@ -2,14 +2,14 @@
 """famiport_payment_plugin
 
 Revision ID: 4855569c078d
-Revises: 569b8783f4f9
+Revises: 3269e5ab0f4e
 Create Date: 2015-04-09 00:37:48.715650
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '4855569c078d'
-down_revision = '569b8783f4f9'
+down_revision = '3269e5ab0f4e'
 
 from alembic import op
 import sqlalchemy as sa
