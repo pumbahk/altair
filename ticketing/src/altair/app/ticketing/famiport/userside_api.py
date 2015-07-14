1c7714362cff0a74314975e31add0d5671557d0c
TakesxiSximada <takesxi.sximada@gmail.com>
2015-07-14 23:03:17 +0900

    refs #9850 altair_famiport_performance_group.altair_famiport_venue.site.name は半角->全角で変更済みなので再度やる必要はない

--- ticketing/src/altair/app/ticketing/famiport/userside_api.py
+++ ticketing/src/altair/app/ticketing/famiport/userside_api.py
@@ -378,7 +378,7 @@ def submit_to_downstream_sync(request, session, tenant, event):
                 client_code=tenant.code,
                 id=None,
                 userside_id=altair_famiport_performance_group.altair_famiport_venue.site_id,
-                name=hankaku2zenkaku(altair_famiport_performance_group.altair_famiport_venue.site.name),
+                name=altair_famiport_performance_group.altair_famiport_venue.site.name,
                 name_kana=u'',
                 prefecture=prefecture,
                 update_existing=True
