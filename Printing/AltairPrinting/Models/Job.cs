bcd66b965464a381b76f3e4c8a6372567fd41812
SASAO Takahiro <sasao@p-strain.com>
2015-07-31 19:42:34 +0900

    new print server with .NET.

---
+++ Printing/AltairPrinting/Models/Job.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Xml;
+using System.Windows.Markup;
+using System.Windows.Documents;
+
+namespace AltairPrinting.Altair.Document
+{
+    class Job
+    {
+        public string PrinterName { get; set; }
+        public FixedDocument Document { get; set; }
+        private string xaml;
+
+        public Job(FixedDocument document)
+        {
+            this.Document = document;
+        }
+
+        public Job(string xaml)
+        {
+            this.xaml = xaml;
+        }
+
+        public void Build()
+        {
+            if (xaml != null)
+            {
+                Document = XamlReader.Load(XmlReader.Create(new StringReader(xaml))) as FixedDocument;
+            }
+        }
+    }
+}
