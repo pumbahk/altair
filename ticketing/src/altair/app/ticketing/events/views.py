b03e57a4cafdde3ae8d754f7d07db2721aba4460
noracorn <keiichi_okada@ticketstar.jp>
2015-09-09 13:26:24 +0900

    イベント検索での指摘事項を修正 refs #12496

--- ticketing/src/altair/app/ticketing/events/views.py
+++ ticketing/src/altair/app/ticketing/events/views.py
@@ -93,13 +93,13 @@ class Events(BaseView):
             sql.asc
             )
 
-        query = slave_session.query(Event)
+        query = slave_session.query(Event) \
+            .filter(Event.organization_id==int(self.context.organization.id))
 
         # イベントの表示、非表示（クッキーで制御）
         from . import VISIBLE_EVENT_SESSION_KEY
         if not self.request.session.get(VISIBLE_EVENT_SESSION_KEY, None):
             query = query.join(EventSetting, Event.id==EventSetting.event_id) \
-                .filter(Event.organization_id==int(self.context.organization.id)) \
                 .filter(EventSetting.visible==True)
 
         if sort is not None:
