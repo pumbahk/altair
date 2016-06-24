a372016997947644b0ab06156839f1fb35cb9729
noracorn <keiichi_okada@ticketstar.jp>
2016-06-24 16:24:16 +0900

    抽選に座席指定ありの商品がある場合、赤くする。 refs #tkt1092

--- ticketing/src/altair/app/ticketing/events/lots/helpers.py
+++ ticketing/src/altair/app/ticketing/events/lots/helpers.py
@@ -2,6 +2,7 @@ from api import get_lots_cart_url
 from webhelpers.html.tags import link_to
 from altair.app.ticketing.lots.helpers import timezone_label
 
+
 class Link(object):
     def __init__(self, label, url, **attrs):
         self.label = label
@@ -14,5 +15,15 @@ class Link(object):
     def __str__(self):
         return self.__html__()
 
+
 def lots_cart_url(request, event_id, lot_id):
     return get_lots_cart_url(request=request, event_id=event_id, lot_id=lot_id)
+
+
+def is_quantity_only_stock_type(lot, seat_stock_type):
+    stock_types = lot.event.stock_types
+    for stock_type in stock_types:
+        if stock_type.id == seat_stock_type.id:
+            if stock_type.quantity_only:
+                return True
+    return False
