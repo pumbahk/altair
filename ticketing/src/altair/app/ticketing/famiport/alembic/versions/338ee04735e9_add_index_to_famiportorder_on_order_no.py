d3a862f0f6bde84c19a68d8b0fe31ba52fefb688
yuanrong.zheng <yuanrong.zheng@rakuten.com>
2016-08-04 16:08:19 +0900

    tkt2213 add index to FamiPortOrder on order_no

---
+++ ticketing/src/altair/app/ticketing/famiport/alembic/versions/338ee04735e9_add_index_to_famiportorder_on_order_no.py
@@ -0,0 +1,25 @@
+"""add index to FamiPortOrder on order_no
+
+Revision ID: 338ee04735e9
+Revises: d068c02f6e6
+Create Date: 2016-08-04 15:43:22.919836
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '338ee04735e9'
+down_revision = 'd068c02f6e6'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import functions as sqlf
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.create_index('ix_famiport_order_no', 'FamiPortOrder', ['order_no'])
+
+def downgrade():
+    op.drop_indexx('ix_famiport_order_no', 'FamiPortOrder')
