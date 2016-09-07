4aae340daffb968fd103d9f1c00783fbc27b71ba
noracorn <keiichi_okada@ticketstar.jp>
2016-09-07 17:02:23 +0900

    ハウステンボスのイベントゲートをマスタの文言で上書きする refs #tkt2433

--- ticketing/src/altair/app/ticketing/payments/plugins/orion.py
+++ ticketing/src/altair/app/ticketing/payments/plugins/orion.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import re
 from altair.app.ticketing.payments.interfaces import IOrderDelivery
 from altair.app.ticketing.cart.interfaces import ICartDelivery
 from altair.app.ticketing.mails.interfaces import (
@@ -10,11 +11,14 @@ from altair.app.ticketing.mails.interfaces import (
     ILotsRejectedMailResource,
     )
 
+tag_re = re.compile(r"<[^>]*?>")
+
 from . import models as m
 from altair.app.ticketing.core import models as core_models
 from . import logger
 import qrcode
 import StringIO
+from markupsafe import Markup
 from pyramid.response import Response
 from altair.pyramid_dynamic_renderer import lbr_view_config
 from altair.app.ticketing.qr import qr
@@ -34,7 +38,10 @@ def _overridable(path, fallback_ua_type=None):
 
 @lbr_view_config(context=ICartDelivery, name="delivery-%d" % DELIVERY_PLUGIN_ID, renderer=_overridable("orion_confirm.html"))
 def deliver_confirm_viewlet(context, request):
-    return dict()
+    logger.debug(u"イベントゲート")
+    cart = context.cart
+    delivery_method = cart.payment_delivery_pair.delivery_method
+    return dict(delivery_name=delivery_method.name, description=Markup(delivery_method.description))
 
 QRTicket = namedtuple("QRTicket", "order performance product seat token printed_at")
 
@@ -42,6 +49,8 @@ QRTicket = namedtuple("QRTicket", "order performance product seat token printed_
 def deliver_completion_viewlet(context, request):
     tickets = [ ]
     order = context.order
+    delivery_method = order.payment_delivery_pair.delivery_method
+
     for op in order.ordered_products:
         for opi in op.ordered_product_items:
             for t in opi.tokens:
@@ -56,19 +65,27 @@ def deliver_completion_viewlet(context, request):
     
     # TODO: orderreviewから呼ばれた場合とcartの完了画面で呼ばれた場合で
     # 処理を分岐したい
-    
+
     return dict(
-        paid = (order.paid_at != None),
-        order = order,
-        tel = order.shipping_address.tel_1,
-        tickets = tickets,
+        paid=(order.paid_at!=None),
+        order=order,
+        tel=order.shipping_address.tel_1,
+        tickets=tickets,
+        description=delivery_method.description
         )
 
 @lbr_view_config(context=ICompleteMailResource, name="delivery-%d" % DELIVERY_PLUGIN_ID, renderer=_overridable("orion_mail_complete.html", fallback_ua_type='mail'))
 def deliver_completion_mail_viewlet(context, request):
     shipping_address = context.order.shipping_address
-    return dict(h=cart_helper, shipping_address=shipping_address, 
-                notice=context.mail_data("D", "notice")
+    order = context.order
+    delivery_method = order.payment_delivery_pair.delivery_method
+    description = ""
+    if delivery_method.description is not None:
+        description = tag_re.sub("", delivery_method.description)
+
+    return dict(h=cart_helper, shipping_address=shipping_address,
+                notice=context.mail_data("D", "notice"),
+                description=description
                 )
 
 @lbr_view_config(context=IOrderCancelMailResource, name="delivery-%d" % DELIVERY_PLUGIN_ID)
