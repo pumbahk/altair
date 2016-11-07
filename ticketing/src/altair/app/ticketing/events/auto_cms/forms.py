04aa1facdd4c750d1abc9d5d11e5b7cf74676b18
noracorn <keiichi_okada@ticketstar.jp>
2016-11-07 16:33:23 +0900

    画像登録で、JPGもあげれるように変更 refs #tkt2704

--- ticketing/src/altair/app/ticketing/events/auto_cms/forms.py
+++ ticketing/src/altair/app/ticketing/events/auto_cms/forms.py
@@ -13,6 +13,5 @@ class ImageUploadForm(Form):
     def validate_upload_file(form, field):
         if not hasattr(field.data, "filename"):
             raise ValidationError(u"ファイルを指定してください")
-
-        if not field.data.filename.endswith('png'):
-            raise ValidationError(u"pngファイルのみアップロード可能です。")
+        if not (field.data.filename.endswith('png') or field.data.filename.endswith('jpg')):
+            raise ValidationError(u"jpg/pngファイルのみアップロード可能です。")
