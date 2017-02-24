4d58ac4119add7a32e0e697d552bda224f912166
Liu <nantsou.liu@rakuten.com>
2017-02-24 16:34:50 +0900

    ref#tkt3079 データベースに保存する内容は文字列からIDに変更

--- ticketing/alembic/versions/5936c72c6490_added_event_operator_and_sales_person_to_event.py
+++ ticketing/alembic/versions/5936c72c6490_added_event_operator_and_sales_person_to_event.py
@@ -1,4 +1,4 @@
-"""added_event_creator_and_operator_to_event
+"""added_event_operator_and_sales_person_to_event
 
 Revision ID: 5936c72c6490
 Revises: 3a4d337b0d8d
@@ -19,9 +19,9 @@ Identifier = sa.BigInteger
 
 
 def upgrade():
-    op.add_column('EventSetting', sa.Column('event_creator', sa.Unicode(length=255), nullable=True))
-    op.add_column('EventSetting', sa.Column('event_operator', sa.Unicode(length=255), nullable=True))
+    op.add_column('EventSetting', sa.Column('event_operator_id', Identifier(), nullable=True))
+    op.add_column('EventSetting', sa.Column('sales_person_id', Identifier(), nullable=True))
 
 def downgrade():
-    op.drop_column('EventSetting', 'event_creator')
-    op.drop_column('EventSetting', 'event_operator')
+    op.drop_column('EventSetting', 'event_operator_id')
+    op.drop_column('EventSetting', 'sales_person_id')
