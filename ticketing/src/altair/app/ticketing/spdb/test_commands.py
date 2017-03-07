cc0016266bd980a4772eebfa28a3e7d9607c27b2
noracorn <keiichi_okada@ticketstar.jp>
2017-03-07 18:36:57 +0900

    SPDBのテスト追加 refs #tkt3269

--- ticketing/src/altair/app/ticketing/spdb/test_commands.py
+++ ticketing/src/altair/app/ticketing/spdb/test_commands.py
@@ -1,7 +1,18 @@
 # -*- coding:utf-8 -*-
+from mock import MagicMock
 from unittest import TestCase
 from nose.tools import ok_, eq_
-from .commands import chop_none
+from .commands import chop_none, SQLCreater
+
+
+class SQLCreaterTestCase(TestCase):
+    @staticmethod
+    def test_sql(self):
+        args = MagicMock(org=15, term_from="2016/01/01", term_to="2030/01/01")
+        sql = "select * from test"
+        creater = SQLCreater(args, sql)
+        creater.sql
+        print "test"
 
 
 class AnotherTestCase(TestCase):
