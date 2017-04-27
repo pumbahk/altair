3df98a47a0408b0fc5ac3057129d7acbb734175c
noracorn <keiichi_okada@ticketstar.jp>
2017-04-27 18:35:27 +0900

    Revert "SPDBに枝番号を追加,削除分も送るようにした  refs #tkt3415"
    This reverts commit 61c7056d912755982ff9390ba091881c3641e105.

--- ticketing/src/altair/app/ticketing/spdb/commands.py
+++ ticketing/src/altair/app/ticketing/spdb/commands.py
@@ -53,12 +53,11 @@ spdb_sql = """
         PaymentMethod.name AS payment_method_name,
         DeliveryMethod.name AS delivery_method_name,
         Cart.user_agent,
-        `Order`.channel,
-        `Order`.branch_no
+        `Order`.channel
 
     FROM
         `Order`
-    LEFT JOIN Cart ON Cart.order_id = `Order`.id
+    JOIN Cart ON Cart.order_id = `Order`.id
     JOIN Performance ON `Order`.performance_id = Performance.id
     JOIN Event ON Event.id = Performance.event_id
     JOIN OrderedProduct ON OrderedProduct.order_id = `Order`.id
@@ -111,7 +110,7 @@ class SQLCreater(object):
         if self._args.delete:
             sql = "{0} AND (`Order`.canceled_at IS NOT NULL OR `Order`.deleted_at IS NOT NULL)".format(sql)
         else:
-            sql = "{0} AND (`Order`.canceled_at IS NULL OR `Order`.deleted_at IS NULL)".format(sql)
+            sql = "{0} AND `Order`.canceled_at IS NULL".format(sql)
         return sql
 
 
@@ -273,10 +272,10 @@ def main():
         cur = con.cursor()
         cur.execute(sql_creater.sql)
         orders = cur.fetchall()
-        file_operator.write_tmp_file(u"\"PrimaryKey\",\"予約番号\",\"ステータス\",\"支払いステータス\",\"予約時間\",\"ユーザID\",\"イベントID\",\"イベントタイトル\",\"パフォーマンスID\",\"パフォーマンス名\",\"開演時間\",\"商品明細名\",\"席名\",\"商品明細金額\",\"商品明細個数\",\"手数料\",\"合計金額\",\"auth_identifier\",\"authz_identifier\",\"会員種別名\",\"会員区分名\",\"ポイント\",\"支払い方法\",\"引取方法\",\"デバイス\",\"チャネル\",\"支払い方法名\",\"引取方法名\",\"枝番号\"\n".encode('utf-8'))
+        file_operator.write_tmp_file(u"\"PrimaryKey\",\"予約番号\",\"ステータス\",\"支払いステータス\",\"予約時間\",\"ユーザID\",\"イベントID\",\"イベントタイトル\",\"パフォーマンスID\",\"パフォーマンス名\",\"開演時間\",\"商品明細名\",\"席名\",\"商品明細金額\",\"商品明細個数\",\"手数料\",\"合計金額\",\"auth_identifier\",\"authz_identifier\",\"会員種別名\",\"会員区分名\",\"ポイント\",\"支払い方法\",\"引取方法\",\"デバイス\",\"チャネル\",\"支払い方法名\",\"引取方法名\"\n".encode('utf-8'))
         for row in orders:
-            row = clean_data(row)
-            file_operator.write_tmp_file(u"\"{0[PrimaryKey]}\",\"{0[order_no]}\",\"{0[order_status]}\",\"{0[payment_status]}\",\"{0[created_at]}\",\"{0[user_id]}\",\"{0[event_id]}\",\"{0[event_title]}\",\"{0[performance_id]}\",\"{0[performance_name]}\",\"{0[performance_start_on]}\",\"{0[product_item_name]}\",\"{0[seat_name]}\",\"{0[product_item_price]}\",\"{0[product_item_quantity]}\",\"{0[fee]}\",\"{0[total_amount]}\",\"{0[auth_identifier]}\",\"{0[authz_identifier]}\",\"{0[membership_name]}\",\"{0[membergroup_name]}\",\"{0[point]}\",\"{0[payment_method]}\",\"{0[delivery_method]}\",\"{0[user_agent]}\",\"{0[channel]}\",\"{0[payment_method_name]}\",\"{0[delivery_method_name]}\",\"{0[branch_no]}\"\n".format(row).encode('utf-8'))
+            row = chop_none(row)
+            file_operator.write_tmp_file(u"\"{0[PrimaryKey]}\",\"{0[order_no]}\",\"{0[order_status]}\",\"{0[payment_status]}\",\"{0[created_at]}\",\"{0[user_id]}\",\"{0[event_id]}\",\"{0[event_title]}\",\"{0[performance_id]}\",\"{0[performance_name]}\",\"{0[performance_start_on]}\",\"{0[product_item_name]}\",\"{0[seat_name]}\",\"{0[product_item_price]}\",\"{0[product_item_quantity]}\",\"{0[fee]}\",\"{0[total_amount]}\",\"{0[auth_identifier]}\",\"{0[authz_identifier]}\",\"{0[membership_name]}\",\"{0[membergroup_name]}\",\"{0[point]}\",\"{0[payment_method]}\",\"{0[delivery_method]}\",\"{0[user_agent]}\",\"{1}\",\"{0[payment_method_name]}\",\"{0[delivery_method_name]}\"\n".format(row, get_channel_str(row['channel'])).encode('utf-8'))
 
         file_operator.close_tmp_file()
         file_operator.upload()
