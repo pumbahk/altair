dde917a56dbe9fe7657cdefa094959a8c946bf8b
noracorn <keiichi_okada@ticketstar.jp>
2017-05-29 16:48:04 +0900

    売上レポートが紐付いているイベントが削除されるとエラーになる refs #tkt3546

--- ticketing/src/altair/app/ticketing/events/sales_reports/commands.py
+++ ticketing/src/altair/app/ticketing/events/sales_reports/commands.py
@@ -74,6 +74,10 @@ def main(argv=sys.argv):
             limited_from = from_date.strftime('%Y-%m-%d 00:00')
 
         event = report_setting.event
+        if not event:
+            # イベント削除されているもの
+            continue
+
         performance = report_setting.performance
         params = dict(
             event_id=report_setting.event_id,
