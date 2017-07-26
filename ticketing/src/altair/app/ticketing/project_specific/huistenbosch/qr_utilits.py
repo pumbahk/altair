c7bd51e7b988437435e2f54d2efaa99e3140d372
Liu <nantsou.liu@rakuten.com>
2017-07-26 15:37:11 +0900

    ref#tkt3838 指摘対応

--- ticketing/src/altair/app/ticketing/project_specific/huistenbosch/qr_utilits.py
+++ ticketing/src/altair/app/ticketing/project_specific/huistenbosch/qr_utilits.py
@@ -113,7 +113,7 @@ def make_data_for_qr(history):
 
     params['valid_date_from'] = performance.open_on.strftime('%Y%m%d') if performance.open_on else '0' * 8
     params['valid_date_to'] = performance.end_on.strftime('%Y%m%d') if performance.end_on else '0' * 8
-    params['issued_at'] = performance.start_on.strftime('%Y%m%d') if performance.end_on else '0' * 8
+    params['issued_at'] = performance.start_on.strftime('%Y%m%d') if performance.start_on else '0' * 8
     params['enterable_from'] = performance.start_on.strftime('%H%M') if performance.start_on else '0' * 4
     usable_date_to = qr_ticket_obj.order.created_at + timedelta(days=int(usable_days))
     params['usable_date_to'] = usable_date_to.strftime('%Y%m%d')
