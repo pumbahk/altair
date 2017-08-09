f608a655bca783f75b002cf6b04923ba65d7ccf4
noracorn <keiichi_okada@ticketstar.jp>
2017-08-09 10:43:21 +0900

    SPDBの項目に商品明細数と商品明細総数を増やしました refs #tkt3840

--- ticketing/src/altair/app/ticketing/spdb/commands.py
+++ ticketing/src/altair/app/ticketing/spdb/commands.py
@@ -40,7 +40,7 @@ spdb_sql = """
         ProductItem.name as product_item_name,
         Seat.name as seat_name,
         ProductItem.price as product_item_price,
-        ProductItem.quantity as product_item_quantity,
+        ProductItem.quantity * OrderedProduct.quantity as product_item_quantity,
         `Order`.system_fee + `Order`.special_fee + `Order`.delivery_fee + `Order`.transaction_fee AS fee,
         `Order`.total_amount,
         UserCredential.auth_identifier,
@@ -273,10 +273,10 @@ def main():
         cur = con.cursor()
         cur.execute(sql_creater.sql)
         orders = cur.fetchall()
-        file_operator.write_tmp_file(u"\"PrimaryKey\",\"予約番号\",\"ステータス\",\"支払いステータス\",\"予約時間\",\"ユーザID\",\"イベントID\",\"イベントタイトル\",\"パフォーマンスID\",\"パフォーマンス名\",\"開演時間\",\"商品明細名\",\"席名\",\"商品明細金額\",\"商品明細個数\",\"手数料\",\"合計金額\",\"auth_identifier\",\"authz_identifier\",\"会員種別名\",\"会員区分名\",\"ポイント\",\"支払い方法\",\"引取方法\",\"デバイス\",\"チャネル\",\"支払い方法名\",\"引取方法名\",\"枝番号\"\n".encode('utf-8'))
+        file_operator.write_tmp_file(u"\"PrimaryKey\",\"予約番号\",\"ステータス\",\"支払いステータス\",\"予約時間\",\"ユーザID\",\"イベントID\",\"イベントタイトル\",\"パフォーマンスID\",\"パフォーマンス名\",\"開演時間\",\"商品明細名\",\"席名\",\"商品明細金額\",\"商品明細個数\",\"商品明細総数\",\"手数料\",\"合計金額\",\"auth_identifier\",\"authz_identifier\",\"会員種別名\",\"会員区分名\",\"ポイント\",\"支払い方法\",\"引取方法\",\"デバイス\",\"チャネル\",\"支払い方法名\",\"引取方法名\",\"枝番号\"\n".encode('utf-8'))
         for row in orders:
             row = clean_data(row)
-            file_operator.write_tmp_file(u"\"{0[PrimaryKey]}\",\"{0[order_no]}\",\"{0[order_status]}\",\"{0[payment_status]}\",\"{0[created_at]}\",\"{0[user_id]}\",\"{0[event_id]}\",\"{0[event_title]}\",\"{0[performance_id]}\",\"{0[performance_name]}\",\"{0[performance_start_on]}\",\"{0[product_item_name]}\",\"{0[seat_name]}\",\"{0[product_item_price]}\",\"{0[product_item_quantity]}\",\"{0[fee]}\",\"{0[total_amount]}\",\"{0[auth_identifier]}\",\"{0[authz_identifier]}\",\"{0[membership_name]}\",\"{0[membergroup_name]}\",\"{0[point]}\",\"{0[payment_method]}\",\"{0[delivery_method]}\",\"{0[user_agent]}\",\"{0[channel]}\",\"{0[payment_method_name]}\",\"{0[delivery_method_name]}\",\"{0[branch_no]}\"\n".format(row).encode('utf-8'))
+            file_operator.write_tmp_file(u"\"{0[PrimaryKey]}\",\"{0[order_no]}\",\"{0[order_status]}\",\"{0[payment_status]}\",\"{0[created_at]}\",\"{0[user_id]}\",\"{0[event_id]}\",\"{0[event_title]}\",\"{0[performance_id]}\",\"{0[performance_name]}\",\"{0[performance_start_on]}\",\"{0[product_item_name]}\",\"{0[seat_name]}\",\"{0[product_item_price]}\",\"1\",\"{0[product_item_quantity]}\",\"{0[fee]}\",\"{0[total_amount]}\",\"{0[auth_identifier]}\",\"{0[authz_identifier]}\",\"{0[membership_name]}\",\"{0[membergroup_name]}\",\"{0[point]}\",\"{0[payment_method]}\",\"{0[delivery_method]}\",\"{0[user_agent]}\",\"{0[channel]}\",\"{0[payment_method_name]}\",\"{0[delivery_method_name]}\",\"{0[branch_no]}\"\n".format(row).encode('utf-8'))
 
         file_operator.close_tmp_file()
         file_operator.upload()
