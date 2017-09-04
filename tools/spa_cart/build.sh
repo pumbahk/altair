36209c55e8959e5f18feed626d210fe79425aecb
SASAO Takahiro <ts-takahiro.a.sasao@rakuten.com>
2017-09-05 00:14:14 +0900

    update build script for new-cart.

--- tools/spa_cart/build.sh
+++ tools/spa_cart/build.sh
@@ -1,43 +1,30 @@
 #!/bin/sh
 
-if [ `git status | head -1 | grep detached | wc -l` = 0 ] ; then
-		git checkout `git rev-parse HEAD`
-fi
+BRANCH=develop
+TMPDIR=altair-new-cart-$(date "+%Y%m%d%H%M%S")
+NGOPT="--aot=false --output-hashing=all --sourcemap=false --extract-css=true --environment=prod"
 
-if [ -d ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart ] ; then
-		echo "please remove ticketing/src/altair/app/ticketing/cart/static/spa_cart/ before use this script."
-		exit
-fi
+git clone -b $BRANCH --depth 1 git@github.com:ticketstar/altair-new-cart.git $TMPDIR
+REV=`(cd $TMPDIR ; git rev-parse HEAD)`
 
-if [ -d ./altair-new-cart-x ] ; then
-		echo "please remove working directory: altair-new-cart-x/"
-		exit
-fi
-
-git clone -b develop --depth 1 git@github.com:ticketstar/altair-new-cart.git altair-new-cart-x
-REV=`(cd altair-new-cart-x ; git rev-parse HEAD)`
-
-if [ -d ./altair-new-cart-$REV ] ; then
-		echo "found same revision. please remove: altair-new-cart-$REV/"
-		rm -rf altair-new-cart-x
-		exit
-fi
-
-mv altair-new-cart-x altair-new-cart-$REV
-
-cd altair-new-cart-$REV
+cd $TMPDIR
 npm update
-ng build --base-href=/cart/spa/ --deploy-url=/cart/static/spa_cart/
+ng build --base-href=/cart/spa/ --deploy-url=/cart/static/spa_cart/ $NGOPT
+sed -i "" '/\/\/# sourceMappingURL=/d' dist/*.js
 for x in dist/*.js
 do
 		gzip -c $x > $x.gz
 done
-cp -r dist ../../../ticketing/src/altair/app/ticketing/cart/static/spa_cart
 cd ..
+
+if [ -d ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart ] ; then
+    git rm ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/*
+fi
+
+cp -r $TMPDIR/dist ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart
+rm ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/index.html
+cp $TMPDIR/dist/index.html ../../ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart/
 
 git add ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart -f
 
-git commit -m "temporary added: built spa $REV"
-
-echo "completed. you can push it by following command:"
-echo "  git push origin HEAD:feature/new-cart-qa -f"
+git commit -m "merge new-cart $REV by script"
