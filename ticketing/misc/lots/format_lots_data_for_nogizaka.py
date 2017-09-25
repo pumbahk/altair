5d7abf14d71f9e87796a44e90c296b7c44a4c64c
norabal <norabal.works@gmail.com>
2017-09-25 15:55:07 +0900

    末列に抽選IDを追加 refs #tkt3974

--- ticketing/misc/lots/format_lots_data_for_nogizaka.py
+++ ticketing/misc/lots/format_lots_data_for_nogizaka.py
@@ -4,6 +4,8 @@ import sys
 
 
 def main(argv=sys.argv):
+    lot_id = argv[1]
+
     add_header = map(lambda name: name.encode('sjis'), [
         u"同行者・姓",
         u"同行者名・名",
@@ -14,12 +16,13 @@ def main(argv=sys.argv):
         u"申込者乃木坂46モバイル会員登録端末電話番号(半角・ハイフン不要)",
         u"申込者名・名",
         u"申込者名・姓(来場代表者苗字)",
+        u"抽選ID",
     ])
 
-    result_file = open("{0}_result.csv".format(argv[1]), 'w')
+    result_file = open("{0}_result.csv".format(lot_id), 'w')
     writer = csv.writer(result_file, lineterminator='\n')
 
-    with open("{0}.csv".format(argv[1]), 'r') as f:
+    with open("{0}.csv".format(lot_id), 'r') as f:
         reader = csv.reader(f)
         order_list = list()
         first_row = None
@@ -37,7 +40,7 @@ def main(argv=sys.argv):
 
             # 次の申し込み番号に切り替わった時点で追加情報を収集
             if keep_order_no != row[1]:
-                write_formatted_content(writer, order_list)
+                write_formatted_content(writer, order_list, lot_id)
 
                 # ループ内変数のリセット
                 order_list = list()
@@ -47,20 +50,26 @@ def main(argv=sys.argv):
             order_list.append(row)
 
     # ファイル末尾の購入情報のフォーマットと書き込み
-    write_formatted_content(writer, order_list)
+    write_formatted_content(writer, order_list, lot_id)
 
     result_file.close()
 
 
-def write_formatted_content(writer, order_list):
-    '''9行に別れた追加情報を先頭行の予約にまとめて付加する'''
+def write_formatted_content(writer, order_list, lot_id):
+    '''
+    9行に別れた追加情報を先頭行の予約にまとめて付加する
+    末列に抽選IDも付加
+    '''
+
     out_list = order_list[0]
     del out_list[44]
     del out_list[45]
     for i in range(1, 9):
         out_list.append(order_list[i][45])
 
-    writer.writerow(out_list)
+    out_list.append(lot_id)
+
+    return writer.writerow(out_list)
 
 
 if __name__ == "__main__":
