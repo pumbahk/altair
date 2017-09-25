fe3c9549b3e4b14f134c7d4fe13d6d22aef08943
norabal <norabal.works@gmail.com>
2017-09-25 15:26:51 +0900

    ヘッダー行に追加情報を文字化けせずに表示 refs #tkt3974

--- ticketing/misc/lots/format_lots_data_for_nogizaka.py
+++ ticketing/misc/lots/format_lots_data_for_nogizaka.py
@@ -4,40 +4,63 @@ import sys
 
 
 def main(argv=sys.argv):
+    add_header = map(lambda name: name.encode('sjis'), [
+        u"同行者・姓",
+        u"同行者名・名",
+        u"同行者電話番号(半角・ハイフン不要)",
+        u"最終確認",
+        u"申込時の注意事項",
+        u"申込者乃木坂46モバイル会員登録メールアドレス",
+        u"申込者乃木坂46モバイル会員登録端末電話番号(半角・ハイフン不要)",
+        u"申込者名・名",
+        u"申込者名・姓(来場代表者苗字)",
+    ])
+
     result_file = open("{0}_result.csv".format(argv[1]), 'w')
     writer = csv.writer(result_file, lineterminator='\n')
-    out_header = ['同行者・姓', '同行者名・名', '同行者電話番号(半角・ハイフン不要)', '最終確認', '申込時の注意事項', '申込者乃木坂46モバイル会員登録メールアドレス',
-            '申込者乃木坂46モバイル会員登録端末電話番号(半角・ハイフン不要)', '申込者名・名', '申込者名・姓(来場代表者苗字)']
-    writer.writerow(out_header)
 
-    position = 0
     with open("{0}.csv".format(argv[1]), 'r') as f:
         reader = csv.reader(f)
-        header = next(reader)
-
         order_list = list()
+        first_row = None
+        keep_order_no = None
+
         for row in reader:
-            position += 1
+            # ヘッダー行作成
+            if first_row is None:
+                first_row = row + add_header
+                writer.writerow(first_row)
+                continue
+
+            if keep_order_no is None:
+                keep_order_no = row[1]
+
+            # 次の申し込み番号に切り替わった時点で追加情報を収集
+            if keep_order_no != row[1]:
+                write_formatted_content(writer, order_list)
+
+                # ループ内変数のリセット
+                order_list = list()
+                keep_order_no = None
+
+            # order_listに取得した行の情報をストック
             order_list.append(row)
 
-            if position == 9:
-                out_list = order_list[0]
-                del out_list[44]
-                del out_list[45]
-                out_list.append(order_list[1][45])
-                out_list.append(order_list[2][45])
-                out_list.append(order_list[3][45])
-                out_list.append(order_list[4][45])
-                out_list.append(order_list[5][45])
-                out_list.append(order_list[6][45])
-                out_list.append(order_list[7][45])
-                out_list.append(order_list[8][45])
-                writer.writerow(out_list)
-
-                position = 0
-                order_list = list()
+    # ファイル末尾の購入情報のフォーマットと書き込み
+    write_formatted_content(writer, order_list)
 
     result_file.close()
+
+
+def write_formatted_content(writer, order_list):
+    '''9行に別れた追加情報を先頭行の予約にまとめて付加する'''
+    out_list = order_list[0]
+    del out_list[44]
+    del out_list[45]
+    for i in range(1, 9):
+        out_list.append(order_list[i][45])
+
+    writer.writerow(out_list)
 
 
 if __name__ == "__main__":
