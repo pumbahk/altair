d76ba16be757ead3b08436a4df0b1cec5b00fa7c
SASAO Takahiro <ts-takahiro.a.sasao@rakuten.com>
2017-10-17 18:23:48 +0900

    fix index.html for spa cart, improve build script.

--- tools/spa_cart/build.sh
+++ tools/spa_cart/build.sh
@@ -20,16 +20,17 @@ node -v || exit 1
 /bin/echo -n "Checking npm... "
 npm -v || exit 1
 
-WORKDIR=$(cd ../../ticketing/src/altair/app/ticketing/spa_cart && pwd)
+BASEDIR=$(cd $(dirname $0) ; cd ../../; pwd)
+SRCDIR=$BASEDIR/ticketing/src/altair/app/ticketing/spa_cart
 
 echo "Calculating digest of source files..."
 TARGET_EXTS="ts|css|html|json|svg|ico|otf"
-COUNT=$(cd $WORKDIR && find package.json src -type f | egrep -e "\.($TARGET_EXTS)$" | wc -l | tr -d ' ')
-DIGEST=$(cd $WORKDIR && find package.json src -type f | egrep -e "\.($TARGET_EXTS)$" | sort | xargs $MD5 | $MD5 | cut -d " " -f 1)
+COUNT=$(cd $SRCDIR && find package.json src -type f | egrep -e "\.($TARGET_EXTS)$" | wc -l | tr -d ' ')
+DIGEST=$(cd $SRCDIR && find package.json src -type f | egrep -e "\.($TARGET_EXTS)$" | sort | xargs $MD5 | $MD5 | cut -d " " -f 1)
 echo " -> $COUNT files, digest: $DIGEST"
 
-if [ -e ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/version ] ; then
-  BUILT_DIGEST=$(cat ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/version)
+if [ -e $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart/version ] ; then
+  BUILT_DIGEST=$(cat $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart/version)
 fi
 
 if [ X$DIGEST == X$BUILT_DIGEST ] ; then
@@ -39,26 +40,26 @@ if [ X$DIGEST == X$BUILT_DIGEST ] ; then
   exit
 fi
 
-if [ -d $WORKDIR/node_modules ] ; then
+if [ -d $SRCDIR/node_modules ] ; then
   echo "Found node_modules directory."
   if [ X$BRANCH == Xdevelop ] ; then
     echo "Removing node_modules..."
-    rm -rf $WORKDIR/node_modules
+    rm -rf $SRCDIR/node_modules
   fi
 fi
 
-DISTDIR=$WORKDIR/dist-$$
+DISTDIR=$SRCDIR/dist-$$
 [ -e $DISTDIR ] && rm -rf $DISTDIR
 mkdir -p $DISTDIR
 
 echo "Running angular build process..."
-(cd $WORKDIR && npm install && ./node_modules/.bin/ng build --output-path=$DISTDIR $NGBASEOPT $NGOPT)
+(cd $SRCDIR && npm install && ./node_modules/.bin/ng build --stats-json --output-path=$DISTDIR $NGBASEOPT $NGOPT)
 echo "Completed"
 
 echo "Injecting rollbar..."
 for x in $DISTDIR/main.*.bundle.js
 do
-    (echo "" ; cat rollbar.js) >> $x
+    (echo "" ; cat $(dirname $0)/rollbar.js) >> $x
 done
 
 echo "Making gzip version scripts..."
@@ -69,22 +70,22 @@ done
 
 # install
 echo "Installing files to ticketing/src/altair/app/ticketing/cart/..."
-mkdir -p ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart
-find ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/ -type f -delete
-(cd $DISTDIR ; tar cf - --exclude index.html .) | (cd ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart ; tar xf -)
-mkdir -p ../../ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart
-cp $DISTDIR/index.html ../../ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart/
+mkdir -p $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart
+find $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart/ -type f -delete
+(cd $DISTDIR ; tar cf - --exclude index.html .) | (cd $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart ; tar xf -)
+mkdir -p $BASEDIR/ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart
+cp $DISTDIR/index.html $BASEDIR/ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart/
 
-echo $DIGEST > ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/version
+echo $DIGEST > $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart/version
 
 # remove workdir
-echo "Removing working directory..."
-rm -rf $DISTDIR
+#echo "Removing working directory..."
+#rm -rf $DISTDIR
 
 # commit
 if [ X$BRANCH == Xdevelop ] ; then
-    git add ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart -f -A
-    git add ../../ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart/index.html
-    git add ../../ticketing/src/altair/app/ticketing/cart/static/spa_cart/version
+    git add $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart -f -A
+    git add $BASEDIR/ticketing/src/altair/app/ticketing/cart/templates/eagles/pc/spa_cart/index.html
+    git add $BASEDIR/ticketing/src/altair/app/ticketing/cart/static/spa_cart/version
     git commit -m "rebuild spa_cart by script"
 fi
