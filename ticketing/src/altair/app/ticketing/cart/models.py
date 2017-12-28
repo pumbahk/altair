2eb2b018e88ae3fb291a7ad85795c43f8cad8c93
noracorn <keiichi_okada@ticketstar.jp>
2017-12-28 19:55:53 +0900

    ディスカウントコードの金額の判定の指摘事項を修正 refs #tkt4482

--- ticketing/src/altair/app/ticketing/cart/models.py
+++ ticketing/src/altair/app/ticketing/cart/models.py
@@ -386,9 +386,12 @@ class Cart(Base, c_models.CartMixin):
         for target in valid_target:
             status = True
             for item in self.items:
+                if not status:
+                    break
                 for element in item.elements:
                     if element.price > Decimal(target.discount_code_setting.condition_price_amount):
                         status = False
+                        break
             if status:
                 return True
 
