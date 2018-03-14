e0b76499e1df9d00cc991c3bb104865381f43cf2
Liu, Nan-Tsou <nantsou.liu@rakuten.com>
2018-03-14 10:58:13 +0900

    ref#tkt5117 QRAESDeliveryMethodFormをdelivery_methods/forms.pyに移す

--- ticketing/src/altair/app/ticketing/delivery_methods/plugins/base.py
+++ ticketing/src/altair/app/ticketing/delivery_methods/plugins/base.py
@@ -2,22 +2,8 @@
 
 from zope.interface import implementer
 
-from altair.app.ticketing.core.models import DeliveryMethodPlugin
-
 from ..interfaces import IDeliveryFormMaker
-from ..forms import DeliveryMethodForm
-
-class QRAESDeliveryMethodForm(DeliveryMethodForm):
-    def __init__(self, formdata=None, obj=None, prefix='', **kwargs):
-        super(QRAESDeliveryMethodForm, self).__init__(formdata=formdata, obj=obj, prefix=prefix, **kwargs)
-        self.delivery_plugin_id.choices = [(dmp.id, dmp.name) for dmp in DeliveryMethodPlugin.all()]
-
-    def get_customized_fields(self):
-        names = []
-        for attr in dir(self):
-            if attr.startswith('qr_aes_'):
-                names.append(attr)
-        return names
+from ..forms import QRAESDeliveryMethodForm
 
 @implementer(IDeliveryFormMaker)
 class QRAESDeliveryFormMaker(object):
