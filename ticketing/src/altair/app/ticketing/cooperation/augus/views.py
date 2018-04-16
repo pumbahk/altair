1e17ae5791749196d399318c1a84a429377cdb9a
ts-yasuhito.ito <ts-yasuhito.ito>
2018-04-16 18:23:01 +0900

    TKT-5267 指摘対応　不要コード削除 modelから参照する場合は論理削除されたレコードが除外される

--- ticketing/src/altair/app/ticketing/cooperation/augus/views.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/views.py
@@ -706,9 +706,7 @@ class AugusPutbackView(_AugusBaseView):
                  renderer='altair.app.ticketing:templates/cooperation/augus/events/putback/index.html')
     def index(self):
         augus_performance_ids = [perf.augus_performances[0].id for perf in self.context.event.performances
-                              if perf.deleted_at is None
-                              and perf.augus_performances
-                              and perf.augus_performances[0].deleted_at is None]
+                              if perf.augus_performances]
         putbacks = AugusPutback.query.filter(AugusPutback.augus_performance_id.in_(augus_performance_ids)).all()
 
         return dict(event=self.context.event,
