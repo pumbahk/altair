d43332307b1d16de90ddce99aaee85c98404544f
Liu, Nan-Tsou <nantsou.liu@rakuten.com>
2018-05-23 19:14:12 +0900

    ref#tkt5532 新規追加の時表示しないバグを修正です

--- ticketing/alembic/versions/13151cb706a_add_reception_time_columns_for_orion_.py
+++ ticketing/alembic/versions/13151cb706a_add_reception_time_columns_for_orion_.py
@@ -12,14 +12,14 @@ down_revision = '3cd361e34c44'
 
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.sql.expression import text
-from sqlalchemy.sql import functions as sqlf
 
 Identifier = sa.BigInteger
 
 def upgrade():
     op.add_column('ResaleSegment', sa.Column('resale_start_at', sa.DateTime(), nullable=True))
     op.add_column('ResaleSegment', sa.Column('resale_end_at', sa.DateTime(), nullable=True))
+    sql = "UPDATE ResaleSegment SET resale_start_at = start_at, resale_end_at=end_at;"
+    op.execute(sql)
 
 def downgrade():
     op.drop_column('ResaleSegment', 'resale_start_at')
