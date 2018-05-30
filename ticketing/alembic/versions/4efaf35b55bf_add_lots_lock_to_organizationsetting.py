e2418f2a9a2ddf6c0d7295264b7612d3fa57e502
pumbahk <pumbahk@gmail.com>
2018-05-31 00:07:03 +0900

    TKT-5591 [alembic]当選処理に在庫チェックがOrg/Event/Lotの設定によって外せるようにする

---
+++ ticketing/alembic/versions/4efaf35b55bf_add_lots_lock_to_organizationsetting.py
@@ -0,0 +1,27 @@
+"""add lots lock to OrganizationSetting
+Revision ID: 4efaf35b55bf
+Revises: 13151cb706a
+Create Date: 2018-05-22 15:52:42.554896
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4efaf35b55bf'
+down_revision = '13151cb706a'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import functions as sqlf
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.add_column('OrganizationSetting',
+                  sa.Column('lot_entry_lock', sa.Boolean(), nullable=False, default=False, server_default=text('0')))
+    op.add_column('LotElectedEntry',
+                  sa.Column('completed_at', sa.DateTime(), nullable=True))
+
+def downgrade():
+    op.drop_column('OrganizationSetting', 'lot_entry_lock')
+    op.drop_column('LotElectedEntry', 'completed_at')
