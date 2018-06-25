dc3bf0f8a68dedba76b1b24fda7cb501a4ca961b
ts-yasuhito.ito <ts-yasuhito.ito>
2018-06-25 18:28:07 +0900

    TKT-4147 alembicのrevisionをdevelopに合わせて修正

--- ticketing/alembic/versions/2fecb351192b_add_column_organizationsetting_enable_.py
+++ ticketing/alembic/versions/2fecb351192b_add_column_organizationsetting_enable_.py
@@ -1,14 +1,14 @@
 """add_column_organizationsetting_enable_price_batch_update
 
 Revision ID: 2fecb351192b
-Revises: 34ed7cee0c78
+Revises: 133d6f048788
 Create Date: 2018-06-20 09:49:41.857835
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '2fecb351192b'
-down_revision = '34ed7cee0c78'
+down_revision = '133d6f048788'
 
 from alembic import op
 import sqlalchemy as sa
