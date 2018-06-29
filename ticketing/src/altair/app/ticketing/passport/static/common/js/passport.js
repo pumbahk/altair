85969083aa4a01319f54fa13b1896ffd845b9a60
noracorn <keiichi_okada@ticketstar.jp>
2018-06-29 21:34:02 +0900

    パスポートのアプリのデザイン部分のみ refs #tkt5858

--- ticketing/src/altair/app/ticketing/passport/static/common/js/passport.js
+++ ticketing/src/altair/app/ticketing/passport/static/common/js/passport.js
@@ -16,6 +16,7 @@ $(document).on('click', '.passport_button', function() {
   var next_url = $(this).attr("next_url");
   var check_url = $(this).attr("check_url");
 
+
   check_can_use_passport(check_url).done(function(result) {
     if (result == true) {
       console.log("passport is not used");
@@ -57,3 +58,43 @@ function centeringModalSyncer(content){
 	content.css({"left": pxleft + "px"});
 	content.css({"top": pxtop + "px"});
 }
+
+  $(document).ready(function(){
+    $("#btnAllPassport").hide();
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 50) {
+            $("#btnAllPassport").fadeIn("fast");
+        } else {
+            $("#btnAllPassport").fadeOut("fast");
+        }
+        scrollHeight = $(document).height();
+        scrollPosition = $(window).height() + $(window).scrollTop();
+        footHeight = $("footer").innerHeight();
+        if ( scrollHeight - scrollPosition  <= footHeight ) {
+            $(".btnAllPassport").css({
+                "position":"fixed",
+                "bottom": footHeight+5
+            });
+        } else {
+            $(".btnAllPassport").css({
+                "position":"fixed",
+                "bottom": "5px"
+            });
+        }
+    });
+    $('#btnAllPassport').click(function () {
+        $('body,html').animate({
+        scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
+
+    var required_confirm = null;
+    if (document.cookie.indexOf("_passport") > -1) {
+        $(".confirm_exist").hide();
+        $(".confirm_not_exist").show();
+    } else {
+        $(".confirm_exist").show();
+        $(".confirm_not_exist").hide();
+    }
