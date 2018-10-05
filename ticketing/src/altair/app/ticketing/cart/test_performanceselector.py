ce5de7c055b0c00911b74c7f4d75c856bcb54d1d
noracorn <keiichi_okada@ticketstar.jp>
2018-10-05 20:54:08 +0900

    performanceselectorのテストケース。スマホ refs #tkt2162

--- ticketing/src/altair/app/ticketing/cart/test_performanceselector.py
+++ ticketing/src/altair/app/ticketing/cart/test_performanceselector.py
@@ -58,7 +58,6 @@ class MatchUpPerformanceSelectorTests(PerformaceSelectorTestsBase, unittest.Test
 
     def test_a_sales_segment(self):
         # PCとスマホのモックが難しいため、こちらでPCの場合のテスト
-        # @TODO スマホの場合のテスト
         self.config.add_route('cart.seat_types2', '/testing/seat_types2/{performance_id}/{sales_segment_id}')
         self.config.add_route('cart.order', '/testing/order/{sales_segment_id}')
         from datetime import datetime
@@ -149,6 +148,100 @@ class MatchUpPerformanceSelectorTests(PerformaceSelectorTestsBase, unittest.Test
                           'name_mobile': u'2013年4月1日(月) - 4月2日(火) 前売券',
                           })
 
+    @mock.patch('altair.app.ticketing.cart.performanceselector._PerformanceSelector.venue_label')
+    def test_a_sales_segment_smartphone(self, venue_label):
+        # PCとスマホのモックが難しいため、こちらでPCの場合のテスト
+        venue_label.return_value = u"テスト会場 前売券"
+        self.config.add_route('cart.seat_types2', '/testing/seat_types2/{performance_id}/{sales_segment_id}')
+        self.config.add_route('cart.order', '/testing/order/{sales_segment_id}')
+        from datetime import datetime
+        self.request.context = testing.DummyResource(
+            request=self.request,
+            event=DummyEvent(id=123),
+            available_sales_segments=[
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance1',
+                        id=1,
+                        start_on=datetime(2013, 3, 31),
+                        end_on=datetime(2013, 4, 1),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=1,
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=2,
+                    max_quantity=3
+                    ),
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance1',
+                        id=3,
+                        start_on=datetime(2013, 4, 1),
+                        end_on=datetime(2013, 4, 1),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=3
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=4,
+                    max_quantity=10
+                    ),
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance2',
+                        id=5,
+                        start_on=datetime(2013, 4, 1),
+                        end_on=datetime(2013, 4, 2),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=2
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=6,
+                    max_quantity=10
+                    ),
+                ]
+            )
+
+        target = self._makeOne(self.request)
+        result = target()
+
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0][0], u'testing performance1')
+
+        self.assertEqual(result[0][1][0],
+                         {'id': 2,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/1/2',
+                          'max_quantity': 3,
+                          'order_url': 'http://example.com/testing/order/2',
+                          'name': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          'name_pc': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          'name_smartphone': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          'name_mobile': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          })
+        self.assertEqual(result[0][1][1],
+                         {'id': 4,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/3/4',
+                          'max_quantity': 10,
+                          'order_url': 'http://example.com/testing/order/4',
+                          'name': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          'name_pc': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          'name_smartphone': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          'name_mobile': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          })
+        self.assertEqual(result[1][0], u'testing performance2')
+        self.assertEqual(result[1][1][0],
+                         {'id': 6,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/5/6',
+                          'max_quantity': 10,
+                          'order_url': 'http://example.com/testing/order/6',
+                          'name': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                          'name_pc': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                          'name_smartphone': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                          'name_mobile': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                          })
+
 
 class DatePerformanceSelectorTests(PerformaceSelectorTestsBase, unittest.TestCase):
     maxDiff = None
@@ -159,7 +252,6 @@ class DatePerformanceSelectorTests(PerformaceSelectorTestsBase, unittest.TestCas
 
     def test_a_sales_segment(self):
         # PCとスマホのモックが難しいため、こちらでPCの場合のテスト
-        # @TODO スマホの場合のテスト
         self.config.add_route('cart.seat_types2', '/testing/seat_types2/{performance_id}/{sales_segment_id}')
         self.config.add_route('cart.order', '/testing/order/{sales_segment_id}')
         from datetime import datetime
@@ -273,6 +365,124 @@ class DatePerformanceSelectorTests(PerformaceSelectorTestsBase, unittest.TestCas
                           'name_pc': u'00:00 前売券',
                           'name_smartphone': u'00:00 前売券',
                           'name_mobile': u'2013年4月1日(月) 00:00 前売券',
+                          })
+
+    @mock.patch('altair.app.ticketing.cart.performanceselector._PerformanceSelector.venue_label')
+    def test_a_sales_segment_smartphone(self, venue_label):
+        # PCとスマホのモックが難しいため、こちらでスマホの場合のテスト
+        venue_label.return_value = u"テスト会場 前売券"
+        self.config.add_route('cart.seat_types2', '/testing/seat_types2/{performance_id}/{sales_segment_id}')
+        self.config.add_route('cart.order', '/testing/order/{sales_segment_id}')
+        from datetime import datetime
+        self.request.context = testing.DummyResource(
+            request=self.request,
+            event=DummyEvent(id=123),
+            available_sales_segments=[
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance1',
+                        id=2,
+                        start_on=datetime(2013, 3, 31),
+                        end_on=datetime(2013, 4, 1),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=1
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=1,
+                    max_quantity=3,
+                    ),
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance2',
+                        id=4,
+                        start_on=datetime(2013, 4, 1),
+                        end_on=datetime(2013, 4, 2),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=2
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=3,
+                    max_quantity=10,
+                    ),
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance3',
+                        id=6,
+                        start_on=datetime(2013, 4, 1),
+                        end_on=datetime(2013, 4, 1),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=3
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 1),
+                    id=5,
+                    max_quantity=10,
+                    ),
+                DummySalesSegment(
+                    performance=DummyPerformance(
+                        name=u'testing performance4',
+                        id=8,
+                        start_on=datetime(2013, 4, 1),
+                        end_on=datetime(2013, 4, 1),
+                        venue=testing.DummyModel(name=u"テスト会場"),
+                        display_order=3
+                        ),
+                    name=u"前売券",
+                    start_at=datetime(2013, 1, 2),
+                    id=7,
+                    max_quantity=10,
+                    ),
+                ]
+            )
+
+        target = self._makeOne(self.request)
+        result = target()
+
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0][0], u'2013年3月31日(日) - 4月1日(月)')
+        self.assertEqual(result[0][1][0],
+                         {'id': 1,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/2/1',
+                          'order_url': 'http://example.com/testing/order/1',
+                          'max_quantity': 3,
+                          'name': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          'name_pc': u'テスト会場 前売券',
+                          'name_smartphone': u'テスト会場 前売券',
+                          'name_mobile': u'2013年3月31日(日) - 4月1日(月) テスト会場 前売券',
+                          })
+        self.assertEqual(result[1][0], u'2013年4月1日(月) - 4月2日(火)')
+        self.assertEqual(result[1][1][0],
+                         {'id': 3,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/4/3',
+                          'order_url': 'http://example.com/testing/order/3',
+                          'max_quantity': 10,
+                          'name': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                          'name_pc': u'テスト会場 前売券',
+                          'name_smartphone': u'テスト会場 前売券',
+                          'name_mobile': u'2013年4月1日(月) - 4月2日(火) テスト会場 前売券',
+                         })
+        self.assertEqual(result[2][0], u'2013年4月1日(月)')
+        self.assertEqual(result[2][1][0],
+                         {'id': 5,
+                          'seat_types_url': 'http://example.com/testing/seat_types2/6/5',
+                          'order_url': 'http://example.com/testing/order/5',
+                          'max_quantity': 10,
+                          'name': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          'name_pc': u'00:00 テスト会場 前売券',
+                          'name_smartphone': u'00:00 テスト会場 前売券',
+                          'name_mobile': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          })
+        self.assertEqual(result[2][1][1],
+                         {'id': 7,
+                          'order_url': 'http://example.com/testing/order/7',
+                          'seat_types_url': 'http://example.com/testing/seat_types2/8/7',
+                          'max_quantity': 10,
+                          'name': u'2013年4月1日(月) 00:00 テスト会場 前売券',
+                          'name_pc': u'00:00 テスト会場 前売券',
+                          'name_smartphone': u'00:00 テスト会場 前売券',
+                          'name_mobile': u'2013年4月1日(月) 00:00 テスト会場 前売券',
                           })
 
 
