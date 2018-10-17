ff5ea6bf434a38c2762acd82816528b3dcf0da14
norabal <norabal.works@gmail.com>
2018-10-17 11:55:40 +0900

    タスクのタイムアウトを削除 TKT-5973

--- ticketing/src/altair/app/ticketing/orders/workers/tasks.py
+++ ticketing/src/altair/app/ticketing/orders/workers/tasks.py
@@ -146,7 +146,6 @@ def import_per_task(context, request):
              name="notify_update_ticket_info",
              consumer="notify_update_ticket_info",
              queue="notify_update_ticket_info",
-             timeout=7200,
              arguments={"maxPriority": 10})
 def notify_update_ticket_info(context, request):
     """現行の券面構成をSEJとFMに通知するため、orders.apiのrefresh_orderを利用して予約を更新する"""
