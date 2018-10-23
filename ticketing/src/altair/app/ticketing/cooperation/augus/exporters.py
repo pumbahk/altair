2404499b9ff1c1b85f3ea78e3607a74c1efe5a34
noracorn <noracorn@gmail.com>
2018-10-23 10:58:36 +0900

    オーガス販売実績の期間の不等号を修正 refs #tkt6488

--- ticketing/src/altair/app/ticketing/cooperation/augus/exporters.py
+++ ticketing/src/altair/app/ticketing/cooperation/augus/exporters.py
@@ -467,8 +467,8 @@ class AugusAchievementExporter(object):
             .filter(
                 sa.func.IF(
                     Performance.end_on == None,
-                    Performance.start_on < self.now,
-                    Performance.end_on > self.now
+                    Performance.start_on >= self.now,
+                    Performance.end_on >= self.now
                 )
             )
 
