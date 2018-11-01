a8b21961eebe8ab6cd7e651238cf055717f2bb4a
ts-yasuhito.ito <ts-yasuhito.ito>
2018-11-01 16:25:07 +0900

    ポイント充当プロジェクト alembicのrebase

--- ticketing/alembic/versions/367c56cd18b3_add_enable_point_allocation_column_to_.py
+++ ticketing/alembic/versions/367c56cd18b3_add_enable_point_allocation_column_to_.py
@@ -1,14 +1,14 @@
 """Add enable_point_allocation column to OrganizationSetting
 
 Revision ID: 367c56cd18b3
-Revises: 3ca4e33768b4
+Revises: 3f8810964ed2
 Create Date: 2018-10-05 13:59:16.314376
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '367c56cd18b3'
-down_revision = '3ca4e33768b4'
+down_revision = '3f8810964ed2'
 
 from alembic import op
 import sqlalchemy as sa
