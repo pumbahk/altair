8882c509deff35840464745a01f568842581d188
ts-yasuhito.ito <ts-yasuhito.ito>
2018-11-15 10:57:57 +0900

    TKT-6712 予約インポートのポイント返還のRefundPointEntry生成をリファクタリング

--- ticketing/src/altair/app/ticketing/orders/api.py
+++ ticketing/src/altair/app/ticketing/orders/api.py
@@ -1309,20 +1309,9 @@ def create_or_update_orders_from_proto_orders(request, reserving, stocker, proto
             point_amount_to_refund = proto_order.original_order.point_amount - proto_order.point_amount
             if point_amount_to_refund > 0:
                 # TKT-6625 ポイント利用額が減る場合はポイント払い戻し用のデータを生成する
-
                 logger.info(
                     'creating RefundPointEntry to refund point(order_no={})'.format(order.order_no))
-                new_refund_point_entry = RefundPointEntry(
-                    order_id=order.id,
-                    order_no=order.order_no,
-                    refund_point_amount=point_amount_to_refund,
-                )
-                latest_refund_point_entry = get_latest_refund_point_entry(order.order_no)
-                if latest_refund_point_entry is not None:
-                    # 同じ予約番号のRefundPointEntryがすでにある場合、枝番をインクリメント
-                    new_refund_point_entry.seq_no = latest_refund_point_entry.seq_no + 1
-                DBSession.add(new_refund_point_entry)
-                DBSession.flush()
+                RefundPointEntry.create_refund_point_entry(order, point_amount_to_refund)
         except OrderAlreadyDeliveredError as orderAlreadyDeliveredError:
             logger.info(u'failed to update order %s because this order is already delivered' % order.order_no)
             errors_map.setdefault(proto_order.ref, []).append(
@@ -2509,8 +2498,3 @@ def send_task_notify_update_ticket_info(request, bundle_id, priority=0):
         routing_key='notify_update_ticket_info',
         properties=dict(content_type="application/json", priority=priority)
     )
-
-def get_latest_refund_point_entry(order_no):
-    return RefundPointEntry.query.filter(RefundPointEntry.order_no==order_no)\
-        .order_by(RefundPointEntry.seq_no.desc()).first()
-
