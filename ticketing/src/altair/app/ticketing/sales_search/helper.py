19ab6debb8edb658e5c00bf8728fb4ceaea7c578
noracorn <noracorn@gmail.com>
2018-12-07 22:12:35 +0900

    発券開始時刻が違っている場合はオペレータにメッセージを出す refs #tkt5977

--- ticketing/src/altair/app/ticketing/sales_search/helper.py
+++ ticketing/src/altair/app/ticketing/sales_search/helper.py
@@ -86,6 +86,10 @@ class SalesSearchHelper(object):
     def disp_issuing_start_at(vh, sales_segment):
         """
         発券開始時刻をHTMLとして返却する
+        PDMPが複数登録されていても、発券開始時刻は同じになる運用となっている。
+        なので、間違って登録されている場合は、オペレータにメッセージを出力する。
+        １つ目のPDMPの発券開始時刻を返す
+        PDMPが登録されていない場合は、発券開始時刻はなし
 
         Parameters
         ----------
@@ -99,11 +103,24 @@ class SalesSearchHelper(object):
         html : Markup
             発券開始時刻のHTML
         """
+        issuing_start_at_list = []
         for pdmp in sales_segment.sales_segment_group.payment_delivery_method_pairs:
             if pdmp.issuing_start_at:
-                return Markup(
+                issuing_start_at_list.append(
                     u"""<td class="span1">{0}</td>""".format(vh.datetime(pdmp.issuing_start_at, with_weekday=True)))
             else:
-                return Markup(u"""<td class="span1">{0}{1}日後</td>""".format(
+                issuing_start_at_list.append(u"""<td class="span1">{0}{1}日後</td>""".format(
                     date_calculation_str_dict[pdmp.issuing_start_day_calculation_base], pdmp.payment_period_days))
-        return u"-"
+
+        if not issuing_start_at_list:
+            # 決済引取方法の設定がない
+            return Markup(u"-")
+
+        if len(list(set(issuing_start_at_list))) > 1:
+            # 発券開始時刻が違う決済引取方法がある
+            issuing_start_at = issuing_start_at_list[0].replace(u"</td>",
+                                                                u"""<br/><span style="color:red;">※発券開始時刻が違う決済引取方法があります</span></td>""")
+            return Markup(issuing_start_at)
+
+        # すべて同じ発券開始時刻
+        return Markup(issuing_start_at_list[0])
