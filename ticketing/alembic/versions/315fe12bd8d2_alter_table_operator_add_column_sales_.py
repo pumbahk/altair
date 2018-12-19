754d0dddcc9fe5656151d5f6c0077124077fb596
noracorn <noracorn@gmail.com>
2018-12-19 17:03:18 +0900

    販売日程管理検索のalembicをリベース refs #tkt5977

--- ticketing/alembic/versions/315fe12bd8d2_alter_table_operator_add_column_sales_.py
+++ ticketing/alembic/versions/315fe12bd8d2_alter_table_operator_add_column_sales_.py
@@ -1,14 +1,14 @@
 """alter table Operator add column sales_seach
 
 Revision ID: 315fe12bd8d2
-Revises: 3f8810964ed2
+Revises: 2aa9d4efe740
 Create Date: 2018-11-16 10:45:12.976026
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '315fe12bd8d2'
-down_revision = '3f8810964ed2'
+down_revision = '2aa9d4efe740'
 
 import sqlalchemy as sa
 from alembic import op
