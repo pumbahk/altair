80b10034f3ba9e98473da605b324e5e9289e473a
ts-yasuhito.ito <ts-yasuhito.ito>
2019-01-29 17:37:14 +0900

    TKT-7149 RT新カート対応のalembicのrebase

--- ticketing/alembic/versions/16d8ed4fdf59_add_column_organizationsetting_enable_.py
+++ ticketing/alembic/versions/16d8ed4fdf59_add_column_organizationsetting_enable_.py
@@ -1,14 +1,14 @@
 """Add column OrganizationSetting enable_spa_cart
 
 Revision ID: 16d8ed4fdf59
-Revises: 315fe12bd8d2
+Revises: 2e01f7df1fb8
 Create Date: 2019-01-16 21:09:44.779340
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '16d8ed4fdf59'
-down_revision = '315fe12bd8d2'
+down_revision = '2e01f7df1fb8'
 
 from alembic import op
 import sqlalchemy as sa
