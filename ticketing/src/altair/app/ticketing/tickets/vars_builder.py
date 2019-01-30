a1d221372bb4175811f3ea34208a91724376097b
Sakamoto <hiroshi.sakamoto@p46703.intra.rakuten.co.jp>
2019-01-30 20:56:39 +0900

    TKT-6693 指摘事項修正

--- ticketing/src/altair/app/ticketing/tickets/vars_builder.py
+++ ticketing/src/altair/app/ticketing/tickets/vars_builder.py
@@ -607,9 +607,10 @@ class TicketDictBuilder(object):
                     ordered_product_item_token.used_discount_codes[0].applied_amount:
                 extra[u'チケット価格'] = u'0円【C払戻不可】'
             else:
-                extra[u'チケット価格'] = u'定価 {ordered_product_item_price}【割引{applied_amount}円】'.format(
+                extra[u'チケット価格'] = u'定価 {ordered_product_item_price}【割引{applied_amount}】'.format(
                     ordered_product_item_price=self.formatter.format_currency(ordered_product_item_token.item.price),
-                    applied_amount=ordered_product_item_token.used_discount_codes[0].applied_amount)
+                    applied_amount=self.formatter.format_currency(
+                        ordered_product_item_token.used_discount_codes[0].applied_amount))
         else:
             extra[u'チケット価格'] = self.formatter.format_currency(ordered_product_item_token.item.price)
 
