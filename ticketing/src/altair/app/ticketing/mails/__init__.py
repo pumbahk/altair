0921ca2186c741730601ad55c7b1555e466025c5
weistom <xexxsa@gmail.com>
2019-02-12 14:27:08 +0900

    ticketing.mails にバーコード生成 factory 登録 # tkt-6972

--- ticketing/src/altair/app/ticketing/mails/__init__.py
+++ ticketing/src/altair/app/ticketing/mails/__init__.py
@@ -1,3 +1,5 @@
+from altair.app.ticketing.cooperation.famima.interfaces import IFamimaURLGeneratorFactory
+from altair.app.ticketing.cooperation.famima.barcode_url import FamimaURLGeneratorFactory
 from altair.app.ticketing.core.models import MailTypeEnum
 from pyramid.settings import asbool
 
@@ -16,6 +18,9 @@ def install_mail_utility(config):
 
     from .interfaces import IMailDataStoreGetter
     config.registry.registerUtility(config.maybe_dotted(".resources.get_mail_data_store"), IMailDataStoreGetter)
+    # register utility to generate famima e-barcode if nothing
+    if not config.registry.queryUtility(IFamimaURLGeneratorFactory):
+        config.registry.registerUtility(FamimaURLGeneratorFactory, IFamimaURLGeneratorFactory)
 
     # from altair.app.ticketing.mails.simple import SimpleMail
     # config.add_mail_utility(MailTypeEnum.PurchaseCompleteMail, ".simple", SimpleMail)
