988ba2a02f2aa37fd04d3ababb0f6d96bc26cedb
noracorn <noracorn@gmail.com>
2019-02-19 11:11:09 +0900

    QRのIDを間違えて入力された場合は、ページが見つかりませんにする refs #tkt7281

--- ticketing/src/altair/app/ticketing/orderreview/views.py
+++ ticketing/src/altair/app/ticketing/orderreview/views.py
@@ -593,13 +593,13 @@ class QRView(object):
 
         ticket = build_qr_by_history_id(self.request, ticket_id)
 
+        if ticket == None or ticket.sign != sign:
+            raise HTTPNotFound()
+
         ordered_product_items = None
         # 一括の場合はticketがproductの情報を持たない。
         if not ticket.product:
             ordered_product_items = [element for item in ticket.order.items for element in item.elements]
-
-        if ticket == None or ticket.sign != sign:
-            raise HTTPNotFound()
 
         return dict(
             sign = sign,
