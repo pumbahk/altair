30fe6819e978eb939b06819152fe6d606d1887b0
noracorn <noracorn@gmail.com>
2019-02-21 09:30:12 +0900

    SPDBの旧サーバへの送信は行わない refs #tkt7293

--- ticketing/src/altair/app/ticketing/spdb/commands.py
+++ ticketing/src/altair/app/ticketing/spdb/commands.py
@@ -1,6 +1,5 @@
 # -*- coding:utf-8 -*-
 import os
-import sys
 import uuid
 import logging
 import socket
@@ -13,63 +12,6 @@ from datetime import datetime, timedelta
 from paramiko import SSHClient, AutoAddPolicy
 
 logger = logging.getLogger(__name__)
-
-spdb_old_sql = """
-    SELECT
-        CONCAT(`Order`.order_no, OrderedProductItemToken.serial) as PrimaryKey,
-        `Order`.order_no,
-        CASE
-            WHEN `Order`.canceled_at IS NOT NULL THEN 'canceled'
-            WHEN delivered_at IS NOT NULL THEN 'delivered'
-            ELSE 'ordered'
-        END AS order_status,
-        CASE
-            WHEN `Order`.refund_id IS NOT NULL AND `Order`.refunded_at IS NULL THEN 'refunding'
-            WHEN refunded_at IS NOT NULL THEN 'refunded'
-            WHEN paid_at IS NOT NULL THEN 'paid'
-            ELSE 'unpaid'
-        END AS payment_status,
-        `Order`.created_at,
-        `Order`.user_id,
-        Event.id as event_id,
-        Event.title as event_title,
-        Performance.id as performance_id,
-        Performance.name as performance_name,
-        Performance.start_on as performance_start_on,
-        ProductItem.name as product_item_name,
-        Seat.name as seat_name,
-        ProductItem.price as product_item_price,
-        ProductItem.quantity as product_item_quantity,
-        `Order`.system_fee + `Order`.special_fee + `Order`.delivery_fee + `Order`.transaction_fee AS fee,
-        `Order`.total_amount,
-        UserCredential.auth_identifier,
-        UserCredential.authz_identifier,
-        Membership.name as membership_name,
-        MemberGroup.name as membergroup_name,
-        PointGrantHistoryEntry.amount as point,
-        CASE PaymentMethod.payment_plugin_id WHEN 1 THEN 'マルチ決済' WHEN 2 THEN '楽天ペイ' WHEN 3 THEN 'セブン' WHEN 4 THEN '窓口' WHEN 5 THEN '無料' WHEN 6 THEN 'ファミマ' ELSE '不明' END AS payment_method,
-        CASE DeliveryMethod.delivery_plugin_id WHEN 1 THEN '配送' WHEN 2 THEN 'セブン' WHEN 3 THEN '窓口' WHEN 4 THEN 'QR認証' WHEN 5 THEN 'イベントゲート' WHEN 6 THEN' ファミマ' ELSE '不明' END AS delivery_method,
-        Cart.user_agent
-
-    FROM
-        `Order`
-    JOIN Cart ON Cart.order_id = `Order`.id
-    JOIN Performance ON `Order`.performance_id = Performance.id
-    JOIN Event ON Event.id = Performance.event_id
-    JOIN OrderedProduct ON OrderedProduct.order_id = `Order`.id
-    JOIN OrderedProductItem ON OrderedProductItem.ordered_product_id = OrderedProduct.id
-    JOIN OrderedProductItemToken ON OrderedProductItemToken.ordered_product_item_id = OrderedProductItem.id
-    JOIN Product ON OrderedProduct.product_id = Product.id
-    JOIN ProductItem ON OrderedProductItem.product_item_id = ProductItem.id
-    LEFT JOIN Seat ON Seat.id = OrderedProductItemToken.seat_id
-    LEFT JOIN UserCredential ON UserCredential.user_id = `Order`.user_id
-    LEFT JOIN MemberGroup ON MemberGroup.id = `Order`.membergroup_id
-    LEFT JOIN Membership ON Membership.id = MemberGroup.membership_id
-    LEFT JOIN PointGrantHistoryEntry ON PointGrantHistoryEntry.order_id = `Order`.id
-    JOIN PaymentDeliveryMethodPair ON PaymentDeliveryMethodPair.id = `Order`.payment_delivery_method_pair_id
-    JOIN PaymentMethod ON PaymentMethod.id = PaymentDeliveryMethodPair.payment_method_id
-    JOIN DeliveryMethod ON DeliveryMethod.id = PaymentDeliveryMethodPair.delivery_method_id
-"""
 
 spdb_sql = """
     SELECT
@@ -249,7 +191,7 @@ class FileOperator(object):
         f = open(file_name, 'a')
         f.close()
 
-    def upload(self, remote_path="/home/odin_ticket/from_ticket/", old=None):
+    def upload(self, remote_path="/home/odin_ticket/from_ticket/"):
         upload_file_name = "{0}{1}".format(self.tmp_dir, self.file_name)
         upload_gzip_name = "{0}.gz".format(upload_file_name)
         upload_flg_name = "{0}.flg".format(upload_gzip_name)
@@ -264,12 +206,8 @@ class FileOperator(object):
         if not self._args.local:
             ssh = SSHClient()
             ssh.set_missing_host_key_policy(AutoAddPolicy())
-            if old:
-                ssh.connect(self.spdb_info.old_host, self.spdb_info.port, self.spdb_info.user,
-                            key_filename=self.spdb_info.private_key)
-            else:
-                ssh.connect(self.spdb_info.host, self.spdb_info.port, self.spdb_info.user,
-                            key_filename=self.spdb_info.private_key)
+            ssh.connect(self.spdb_info.host, self.spdb_info.port, self.spdb_info.user,
+                        key_filename=self.spdb_info.private_key)
 
             sftp = ssh.open_sftp()
             sftp.put(upload_gzip_name, "{0}{1}".format(remote_path, self.gzip_file_name))
@@ -295,10 +233,6 @@ class SpdbInfo(object):
         self.settings = self.env['registry'].settings
 
     @property
-    def old_host(self):
-        return self.settings['spdb.old_host']
-
-    @property
     def host(self):
         return self.settings['spdb.host']
 
@@ -317,38 +251,6 @@ class SpdbInfo(object):
     @property
     def var_dir(self):
         return self.settings['spdb.var_dir']
-
-
-def send_spdb_data_old(args, connection):
-    sql_creater = SQLCreater(args, spdb_old_sql)
-    file_operator = FileOperator(args, SpdbInfo(args))
-
-    file_operator.open_tmp_file()
-    logger.info(u"SPDB old start {0}".format(file_operator.file_name))
-
-    try:
-        cur = connection.cursor()
-        cur.execute(sql_creater.sql)
-        orders = cur.fetchall()
-        file_operator.write_tmp_file(
-            u"\"PrimaryKey\",\"予約番号\",\"ステータス\",\"支払いステータス\",\"予約時間\",\"ユーザID\",\"イベントID\",\"イベントタイトル\",\"パフォーマンスID\",\"パフォーマンス名\",\"開演時間\",\"商品明細名\",\"席名\",\"商品明細金額\",\"商品明細個数\",\"手数料\",\"合計金額\",\"auth_identifier\",\"authz_identifier\",\"会員種別名\",\"会員区分名\",\"ポイント\",\"支払い方法\",\"引取方法\",\"デバイス\"\n".encode(
-                'utf-8'))
-        for row in orders:
-            row = chop_none(row)
-            file_operator.write_tmp_file(
-                u"\"{0[PrimaryKey]}\",\"{0[order_no]}\",\"{0[order_status]}\",\"{0[payment_status]}\",\"{0[created_at]}\",\"{0[user_id]}\",\"{0[event_id]}\",\"{0[event_title]}\",\"{0[performance_id]}\",\"{0[performance_name]}\",\"{0[performance_start_on]}\",\"{0[product_item_name]}\",\"{0[seat_name]}\",\"{0[product_item_price]}\",\"{0[product_item_quantity]}\",\"{0[fee]}\",\"{0[total_amount]}\",\"{0[auth_identifier]}\",\"{0[authz_identifier]}\",\"{0[membership_name]}\",\"{0[membergroup_name]}\",\"{0[point]}\",\"{0[payment_method]}\",\"{0[delivery_method]}\",\"{0[user_agent]}\"\n".format(
-                    row).encode('utf-8'))
-        file_operator.close_tmp_file()
-        file_operator.upload(old=True)
-    except socket.error as e:
-        logger.error(u"SPDB old upload error.{0}".format(file_operator.file_name))
-
-    finally:
-        if connection:
-            connection.close()
-        file_operator.close_tmp_file()
-
-    logger.info(u"SPDB old end {0}".format(file_operator.file_name))
 
 
 def send_spdb_data(args, connection):
@@ -409,10 +311,8 @@ def main():
     setup_logging(args.conf)
 
     global spdb_sql
-    global spdb_old_sql
 
     send_spdb_data(args, get_connection(args))
-    send_spdb_data_old(args, get_connection(args))
 
 
 def get_channel_str(channel_num):
