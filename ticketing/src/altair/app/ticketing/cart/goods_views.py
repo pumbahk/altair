0f0d83159710f413828bc43bc1ce5398c39c1781
ts-dongmei.zhang <ts-dongmei.zhang@rakuten.com>
2019-03-01 16:00:36 +0900

    TKT-7157受付フォームで公演の並び順がランダム

--- ticketing/src/altair/app/ticketing/cart/goods_views.py
+++ ticketing/src/altair/app/ticketing/cart/goods_views.py
@@ -84,6 +84,7 @@ class GoodsEventIndexView(object):
             sales_segment.performance
             for sales_segment in self.context.available_sales_segments
             )
+        performances = sorted(performances, key=lambda p: (p.display_order, p.id))
         links = [
             (performance.name, self.request.route_path('cart.index2', performance_id=performance.id))
             for performance in performances
