8a4d8d5de64bcdeff46d077f87cfb22cc0c9b40f
weistom <xexxsa@gmail.com>
2019-03-13 15:36:24 +0900

    １週間前の変数名修正 # tkt-6972

--- ticketing/src/altair/app/ticketing/famiport/scripts/famiport_change_shop_code.py
+++ ticketing/src/altair/app/ticketing/famiport/scripts/famiport_change_shop_code.py
@@ -64,9 +64,9 @@ class ShopCodeChangeProcessor(object):
                             # famiport_order_identifier の最初3桁は FamiPortClient の prefix です。
                             # 後方一致はインデックスが効かないので、インデックスのある completed_at で過去１週間のデータに絞ってから検索します。
                             # 90 分確定かどうかの判定は完了時刻と救済時刻の有無です。
-                            yesterday = date.today() - timedelta(days=7)
+                            from_day = date.today() - timedelta(days=7)
                             famiport_receipt = self.db_session.query(FamiPortReceipt)\
-                                .filter(FamiPortReceipt.completed_at >= yesterday.strftime('%Y-%m-%d'))\
+                                .filter(FamiPortReceipt.completed_at >= from_day.strftime('%Y-%m-%d'))\
                                 .filter(FamiPortReceipt.rescued_at.isnot(None))\
                                 .filter(FamiPortReceipt.famiport_order_identifier.endswith(management_number))\
                                 .one()
