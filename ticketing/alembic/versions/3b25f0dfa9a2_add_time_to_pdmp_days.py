126918778c4bf92630cf2aa2f43a47745ea4484b
weistom <xexxsa@gmail.com>
2019-03-18 17:50:07 +0900

    PDMPに相対時間の追加 # tkt-7405

---
+++ ticketing/alembic/versions/3b25f0dfa9a2_add_time_to_pdmp_days.py
@@ -0,0 +1,32 @@
+"""Adds time type columns to PaymentDeliveryMethodPair
+
+Revision ID: 3b25f0dfa9a2
+Revises: 2e1dcb75573
+Create Date: 2019-03-18 16:55:31.930764
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3b25f0dfa9a2'
+down_revision = '2e1dcb75573'
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.sql.expression import text
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.add_column(table_name='PaymentDeliveryMethodPair',
+                  column=sa.Column('payment_period_time', sa.Time(), nullable=True, server_default=text('0')))
+    op.add_column(table_name='PaymentDeliveryMethodPair',
+                  column=sa.Column('issuing_interval_time', sa.Time(), nullable=True, server_default=text('0')))
+    op.add_column(table_name='PaymentDeliveryMethodPair',
+                  column=sa.Column('issuing_end_in_time', sa.Time(), nullable=True, server_default=text('0')))
+
+
+def downgrade():
+    op.drop_column(table_name='PaymentDeliveryMethodPair', column_name='payment_period_time')
+    op.drop_column(table_name='PaymentDeliveryMethodPair', column_name='issuing_interval_time')
+    op.drop_column(table_name='PaymentDeliveryMethodPair', column_name='issuing_end_in_time')
