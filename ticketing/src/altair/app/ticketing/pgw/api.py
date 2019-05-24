665882fa1e37d914af7697ccb0b3adae641f9d90
Sakamoto <hiroshi.sakamoto@p46703.intra.rakuten.co.jp>
2019-05-24 17:43:00 +0900

    TKT-7857 メソッドのコメントを追加

--- ticketing/src/altair/app/ticketing/pgw/api.py
+++ ticketing/src/altair/app/ticketing/pgw/api.py
@@ -3,6 +3,9 @@ import altair.pgw.api as pgw_api
 
 
 class PGWPayment(object):
+    """
+    authorize, authorize_and_capture呼び出し時に生成するリクエストオブジェクトです
+    """
     def __init__(self, request, sub_service_id, payment_id, gross_amount,
                  card_amount, card_token, cvv_token, email, three_d_secure_authentication_result=None):
         self.request = request
@@ -17,6 +20,12 @@ class PGWPayment(object):
 
 
 def pgw_authorize(request, pgw_payment):
+    """
+    PGWのAuthorizeAPIをコールします
+    :param request: リクエスト
+    :param pgw_payment: PGW決済リクエストオブジェクト(ticketing.src.altair.app.ticketing.pgw.api.PGWPayment)
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのAuthorizeAPIをコールします
     result = pgw_api.authorize(request, pgw_payment)
 
@@ -27,6 +36,13 @@ def pgw_authorize(request, pgw_payment):
 
 
 def pgw_capture(request, payment_id, capture_amount):
+    """
+    PGWのCaptureAPIをコールします
+    :param request: リクエスト
+    :param payment_id: 予約番号(cart:order_no, lots:entry_no)
+    :param capture_amount: キャプチャする決済金額
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのCaptureAPIをコールします
     result = pgw_api.capture(request, payment_id, capture_amount)
 
@@ -36,6 +52,12 @@ def pgw_capture(request, payment_id, capture_amount):
 
 
 def pgw_authorize_and_capture(request, pgw_payment):
+    """
+    PGWのAuthorizeAndCaptureAPIをコールします
+    :param request: リクエスト
+    :param pgw_payment: PGW決済リクエストオブジェクト(ticketing.src.altair.app.ticketing.pgw.api.PGWPayment)
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのAuthorizeAndCaptureAPIをコールします
     result = pgw_api.authorize_and_capture(request, pgw_payment)
 
@@ -46,6 +68,13 @@ def pgw_authorize_and_capture(request, pgw_payment):
 
 
 def pgw_find(request, payment_ids, search_type=None):
+    """
+    PGWのFindAPIをコールします
+    :param request: リクエスト
+    :param payment_ids: 予約番号リスト(cart:order_no, lots:entry_no)
+    :param search_type: 検索タイプ
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのFindAPIをコールします
     result = pgw_api.find(request, payment_ids, search_type)
 
@@ -53,6 +82,12 @@ def pgw_find(request, payment_ids, search_type=None):
 
 
 def pgw_cancel_or_refund(request, payment_id):
+    """
+    PGWのCancelOrRefundAPIをコールします
+    :param request: リクエスト
+    :param payment_id: 予約番号(cart:order_no, lots:entry_no)
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのCancelOrRefundAPIをコールします
     result = pgw_api.cancel_or_refund(request, payment_id)
 
@@ -62,6 +97,13 @@ def pgw_cancel_or_refund(request, payment_id):
 
 
 def pgw_modify(request, payment_id, modified_amount):
+    """
+    PGWのModifyAPIをコールします
+    :param request: リクエスト
+    :param payment_id: 予約番号(cart:order_no, lots:entry_no)
+    :param modified_amount: 変更後の決済金額
+    :return: PGWからのAPIレスポンス
+    """
     # PGWのModifyAPIをコールします
     result = pgw_api.modify(request, payment_id, modified_amount)
 
@@ -71,6 +113,16 @@ def pgw_modify(request, payment_id, modified_amount):
 
 
 def pgw_three_d_secure_enrollment_check(request, sub_service_id, enrollment_id, callback_url, amount, card_token):
+    """
+    PGWの3DSecureEnrollmentCheckAPIをコールします
+    :param request: リクエスト
+    :param sub_service_id: 店舗ID
+    :param enrollment_id: 3DSecure認証用ID(予約番号)(cart:order_no, lots:entry_no)
+    :param callback_url: コールバックURL
+    :param amount: 決済予定金額
+    :param card_token: カードトークン
+    :return: PGWからのAPIレスポンス
+    """
     # PGWの3DSecureEnrollmentCheckAPIをコールします
     result = pgw_api.three_d_secure_enrollment_check(
         request,
@@ -87,6 +139,12 @@ def pgw_three_d_secure_enrollment_check(request, sub_service_id, enrollment_id, 
 
 
 def get_pgw_status(request, payment_id):
+    """
+    PGWOrderStatusのステータスを返します
+    :param request: リクエスト
+    :param payment_id: 予約番号(cart:order_no, lots:entry_no)
+    :return:
+    """
     # PGWOrderStatusのステータスを返す
     # 現状は暫定でPGWのFindAPIのステータスを返すようにします
     result = pgw_api.find(request, payment_id)
