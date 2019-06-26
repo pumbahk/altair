e99501f1797945ccb07c94dbdbffc5149b101ac1
Liu, Ruiqiang | OSPD <ts-ruiqiang.liu@rakuten.com>
2019-06-26 17:54:42 +0900

    TKT-7931 会員種別に表示順と表示／非表示機能を追加

--- ticketing/src/altair/app/ticketing/memberships/__init__.py
+++ ticketing/src/altair/app/ticketing/memberships/__init__.py
@@ -3,5 +3,8 @@ def includeme(config):
     config.add_route("membergroups", "/membergroups/{action}/{membergroup_id:.*}", )
     config.add_route("membergroups.sales_segment_groups", "/sales_segment_groups/{action}/{membergroup_id:.*}", )
     config.add_route("membergrups.api.sales_segment_groups.candidates", "/api/{event_id}")
+    config.add_route('memberships.visible', '/visible')
+    config.add_route('memberships.invisible', '/invisible')
     config.scan(".views")
     
+VISIBLE_MEMBERSHIPS_SESSION_KEY = "_visible_memberships"
