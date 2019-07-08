61f8beea69234f319b36e36c7c82b2fbe567b529
weistom <xexxsa@gmail.com>
2019-07-08 13:44:15 +0900

    predicateクラス名変更 # tkt-7932

--- ticketing/src/altair/app/ticketing/cooperation/rakuten_live/__init__.py
+++ ticketing/src/altair/app/ticketing/cooperation/rakuten_live/__init__.py
@@ -1,4 +1,4 @@
-from altair.app.ticketing.cooperation.rakuten_live.predicates import RakutenLiveRouteContainedIn
+from altair.app.ticketing.cooperation.rakuten_live.predicates import RakutenLiveRequestCorrespondingTo
 
 # List of route names possible to be requested from R-Live App
 # Route names need to contain performance_id or lot_id
@@ -18,5 +18,5 @@ R_LIVE_REQUEST_ROUTES = (
 
 def includeme(config):
     config.include('.communicator')
-    config.add_subscriber_predicate('r_live_route', RakutenLiveRouteContainedIn)
+    config.add_subscriber_predicate('r_live_route', RakutenLiveRequestCorrespondingTo)
     config.scan('.subscribers')
