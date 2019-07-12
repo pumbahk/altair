f89b107e27c9d1740eda293a82c19822c43ef088
noracorn <noracorn@gmail.com>
2019-07-12 16:43:14 +0900

    現時点のSMAのアセットを登録 refs #tkt8228

--- cms/src/altaircms/static/ST/js/common.js
+++ cms/src/altaircms/static/ST/js/common.js
@@ -1,4 +1,13 @@
 ﻿$(function(){
+    var ua = navigator.userAgent;
+    if((ua.indexOf('iPhone') > 0) || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)){
+        $('head').prepend('<meta name="viewport" content="width=device-width,initial-scale=1">');
+    } else {
+        $('head').prepend('<meta name="viewport" content="width=1200">');
+    }
+});
+
+$(function(){
   $('a[href^="#"]').click(function() {
       var speed = 400;
       var href= $(this).attr("href");
---
+++ cms/src/altaircms/static/ST/js/help.js
@@ -0,0 +1,8 @@
+﻿$(function(){
+  $('.help_list dt p').click(function() {
+    $('.help_list dd').slideToggle();
+    $('.help_list').toggleClass("on");
+  });
+});
+
+
