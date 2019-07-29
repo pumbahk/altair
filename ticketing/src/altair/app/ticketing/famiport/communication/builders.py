d7870f52af94cffc94f0b31dc806e1f6f96ee8b4
weistom <xexxsa@gmail.com>
2019-07-29 13:27:54 +0900

    ファミマ予約が見つからないアラートのログレベルを下げる # tkt-7860

--- ticketing/src/altair/app/ticketing/famiport/communication/builders.py
+++ ticketing/src/altair/app/ticketing/famiport/communication/builders.py
@@ -431,7 +431,10 @@ class FamiPortPaymentTicketingResponseBuilder(FamiPortResponseBuilder):
             try:
                 famiport_receipt = FamiPortReceipt.get_by_barcode_no(barCodeNo, session=session)
             except NoResultFound:
-                logger.exception(u'FamiPortOrder not found with barCodeNo=%s' % barCodeNo)
+                # Famiポートと電子バーコードの両方で操作が行われた時に最初に来た入金発券要求リクエストで処理が通るので
+                # 後から来た入金発券要求リクエストのバーコード番号から予約は見つかりません。
+                # ユーザーは最初行った操作アクセスで入金・発券を行えているのでアラートに出しません。TKT-7860
+                logger.warn(u'FamiPortOrder not found with barCodeNo=%s' % barCodeNo)
                 replyCode = ReplyCodeEnum.SearchKeyError.value
 
             if famiport_receipt is not None:
