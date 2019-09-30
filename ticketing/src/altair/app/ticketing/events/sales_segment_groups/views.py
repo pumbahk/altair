39f0cfe1700d29fee5a907c6e6ff07cca6621b9b
Liu, Ruiqiang | OSPD <ts-ruiqiang.liu@rakuten.com>
2019-09-30 10:39:29 +0900

    TKT-7931 会員種別に表示順と表示／非表示機能を追加:fix new design

--- ticketing/src/altair/app/ticketing/events/sales_segment_groups/views.py
+++ ticketing/src/altair/app/ticketing/events/sales_segment_groups/views.py
@@ -299,7 +299,7 @@ class SalesSegmentGroups(BaseView, SalesSegmentViewHelperMixin):
         sales_segment_group = self.context.sales_segment_group
         redirect_to = self.request.route_path("sales_segment_groups.show",  sales_segment_group_id=sales_segment_group.id)
         membergroups = MemberGroup.query.filter(MemberGroup.membership_id==Membership.id, Membership.organization_id==self.context.user.organization_id)\
-            .filter(Membership.visible == True).order_by(Membership.display_order)
+            .filter(Membership.visible == True).order_by(Membership.display_order).order_by(Membership.id)
         form = MemberGroupToSalesSegmentForm(obj=sales_segment_group, membergroups=membergroups)
         return {
             'form': form,
