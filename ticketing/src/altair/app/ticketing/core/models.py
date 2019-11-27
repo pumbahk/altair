ad06bdf73eab4aa4d376429227b6f6bb9308340f
ts-yasuhito.ito <ts-yasuhito.ito>
2019-11-27 19:35:39 +0900

    TKT-8936 StockTypeテーブルにattributeカラム追加

--- ticketing/src/altair/app/ticketing/core/models.py
+++ ticketing/src/altair/app/ticketing/core/models.py
@@ -2642,6 +2642,7 @@ class StockType(Base, BaseModel, WithTimestamp, LogicallyDeleted):
     max_quantity = Column(Integer, nullable=True, default=None)
     min_product_quantity = Column(Integer, nullable=True, default=None)
     max_product_quantity = Column(Integer, nullable=True, default=None)
+    attribute = Column(String(255), nullable=False)
 
     @property
     def is_seat(self):
