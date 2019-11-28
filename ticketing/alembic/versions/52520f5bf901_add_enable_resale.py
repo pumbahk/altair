4439c612a9aeb3653bf17f10b84de0e844eddcc6
pumbahk <pumbahk@gmail.com>
2019-11-28 14:05:02 +0900

    TKT-8917[QR-Resale][Alembic]販売区分単位でリセール可否を設定,DBスキーマ変更 #SalesSegmentGroupSetting, SalesSegmentSettingスキーマ変更 #販売区分、販売区分グループの画面改修

---
+++ ticketing/alembic/versions/52520f5bf901_add_enable_resale.py
@@ -0,0 +1,31 @@
+"""add enable_resale SalesSegmentGroupSetting
+
+Revision ID: 52520f5bf901
+Revises: 294ca5bb74d2
+Create Date: 2019-11-27 17:30:23.203595
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '52520f5bf901'
+down_revision = '294ca5bb74d2'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.add_column('SalesSegmentGroupSetting',
+                  sa.Column('enable_resale', sa.Boolean(), nullable=False, server_default=text('0')))
+    op.add_column('SalesSegmentSetting',
+                  sa.Column('enable_resale', sa.Boolean(), nullable=False, server_default=text('0')))
+    op.add_column('SalesSegmentSetting',
+                  sa.Column('use_default_enable_resale', sa.Boolean(), nullable=False, server_default=text('0')))
+
+def downgrade():
+    op.drop_column('SalesSegmentGroupSetting', 'enable_resale')
+    op.drop_column('SalesSegmentSetting', 'enable_resale')
+    op.drop_column('SalesSegmentSetting', 'use_default_enable_resale')
