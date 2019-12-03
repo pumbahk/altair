b0500a8c4cdc2ead9199bb88883899cdfd8731cd
ts-yasuhito.ito <ts-yasuhito.ito>
2019-12-03 14:48:44 +0900

    TKT-8964 PaymentGW Alembicのrebase(20191203)

--- ticketing/alembic/versions/3cd6601615a3_add_column_organizationsetting_for_payment_gw.py
+++ ticketing/alembic/versions/3cd6601615a3_add_column_organizationsetting_for_payment_gw.py
@@ -1,14 +1,14 @@
 """Add column OrganizationSetting for PaymentGW.
 
 Revision ID: 3cd6601615a3
-Revises: 2f1e122b431b
+Revises: 44b46209e07f
 Create Date: 2019-04-25 15:34:48.282642
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '3cd6601615a3'
-down_revision = '2f1e122b431b'
+down_revision = '44b46209e07f'
 
 from alembic import op
 import sqlalchemy as sa
