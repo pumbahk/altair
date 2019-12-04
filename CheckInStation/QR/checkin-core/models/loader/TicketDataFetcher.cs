90c8e0237312bcb542c3fbba975924f9c584bc69
Ono <kazuhiko.ono@p60512.intra.rakuten.co.jp>
2019-12-04 15:57:09 +0900

    TKT-8852:[QRゲート]【チェックインステーション】クライアントサイド新規API対応

--- CheckInStation/QR/checkin-core/models/loader/TicketDataFetcher.cs
+++ CheckInStation/QR/checkin-core/models/loader/TicketDataFetcher.cs
@@ -24,34 +24,61 @@ namespace checkin.core.models
             public bool refreshMode { get; set; }
         }
 
+        public class QRSkidataRequest : QRRequest
+        {
+            public string qrdata { get; set; }
+        }
+
         public TicketDataFetcher (IResource resource)
         {
             Resource = resource;
         }
 
-        public virtual string GetQRFetchDataUrl ()
+        public virtual string GetQRFetchDataUrl (bool isSkidata)
         {
+            if (isSkidata)
+            {
+                return Resource.EndPoint.QRSkidataFetchData;
+            }
             return Resource.EndPoint.QRFetchData;
+        }
+
+        public QRRequest getRequestData(bool isSkidata, string qrcode)
+        {
+            return isSkidata ? new QRSkidataRequest() { qrdata = qrcode, refreshMode = this.Resource.RefreshMode } 
+                             : new QRRequest() { qrsigned = qrcode, refreshMode = this.Resource.RefreshMode };
         }
 
         public async Task<ResultTuple<string, TicketData>> FetchAsync(string qrcode)
         {
             IHttpWrapperFactory<HttpWrapper> factory = Resource.HttpWrapperFactory;
-            using (var wrapper = factory.Create(GetQRFetchDataUrl()))
+            bool isSkidata = qrcode.Length == 20;
+            using (var wrapper = factory.Create(GetQRFetchDataUrl(isSkidata)))
             {
-                var qrdata = new QRRequest() { qrsigned = qrcode, refreshMode = this.Resource.RefreshMode };
+#if DEBUG
+                Console.WriteLine("isSkidata: " + isSkidata);
+                Console.WriteLine("qrcode: " + qrcode);
+                Console.WriteLine("URL: " + wrapper.UrlBuilder.Build());
+#endif
+                var qrdata = getRequestData(isSkidata, qrcode);
+#if DEBUG
+                Console.WriteLine("Request data: " + qrdata.ToString());
+#endif
                 HttpResponseMessage response = await wrapper.PostAsJsonAsync(qrdata).ConfigureAwait(false);
                 response.EnsureSuccessStatusCodeExtend();
-                return Parse(await wrapper.ReadAsStreamAsync(response.Content).ConfigureAwait(false));
+                return Parse(await wrapper.ReadAsStreamAsync(response.Content).ConfigureAwait(false), isSkidata);
             }
 
         }
 
-        public ResultTuple<string, TicketData> Parse (Stream response)
+        public ResultTuple<string, TicketData> Parse (Stream response, bool isSkidata)
         {
             try {
                 var json = DynamicJson.Parse (response);
-                logger.Info("*API Response* method=POST, url={0}, data={1}".WithMachineName(), this.GetQRFetchDataUrl(), json.ToString());
+#if DEBUG
+                Console.WriteLine("Response data: " + json);
+#endif
+                logger.Info("*API Response* method=POST, url={0}, data={1}".WithMachineName(), this.GetQRFetchDataUrl(isSkidata), json.ToString());
                 return new Success<string, TicketData> (new TicketData (json));
             } catch (System.Xml.XmlException e) {
                 logger.ErrorException (":".WithMachineName(), e);
