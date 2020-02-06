70c0be048d82a1092c50ff5b164b499880eed4f6
noracorn <noracorn@gmail.com>
2020-02-06 10:46:46 +0900

    alembic のリベース

--- ticketing/alembic/versions/3cd6601615a3_add_column_organizationsetting_for_payment_gw.py
+++ ticketing/alembic/versions/3cd6601615a3_add_column_organizationsetting_for_payment_gw.py
@@ -1,14 +1,14 @@
 """Add column OrganizationSetting for PaymentGW.
 
 Revision ID: 3cd6601615a3
-Revises: 58f5b688709d
+Revises: 1d494fd22d5b
 Create Date: 2019-04-25 15:34:48.282642
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '3cd6601615a3'
-down_revision = '58f5b688709d'
+down_revision = '1d494fd22d5b'
 
 from alembic import op
 import sqlalchemy as sa
