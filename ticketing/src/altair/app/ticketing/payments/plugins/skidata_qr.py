e5b39f502d3ccac12c50c7ca1cb5c523d8d102fb
ts-dongmei.zhang <ts-dongmei.zhang@rakuten.com>
2020-02-07 10:25:17 +0900

    TKT9344--レビュー対応

--- ticketing/src/altair/app/ticketing/payments/plugins/skidata_qr.py
+++ ticketing/src/altair/app/ticketing/payments/plugins/skidata_qr.py
@@ -65,7 +65,10 @@ def deliver_completion_viewlet(context, request):
 @lbr_view_config(context=ILotsAcceptedMailResource, name="delivery-%d" % DELIVERY_PLUGIN_ID,
                  renderer=_overridable("skidata_qr_mail_complete.html", fallback_ua_type='mail'))
 def deliver_completion_mail_viewlet(context, request):
-    return dict(notice=context.mail_data("D", "notice"))
+    order = context.order
+    logger.debug(u"order_no = %s" % order.order_no)
+    delivery_method_id = order.payment_delivery_pair.delivery_method_id
+    return dict(delivery_method_id=delivery_method_id, notice=context.mail_data("D", "notice"))
 
 
 @lbr_view_config(context=IOrderCancelMailResource, name="delivery-%d" % DELIVERY_PLUGIN_ID)
