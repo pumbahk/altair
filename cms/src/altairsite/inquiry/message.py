b10220804357ad5b551397ffa5818167a032347a
ts-dongmei.zhang <ts-dongmei.zhang@rakuten.com>
2020-02-21 10:49:22 +0900

    TKT8689--メール内容の変更

--- cms/src/altairsite/inquiry/message.py
+++ cms/src/altairsite/inquiry/message.py
@@ -82,28 +82,34 @@ class SupportMailST:
 
     implements(IMailMessage)
 
-    def __init__(self, username, username_kana, mail, tel, num, member_id, title, body, user_agent):
+    def __init__(self, username, username_kana, mail, zip_no, address, tel, num, app_status, event_name, start_date, category, body, user_agent):
         self.username = username
         self.username_kana = username_kana
         self.mail = mail
+        self.zip_no = zip_no
+        self.address = address
         self.tel = tel
         self.num = num
-        self.member_id = member_id
-        self.title = title
+        self.app_status = app_status
+        self.event_name = event_name
+        self.start_date = start_date
+        self.category = category
         self.body = body
-        self.user_agent = user_agent
 
-    def  create_mail(self):
+    def create_mail(self):
         result = self.username + "(" + self.username_kana+ ")" + u"さんからのお問合せです。\n"
         result = result + u"メールアドレス：" + str(self.mail) + u"\n"
+        result = result + u"住所：〒" + self.zip_no + u"\n" + self.address + u"\n"
         result = result + u"電話番号：" + str(self.tel) + u"\n"
-        result = result + u"受付番号：" + str(self.num) + u"\n\n"
 
         result = result + u"---------------------------------------\n"
-        result = result + u"会員ID ：" + self.member_id + u"\n"
-        result = result + u"件名：" + self.title + u"\n"
+        result = result + u"受付番号：" + str(self.num) + u"\n\n"
+        result = result + u"申し込み状況：" + self.app_status + u"\n\n"
+        result = result + u"公演・イベント名：" + self.event_name + u"\n\n"
+        result = result + u"開催日時：" + self.start_date + u"\n\n"
+        result = result + u"お問い合わせ項目：" + self.category + u"\n"
         result = result + u"---------------------------------------\n"
-        result = result + u"内容：" + self.body + u"\n\n"
+        result = result + u"お問い合わせ内容：" + self.body + u"\n\n"
 
         result = result + self.user_agent + u"\n\n"
         return result
@@ -113,14 +119,18 @@ class CustomerMailST:
 
     implements(IMailMessage)
 
-    def __init__(self, username, username_kana, mail, tel, num, member_id, title, body):
+    def __init__(self, username, username_kana, mail, zip_no, address, tel, num, app_status, event_name, start_date, category, body):
         self.username = username
         self.username_kana = username_kana
         self.mail = mail
+        self.zip_no = zip_no
+        self.address = address
         self.tel = tel
         self.num = num
-        self.member_id = member_id
-        self.title = title
+        self.app_status = app_status
+        self.event_name = event_name
+        self.start_date = start_date
+        self.category = category
         self.body = body
 
     def create_mail(self):
@@ -130,10 +140,11 @@ class CustomerMailST:
         result = result + u"この度は、弊社にお問い合わせを頂き、ありがとうございました。以下の内容で、お問い合わせを受け付けました。\n\n"
 
         result = result + u"受付番号：" + str(self.num) + u"\n"
-        result = result + u"会員ID：" + str(self.member_id) + u"\n"
-        result = result + u"件名：" + self.title + u"\n"
-        result = result + u"内容：" + self.body + u"\n\n"
-
+        result = result + u"申し込み状況：" + self.app_status + u"\n"
+        result = result + u"公演・イベント名：" + self.event_name + u"\n"
+        result = result + u"開催日時：" + str(self.start_date) + u"\n"
+        result = result + u"お問い合わせ項目：" + self.category + u"\n"
+        result = result + u"お問い合わせ内容：" + self.body + u"\n\n"
         result = result + u"お問い合わせ頂いた内容については、弊社カスタマーサポート担当より、基本的に返信メールにて、3営業日内に回答させていただきます。\n"
         result = result + u"※購入されたチケットの変更・キャンセルの依頼についてはお受けできません。\n"
         result = result + u"その他（SMAアーティスト・SMAVOICE・UCFC等）各種会員サイトに関しては、別サイトになりますので、お答えできない場合がございます。\n"
