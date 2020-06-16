72e80e57291e8b8364e9b8048cda4a478032a48c
pumbahk <pumbahk@gmail.com>
2020-06-16 19:43:58 +0900

    TKT-10188 [orion][アプリ同意画面]電子チケット設定画面から注意文言を設定し、orionに送信する ・Alembic追加(OrionPerformance.questions) ・電子チケット画面に同意文言項目を追加

---
+++ ticketing/alembic/versions/22028c1d2a09_add_columns_questions_to_orionperformance.py
@@ -0,0 +1,25 @@
+"""add columns questions to orionperformance
+
+Revision ID: 22028c1d2a09
+Revises: 2e45c8b61b2d
+Create Date: 2020-06-15 13:08:14.665754
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '22028c1d2a09'
+down_revision = '2e45c8b61b2d'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import functions as sqlf
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.add_column('OrionPerformance', sa.Column('questions', sa.TEXT(), nullable=True, default=""))
+
+def downgrade():
+    op.drop_column('OrionPerformance', 'questions')
