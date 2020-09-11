76bc5eda02ab6bbc7ad99d1a32359664e2926637
noracorn <noracorn@gmail.com>
2020-09-11 12:06:45 +0900

    word alembic

---
+++ ticketing/alembic/versions/2c98f57e91b2_add_talbe_word.py
@@ -0,0 +1,33 @@
+"""add talbe Word
+
+Revision ID: 2c98f57e91b2
+Revises: 46d8b483857d
+Create Date: 2020-09-11 11:54:21.320202
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2c98f57e91b2'
+down_revision = '46d8b483857d'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql import functions as sqlf
+
+Identifier = sa.BigInteger
+
+
+def upgrade():
+    op.create_table(
+        'Word',
+        sa.Column('id', Identifier, primary_key=True),
+        sa.Column('type', sa.String(length=255), nullable=True, default=""),
+        sa.Column('label', sa.String(length=255), nullable=True, default=""),
+        sa.Column('label_kana', sa.String(length=255), nullable=True, default=""),
+        sa.Column('description', sa.String(length=255), nullable=True, default=""),
+    )
+
+
+def downgrade():
+    op.drop_table('Word')
